15cb8ee6e0bd8a1b70462883cc5ff63e63b8b846
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-03-12 21:01:47 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@110 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/NSRange.h
@@ -0,0 +1,54 @@
+/* Interface for NSObject for GNUStep
+   Copyright (C) 1994 NeXT Computer, Inc.
+   
+   Typed by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: August 1994
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __NSRange_h_OBJECTS_INCLUDE
+#define __NSRange_h_OBJECTS_INCLUDE
+
+#include <foundation/NSObject.h>
+
+typedef struct _NSRange {
+  unsigned int location;
+  unsigned int length;
+} NSRange;
+
+
+static inline unsigned
+NSMaxRange(NSRange range) 
+{
+  return range.location + range.length;
+}
+
+static inline BOOL 
+NSLocationInRange(unsigned location, NSRange range) 
+{
+  return (location >= range.location) && (location < NSMaxRange(range));
+}
+
+extern NSRange NSUnionRange(NSRange range1, NSRange range2);
+
+extern NSRange NSIntersectionRange(NSRange range1, NSRange range2);
+
+@class NSString;
+extern NSString *NSStringFromRange(NSRange range);
+
+#endif /* __NSRange_h_OBJECTS_INCLUDE */
