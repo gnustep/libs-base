4b11780999e1e5e138b6e94e70f50c2f17710c3b
Andrew McCallum <mccallum@gnu.org>
1995-03-18 16:39:48 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@153 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/NSCopyObject.m
@@ -0,0 +1,32 @@
+/* Implementation of NSCopyObject() for GNUStep
+   Copyright (C) 1994, 1995 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: August 1994
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#include <objects/stdobjects.h>
+
+NSObject *NSCopyObject(NSObject *anObject, unsigned extraBytes, NSZone *zone)
+{
+  id copy = NSAllocateObject(((id)anObject)->class_pointer, extraBytes, zone);
+  memcpy(copy, anObject, 
+	 ((id)anObject)->class_pointer->instance_size + extraBytes);
+  return copy;
+}
---
+++ Source/NSDeallocateObject.m
@@ -0,0 +1,31 @@
+/* Implementation of NSDeallocateObject() for GNUStep
+   Copyright (C) 1994, 1995 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: August 1994
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#include <objects/stdobjects.h>
+
+void NSDeallocateObject(NSObject *anObject)
+{
+  if ((anObject!=nil) && CLS_ISCLASS(((id)anObject)->class_pointer))
+    NSZoneFree (anObject);
+  return;
+}
