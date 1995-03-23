06b866409a696f88d3215ac2c5f9e8f2b8bb36ab
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-03-23 03:39:18 +0000

    Inherit from NSObject, not Object.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@174 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/NSInvocation.h
+++ Headers/gnustep/base/NSInvocation.h
@@ -1,11 +1,31 @@
-#ifndef __NSInvocation_h_INCLUDE_GNU
-#define __NSInvocation_h_INCLUDE_GNU
+/* Interface for NSInvocation for GNUStep
+   Copyright (C) 1994 NeXT Computer, Inc.
+   
+   This file is part of the GNU Objective C Class Library.
 
-#include <objc/Object.h>
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __NSInvocation_h_OBJECTS_INCLUDE
+#define __NSInvocation_h_OBJECTS_INCLUDE
+
+#include <objects/stdobjects.h>
 
 @class NSMethodSignature;
 
-@interface NSInvocation : Object
+@interface NSInvocation : NSObject
 {
   id methodSignature;
   arglist_t argFrame;
@@ -32,4 +52,4 @@
 
 @end
 
-#endif /* __NSInvocation_h_INCLUDE_GNU */
+#endif /* __NSInvocation_h_OBJECTS_INCLUDE */
