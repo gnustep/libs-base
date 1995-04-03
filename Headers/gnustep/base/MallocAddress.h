e62b6ae45c82c8422a9aebda807cf5b22665900f
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-03 01:38:23 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@232 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/MallocAddress.h
@@ -0,0 +1,42 @@
+/* Provides autoreleasing of malloc'ed pointers
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: January 1995
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __MallocAddress_h_OBJECTS_INCLUDE
+#define __MallocAddress_h_OBJECTS_INCLUDE
+
+#include <objects/stdobjects.h>
+
+@interface MallocAddress : NSObject
+{
+  void *address;
+}
+
++ autoreleaseMallocAddresss: (void*)addr;
++ objectForAddress: (void*)addr;
+
+- initWithAddress: (void*)addr;
+- (void) dealloc;
+
+@end
+
+#endif /* __MallocAddress_h_OBJECTS_INCLUDE */
