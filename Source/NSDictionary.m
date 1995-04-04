636e30cd54e689e1e64c686d67b3b1a33167eaad
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-04 16:15:31 +0000

    Fedor's skeleton.  Fix indentation.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@268 72102866-910b-0410-8b05-ffd578937521

--- Source/NSDictionary.m
+++ Source/NSDictionary.m
@@ -1,11 +1,11 @@
 /* NSDictionary - Dictionary object to store key/value pairs
    Copyright (C) 1993,1994 Free Software Foundation, Inc.
-
+   
    Written by:  Adam Fedor <fedor@boulder.colorado.edu>
    Date: Mar 1995
-
+   
    This file is part of the GNU Objective C Class Library.
-
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
@@ -15,11 +15,11 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
-
+   
    You should have received a copy of the GNU Library General Public
    License along with this library; if not, write to the Free
    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+   */
 
 #include <foundation/NSDictionary.h>
 
@@ -27,112 +27,112 @@
 
 - copyWithZone:(NSZone *)zone
 {
-    return [super copyWithZone:zone];
+  return [super copyWithZone:zone];
 }
 
 + allocWithZone:(NSZone *)zone
 {
-    return [super allocWithZone:zone];
+  return [super allocWithZone:zone];
 }
 
 + dictionary
 {
-    return [[[NSDictionary alloc] init] autorelease];
+  return [[[NSDictionary alloc] init] autorelease];
 }
 
 + dictionaryWithObjects:(id *)objects forKeys:(NSString **)keys count:(unsigned)count
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 + dictionaryWithObjects:(NSArray *)objects forKeys:(NSArray *)keys
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - initWithObjects:(id *)objects forKeys:(NSString **)keys count:(unsigned)count
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - initWithDictionary:(NSDictionary *)otherDictionary
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - initWithContentsOfFile:(NSString *)path
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (unsigned)count
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - objectForKey:(NSString *)aKey
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 //- (NSEnumerator *)keyEnumerator
 //{
-//    [self notImplemented:_cmd];
-//}
+  //    [self notImplemented:_cmd];
+  //}
 
 - (BOOL)isEqualToDictionary:(NSDictionary *)other
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (NSString *)description
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (NSString *)descriptionWithIndent:(unsigned)level
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (NSArray *)allKeys
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (NSArray *)allValues
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (NSArray *)allKeysForObject:anObject
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - (BOOL)writeToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 //- (NSEnumerator *)objectEnumerator
 //{
-//    [self notImplemented:_cmd];
-//}
+  //    [self notImplemented:_cmd];
+  //}
 
 
 @end
@@ -141,104 +141,47 @@
 
 + allocWithZone:(NSZone *)zone
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 + dictionaryWithCapacity:(unsigned)numItems
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 - initWithCapacity:(unsigned)numItems
 {
-    [self notImplemented:_cmd];
-    return 0;
+  [self notImplemented:_cmd];
+  return 0;
 }
 
 
 - (void)setObject:anObject forKey:(NSString *)aKey
 {
-    [self notImplemented:_cmd];
+  [self notImplemented:_cmd];
 }
 
 - (void)removeObjectForKey:(NSString *)aKey
 {
-    [self notImplemented:_cmd];
+  [self notImplemented:_cmd];
 }
 
 - (void)removeAllObjects
 {
-    [self notImplemented:_cmd];
+  [self notImplemented:_cmd];
 }
 
 - (void)removeObjectsForKeys:(NSArray *)keyArray
 {
-    [self notImplemented:_cmd];
+  [self notImplemented:_cmd];
 }
 
 - (void)addEntriesFromDictionary:(NSDictionary *)otherDictionary
 {
-    [self notImplemented:_cmd];
+  [self notImplemented:_cmd];
 }
 
 
 @end
-
-/*	NSDictionary.h
-	Basic dictionary container
-  	Copyright 1993, 1994, NeXT, Inc.
-	NeXT, March 1993
-*/
-
-#include "NSDictionary.h"
-
-@implementation NSDictionary 
-
-- copyWithZone:(NSZone *)zone
-{
-    return [super copy];
-}
-
-- (void)dealloc
-{
-    [super free];
-}
-
-//+ allocWithZone:(NSZone *)zone;
-//+ dictionary;
-//+ dictionaryWithObjects:(id *)objects forKeys:(NSString **)keys count:(unsigned)count;
-//+ dictionaryWithObjects:(NSArray *)objects forKeys:(NSArray *)keys;
-//- initWithObjects:(id *)objects forKeys:(NSString **)keys count:(unsigned)count;
-//- initWithDictionary:(NSDictionary *)otherDictionary;
-//- initWithContentsOfFile:(NSString *)path;
-
-//- (unsigned)count;
-//- objectForKey:(NSString *)aKey;
-//- (NSEnumerator *)keyEnumerator;
-//- (BOOL)isEqualToDictionary:(NSDictionary *)other;
-//- (NSString *)description;
-//- (NSString *)descriptionWithIndent:(unsigned)level;
-//- (NSArray *)allKeys;
-//- (NSArray *)allValues;
-//- (NSArray *)allKeysForObject:anObject;
-//- (BOOL)writeToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile;
-//- (NSEnumerator *)objectEnumerator;
-
-@end
-
-@implementation NSMutableDictionary
-
-//+ allocWithZone:(NSZone *)zone;
-//+ dictionaryWithCapacity:(unsigned)numItems;
-//- initWithCapacity:(unsigned)numItems;
-
-//- (void)setObject:anObject forKey:(NSString *)aKey;
-//- (void)removeObjectForKey:(NSString *)aKey;
-//- (void)removeAllObjects;
-//- (void)removeObjectsForKeys:(NSArray *)keyArray;
-//- (void)addEntriesFromDictionary:(NSDictionary *)otherDictionary;
-
-@end
-
