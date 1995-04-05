6d7cf363353ba511287c35557ba7b0122f8cd496
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-05 20:26:38 +0000

    Added header comment.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@288 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/NSUtilities.h
+++ Headers/gnustep/base/NSUtilities.h
@@ -1,3 +1,26 @@
+/* Utilities for GNUStep
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: March 1995
+
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #ifndef __NSUtilties_h_OBJECTS_INCLUDE
 #define __NSUtilties_h_OBJECTS_INCLUDE
 
@@ -7,4 +30,4 @@
 - (id) nextObject;
 @end
 
-endif /* __NSUtilties_h_OBJECTS_INCLUDE */
+#endif /* __NSUtilties_h_OBJECTS_INCLUDE */
