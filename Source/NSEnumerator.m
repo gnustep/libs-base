da85fc60b7165ab8dfc9dcf0ae1c6117ff4e2402
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-09 01:28:47 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@325 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/NSEnumerator.m
@@ -0,0 +1,35 @@
+/* NSEnumerator abstrace class for GNUStep
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: March 1995
+
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <objects/stdobjects.h>
+#include <foundation/NSUtilities.h>
+
+@implementation NSEnumerator
+
+- (id) nextObject
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
+
+@end
