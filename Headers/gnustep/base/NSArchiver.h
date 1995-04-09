a6096bca8f89548939b0e0d4f8187067bd342eed
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-09 02:42:37 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@339 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/NSArchiver.h
@@ -0,0 +1,51 @@
+/* Interface for NSArchiver for GNUStep
+   Copyright (C) 1994 NeXT Computer, Inc.
+   
+   This file is part of the GNU Objective C Class Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __NSArchiver_h_OBJECTS_INCLUDE
+#define __NSArchiver_h_OBJECTS_INCLUDE
+
+#include <foundation/NSCoder.h>
+
+@class NSMutableData, NSData, NSString;
+
+@interface NSArchiver : NSCoder
+
+// Initializing an archiver
+
+- (id) initForWritingWithMutableData: (NSMutableData*)mdata;
+
+// Archiving Data
+
++ (NSData*) archivedDataWithRootObject: (id)rootObject
++ (BOOL) archiveRootObject: (id)rootObject toFile: (NSString*)path;
+
+// Getting data from the archiver
+
+- (NSMutableData*) archiverData;
+
+// Substituting Classes
+
++ (NSString*) classNameEncodedForTrueClassName: (NSString*)trueName;
+- (void) enocdeClassName: (NSString*)trueName
+   intoClassName: (NSSTring*)inArchiveName;
+
+@end
+
+#endif	/* __NSArchiver_h_OBJECTS_INCLUDE */
