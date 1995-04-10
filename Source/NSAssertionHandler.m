730b7fffe3ece51639c241837464dac6e3f6e4f8
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-10 00:34:30 +0000

    Fix copyright date and indentation.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@345 72102866-910b-0410-8b05-ffd578937521

--- Source/NSAssertionHandler.m
+++ Source/NSAssertionHandler.m
@@ -1,11 +1,11 @@
 /* NSAssertionHandler - Object encapsulation of assertions
-   Copyright (C) 1993,1994 Free Software Foundation, Inc.
-
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   
    Written by:  Adam Fedor <fedor@boulder.colorado.edu>
    Date: Apr 1995
-
+   
    This file is part of the GNU Objective C Class Library.
-
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
@@ -15,11 +15,11 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
-
+   
    You should have received a copy of the GNU Library General Public
    License along with this library; if not, write to the Free
    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+   */
 #include <foundation/NSException.h>
 #include <foundation/NSString.h>
 
@@ -27,55 +27,55 @@
 
 + (NSAssertionHandler *)currentHandler
 {
-    // FIXME: current handler should come from current thread dictionary
-    static NSAssertionHandler *only_one = nil;
+  // FIXME: current handler should come from current thread dictionary;
+  static NSAssertionHandler *only_one = nil;
     
-    if (!only_one)
-    	only_one = [NSAssertionHandler new];
-    return only_one;
+  if (!only_one)
+    only_one = [NSAssertionHandler new];
+  return only_one;
 }
 
 - (void)handleFailureInFunction:(NSString *)functionName 
-	file:(NSString *)fileName 
-	lineNumber:(int)line 
-	description:(NSString *)format,...
+   file:(NSString *)fileName 
+   lineNumber:(int)line 
+   description:(NSString *)format,...
 {
-    va_list ap;
+  va_list ap;
 
-    va_start(ap, format);
-    // FIXME: should be NSLog
-    fprintf(stderr, "Assertion failed in %s, file %s:%d. ",
-    	[functionName cString], [fileName cString], line);
-    vfprintf(stderr, [format cString], ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
+  va_start(ap, format);
+  // FIXME: should be NSLog;
+  fprintf(stderr, "Assertion failed in %s, file %s:%d. ",
+	  [functionName cString], [fileName cString], line);
+  vfprintf(stderr, [format cString], ap);
+  fprintf(stderr, "\n");
+  va_end(ap);
     
-    [NSException raise:NSInternalInconsistencyException
-    	format:@"Assertion failed in %s", [functionName cString]];
-    /* NOT REACHED */
+  [NSException raise:NSInternalInconsistencyException
+	       format:@"Assertion failed in %s", [functionName cString]];
+  /* NOT REACHED */
 }
 
 - (void)handleFailureInMethod:(SEL)aSelector 
-	object:object 
-	file:(NSString *)fileName 
-	lineNumber:(int)line 
-	description:(NSString *)format,...
+   object:object 
+   file:(NSString *)fileName 
+   lineNumber:(int)line 
+   description:(NSString *)format,...
 {
-    va_list ap;
+  va_list ap;
 
-    va_start(ap, format);
-    // FIXME: should be NSLog
-    fprintf(stderr, "Assertion failed in %s, method %s, file %s:%d. ",
-    	object_get_class_name(object), sel_get_name(aSelector),
-	[fileName cString], line);
-    vfprintf(stderr, [format cString], ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
+  va_start(ap, format);
+  // FIXME: should be NSLog;
+  fprintf(stderr, "Assertion failed in %s, method %s, file %s:%d. ",
+	  object_get_class_name(object), sel_get_name(aSelector),
+	  [fileName cString], line);
+  vfprintf(stderr, [format cString], ap);
+  fprintf(stderr, "\n");
+  va_end(ap);
     
-    [NSException raise:NSInternalInconsistencyException
-    	format:@"Assertion failed in %s, method %s",
-	    	object_get_class_name(object), sel_get_name(aSelector)];
-    /* NOT REACHED */
+  [NSException raise:NSInternalInconsistencyException
+	       format:@"Assertion failed in %s, method %s",
+	       object_get_class_name(object), sel_get_name(aSelector)];
+  /* NOT REACHED */
 }
 
 @end
