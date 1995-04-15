2a6015020fb92f1f42413de5e84f08236f9711e0
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-04-15 19:39:49 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@361 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/NSGArchiver.h
@@ -0,0 +1,56 @@
+/* Interface to concrete implementation of NSArchiver
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: April 1995
+   
+   This file is part of the GNU Objective C Class Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */
+
+#ifndef __NSGArchiver_h_OBJECTS_INCLUDE
+#define __NSGArchiver_h_OBJECTS_INCLUDE
+
+#include <objects/stdobjects.h>
+#include <foundation/NSArchiver.h>
+#include <objects/Stream.h>
+#include <objects/Dictionary.h>
+#include <objects/Stack.h>
+#include <objects/Coding.h>
+
+@interface NSGArchiver : NSArchiver
+{
+  /* For now, these must match the instance variables in foundation/NSGCoder.h.
+     This will change. */
+  int format_version;
+  int concrete_format_version;
+  Stream *stream;
+  BOOL is_decoding;
+  BOOL doing_root_object;
+  Dictionary *object_table;	     /* read/written objects */
+  Dictionary *const_ptr_table;       /* read/written const *'s */
+  Stack *root_object_tables;         /* Stack of Dicts for interconnt'd objs */
+  Stack *forward_object_tables;      /* Stack of Dictionaries for frwd refs */
+
+  NSZone *object_zone;
+}
+
+@end
+
+@interface NSGArchiver (GNU) <Encoding>
+@end
+
+#endif /* __NSGArchiver_h_OBJECTS_INCLUDE */
---
+++ Source/NSUnarchiver.m
@@ -0,0 +1,108 @@
+/* Implementation of NSUnrchiver for GNUStep
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Date: March 1995
+   
+   This file is part of the GNU Objective C Class Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#include <foundation/NSUnarchiver.h>
+
+@implementation NSUnarchiver 
+
+// Initializing an unarchiver
+
+- (id) initForReadingWithData: (NSData*)data
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
+
+// Decoding objects
+
++ (id) unarchiveObjectWithData: (NSData*)data
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
+
++ (id) unarchiveObjectWithFile: (NSString*)path
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
+
+- (void) decodeArrayOfObjCType: (const char*)type 
+			 count: (unsigned int)count
+			    at: (void*)array
+{
+  [self notImplemented:_cmd];
+}
+
+
+// Managing
+
+- (BOOL) isAtEnd
+{
+  [self notImplemented:_cmd];
+  return NO;
+}
+
+- (NSZone*) objectZone
+{
+  [self notImplemented:_cmd];
+  return NULL;
+}
+
+- (void) setObjectZone: (NSZone*)zone
+{
+  [self notImplemented:_cmd];
+}
+
+- (unsigned int) systermVersion
+{
+  [self notImplemented:_cmd];
+  return 0;
+}
+
+// Substituting Classes
+
++ (NSString*) classNameDecodedForArchiveClassName: (NSString*)nameInArchive
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
++ (void) decodeClassName: (NSString*)nameInArchive
+	     asClassName: (NSString*)trueName
+{
+  [self notImplemented:_cmd];
+}
+
+- (NSString*) classNameDecodedForArchiveClassName: (NSString*)nameInArchive
+{
+  [self notImplemented:_cmd];
+  return nil;
+}
+
+- (void) decodeClassName: (NSString*)nameInArchive 
+	     asClassName: (NSString*)trueName
+{
+  [self notImplemented:_cmd];
+}
+
+@end
