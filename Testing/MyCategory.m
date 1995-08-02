1cdc3146c065aa7ed7bb38cbbb04490bb28bb86a
Andrew McCallum <mccallum@gnu.org>
1995-08-02 18:51:42 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@521 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/MyCategory.m
@@ -0,0 +1,20 @@
+/* Test Category for NSBundle.
+   Copyright (C) 1993,1994,1995 Free Software Foundation, Inc.
+
+   Written by:  Adam Fedor <fedor@boulder.colorado.edu>
+   Date: Jul 1995
+
+   This file is part of the GNU Objective C Class Library.
+
+*/
+#include "MyCategory.h"
+
+@implementation NSObject(MyCategory)
+
+- printMyName
+{
+	printf("Class %s had MyCategory added to it\n", [self name]);
+	return self;
+}
+
+@end
