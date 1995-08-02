826de26482904875adadcf244a891a4bc289aa8b
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1995-08-02 15:37:10 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@508 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/NSUser.h
@@ -0,0 +1,34 @@
+/* Interface to NSUser functions for GNUStep
+   Copyright (C) 1995 Free Software Foundation, Inc.
+
+   Written by:  Martin Michlmayer 
+   Date: 1995
+ 
+   This file is part of the GNU Objective-C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   See NSUser.c for additional information. */
+
+
+#ifndef __NSUser_h_INCLUDE_GNU
+#define __NSUser_h_INCLUDE_GNU
+
+extern char * NSUserName (void);
+extern char * NSHomeDirectory (void);
+extern char * NSDirectoryForUser (char * userName);
+
+#endif /* __NSUser_h_INCLUDE_GNU */
+
