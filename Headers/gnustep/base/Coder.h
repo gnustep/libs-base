8718b43b7a9c21e8d84215a732f051faafa87213
Andrew McCallum <mccallum@gnu.org>
1995-09-08 22:49:36 +0000

    Fix typo.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@601 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/Coder.h
+++ Headers/gnustep/base/Coder.h
@@ -30,6 +30,7 @@
 @class Stream;
 @class Dictionary;
 @class Stack;
+@class Set;
 
 @interface Coder : NSObject <Encoding, Decoding>
 {
@@ -42,7 +43,7 @@
   Dictionary *const_ptr_table;       /* read/written const *'s */
   Stack *root_object_tables;         /* Stack of Dicts for interconnt'd objs */
   Stack *forward_object_tables;      /* Stack of Dictionaries for frwd refs */
-  Dictionary *in_progress_table;     /* objects started r/w, but !finished */
+  Set *in_progress_table;     /* objects started r/w, but !finished */
 }
 
 + (void) setDefaultStreamClass: sc;
