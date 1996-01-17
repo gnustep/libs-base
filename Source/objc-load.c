2103ebe3a444cd514d3ed4b4c77e7629b480657b
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-01-17 00:33:08 +0000

    Comment indentation fix.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@734 72102866-910b-0410-8b05-ffd578937521

--- Source/objc-load.c
+++ Source/objc-load.c
@@ -1,24 +1,25 @@
-/*
-    objc-load - Dynamically load in Obj-C modules (Classes, Categories)
+/* objc-load - Dynamically load in Obj-C modules (Classes, Categories)
 
-    Written by: Adam Fedor, Pedja Bogdanovich
+   Copyright (C) 1995 Free Software Foundation, Inc.
+   
+   Written by:  Adam Fedor, Pedja Bogdanovich
+   
+   This file is part of the GNU Objective C Class Library.
 
-    This file is part of the GNU Objective C Class Library.
- 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-    
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-    
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-    */ 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
 
 /*
     CAVEATS:
