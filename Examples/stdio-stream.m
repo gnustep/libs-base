d3a426f8658b1c65acac21d292b780354b67e1c8
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-01-27 00:53:47 +0000

    (main): Use new coding scheme and @"" strings properly.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@871 72102866-910b-0410-8b05-ffd578937521

--- Examples/stdio-stream.m
+++ Examples/stdio-stream.m
@@ -16,16 +16,16 @@ int main()
   char *cp = NULL;
 
   stream = [[StdioStream alloc] 
-	    initWithFilename:"./stdio-stream.txt"
+	    initWithFilename: @"./stdio-stream.txt"
 	    fmode:"w"];
-  [stream writeFormat:"testing %d %u %f %f 0x%x \"cow\"\n", 
+  [stream writeFormat: @"testing %d %u %f %f 0x%x \"cow\"\n", 
 	  1234, 55, 3.14159, 1.23456789, 0xfeedface];
   [stream release];
 
   stream = [[StdioStream alloc] 
-	    initWithFilename:"./stdio-stream.txt"
+	    initWithFilename: @"./stdio-stream.txt"
 	    fmode:"r"];
-  count = [stream readFormat:"testing %d %u %f %lf 0x%x \"%a[^\"]\"\n", 
+  count = [stream readFormat: @"testing %d %u %f %lf 0x%x \"%a[^\"]\"\n", 
 	  &i, &u, &f, &d, &ux, &cp];
   uc = (unsigned char) ux;
   [stream release];
