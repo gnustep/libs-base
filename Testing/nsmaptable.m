a05d07ed54473ad037c06c6bb8aeb22fa20a2ca7
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-02-22 16:14:17 +0000

    New file.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@974 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/nsmaptable.m
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <Foundation/NSMapTable.h>
+#include <Foundation/NSValue.h>
+
+int main ()
+{
+  NSMapTable *mt;
+  NSMapEnumerator me;
+  int i;
+  void *k;
+  void *v;
+  id o;
+
+  /* Test with ints */
+
+  mt = NSCreateMapTable (NSIntMapKeyCallBacks,
+			 NSIntMapValueCallBacks,
+			 0);
+
+  for (i = 0; i < 16; i++)
+    NSMapInsert (mt, (void*)i, (void*)(i*2));
+
+  printf ("value for key %d is %d\n",
+	  3, (int)NSMapGet (mt, (void*)3));
+  NSMapRemove (mt, (void*)3);
+  printf ("after removing: value for key %d is %d\n",
+	  3, (int)NSMapGet (mt, (void*)3));
+
+  me = NSEnumerateMapTable (mt);
+  while (NSNextMapEnumeratorPair (&me, &k, &v))
+    printf ("(%d,%d) ", (int)k, (int)v);
+  printf ("\n");
+
+  NSFreeMapTable (mt);
+
+
+  /* Test with NSNumber objects */
+
+  mt = NSCreateMapTable (NSObjectMapKeyCallBacks,
+			 NSObjectMapValueCallBacks,
+			 0);
+
+  for (i = 0; i < 16; i++)
+    NSMapInsert (mt, 
+		 [NSNumber numberWithInt: i], 
+		 [NSNumber numberWithInt: i*i]);
+
+  o = [NSNumber numberWithInt: 3];
+  printf ("value for key %s is %s\n",
+	  [[o description] cString],
+	  [[(id)NSMapGet (mt, o) description] cString]);
+  NSMapRemove (mt, o);
+  printf ("after removing: value for key %s is %s\n",
+	  [[o description] cString],
+	  [[(id)NSMapGet (mt, o) description] cString]);
+
+  me = NSEnumerateMapTable (mt);
+  while (NSNextMapEnumeratorPair (&me, &k, &v))
+    printf ("(%d,%d) ", [(id)k intValue], [(id)v intValue]);
+  printf ("\n");
+
+  NSFreeMapTable (mt);
+
+  exit (0);
+}
