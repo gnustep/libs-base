e8402c57f0c5ed513bd8953985402abe49d95668
Andrew McCallum <mccallum@gnu.org>
1996-02-22 16:14:17 +0000

    New file.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@974 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/nshashtable.m
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <Foundation/NSHashTable.h>
+#include <Foundation/NSValue.h>
+
+int main ()
+{
+  NSHashTable *ht;
+  NSHashEnumerator he;
+  int i;
+  void *v;
+
+  /* Test with ints */
+
+  ht = NSCreateHashTable (NSIntHashCallBacks, 0);
+
+  for (i = 0; i < 16; i++)
+    NSHashInsert (ht, (void*)i);
+
+  NSHashRemove (ht, (void*)3);
+
+  he = NSEnumerateHashTable (ht);
+  while ((v = NSNextHashEnumeratorItem (&he)))
+    printf ("(%d) ", (int)v);
+  printf ("\n");
+
+  NSFreeHashTable (ht);
+
+
+#if 0
+  /* Test with NSNumber objects */
+
+  mt = NSCreateHashTable (NSObjectHashKeyCallBacks,
+			 NSObjectHashValueCallBacks,
+			 0);
+
+  for (i = 0; i < 16; i++)
+    NSHashInsert (mt, 
+		 [NSNumber numberWithInt: i], 
+		 [NSNumber numberWithInt: i*i]);
+
+  o = [NSNumber numberWithInt: 3];
+  printf ("value for key %s is %s\n",
+	  [[o description] cString],
+	  [[(id)NSHashGet (mt, o) description] cString]);
+  NSHashRemove (mt, o);
+  printf ("after removing: value for key %s is %s\n",
+	  [[o description] cString],
+	  [[(id)NSHashGet (mt, o) description] cString]);
+
+  me = NSEnumerateHashTable (mt);
+  while (NSNextHashEnumeratorPair (&me, &k, &v))
+    printf ("(%d,%d) ", [(id)k intValue], [(id)v intValue]);
+  printf ("\n");
+
+  NSFreeHashTable (mt);
+#endif
+
+  exit (0);
+}
