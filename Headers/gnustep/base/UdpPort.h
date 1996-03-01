b28e7c8edebebd05dbe1950d052717c5b6d451f2
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-03-01 16:06:57 +0000

    New file.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1022 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/UdpPort.h
@@ -0,0 +1,59 @@
+/* Interface for socket-based port object for use with Connection
+   Copyright (C) 1994, 1996 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Created: July 1994
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __SocketPort_h_INCLUDE_GNU
+#define __SocketPort_h_INCLUDE_GNU
+
+#include <objects/stdobjects.h>
+#include <objects/Port.h>
+#include <sys/types.h>
+#ifndef WIN32
+# include <sys/socket.h>
+# include <netinet/in.h>
+#endif /* !WIN32 */
+
+@interface UdpInPort : InPort
+{
+  int _socket;
+  struct sockaddr_in _address;
+}
+
+- (int) portNumber;
+- (int) socket;
+
+@end
+
+@interface UdpOutPort : OutPort
+{
+  struct sockaddr_in _address;
+}
+
+- (int) portNumber;
+- (id <String>) hostname;
+
+@end
+
+@interface UdpPacket : Packet
+@end
+
+#endif /* __SocketPort_h_INCLUDE_GNU */
---
+++ Source/objects/TcpPort.h
@@ -0,0 +1,76 @@
+/* Interface for stream based on TCP sockets
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Created: February 1996
+   
+   This file is part of the GNU Objective C Class Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */ 
+
+#ifndef __TcpPort_h__OBJECTS_INCLUDE
+#define __TcpPort_h__OBJECTS_INCLUDE
+
+#include <objects/stdobjects.h>
+#include <objects/Port.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <Foundation/NSMapTable.h>
+
+@interface TcpPacket : Packet
+@end
+
+@interface TcpInPort : InPort
+{
+  int _socket;
+  struct sockaddr_in _address;
+  fd_set active_fd_set;
+  NSMapTable *client_sock_2_out_port;
+  NSMapTable *client_sock_2_packet;
+}
+
++ newForReceivingFromPortNumber: (unsigned short)n;
++ newForReceivingFromRegisteredName: (id <String>)name;
+
+/* Get a packet from the net and return it.  If no packet is received 
+   within MILLISECONDS, then return nil.  The caller is responsible 
+   for releasing the packet. */
+- receivePacketWithTimeout: (int)milliseconds;
+
+- (id <Collecting>) connectedOutPorts;
+
+- (void) checkConnection;
+
+@end
+
+@interface TcpOutPort : OutPort
+{
+  int _socket;
+  struct sockaddr_in _address;
+  id connected_in_port;
+}
+
++ newForSendingToPortNumber: (unsigned short)n 
+		     onHost: (id <String>)hostname;
++ newForSendingToRegisteredName: (id <String>)name 
+                         onHost: (id <String>)hostname;
+- (BOOL) sendPacket: packet withTimeout: (int)milliseconds;
+
+@end
+
+#endif /* __TcpPort_h__OBJECTS_INCLUDE */
+
