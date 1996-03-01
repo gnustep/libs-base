10fd5300fb2e7f8c68bb671c411dbf3452da59d9
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-03-01 16:06:03 +0000

    Overhauled for new Port heirarchy.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1021 72102866-910b-0410-8b05-ffd578937521

--- Source/Port.m
+++ Source/Port.m
@@ -2,7 +2,7 @@
    Copyright (C) 1994, 1995, 1996 Free Software Foundation, Inc.
    
    Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
-   Date: July 1994
+   Created: July 1994
    
    This file is part of the GNU Objective C Class Library.
 
@@ -26,75 +26,133 @@
 
 @implementation Port
 
-+ newRegisteredPortWithName: (id <String>)n
+/* This is the designated initializer. */
+- init
+{
+  [super init];
+  is_valid = YES;
+  retain_count = 0;
+  return self;
+}
+
+- retain
+{
+  retain_count++;
+  return self;
+}
+
+- (oneway void) release
+{
+  if (!retain_count--)
+    [self dealloc];
+}
+
+- (unsigned) retainCount
+{
+  return retain_count;
+}
+
+- (BOOL) isValid
+{
+  return is_valid;
+}
+
+- (void) close
+{
+  [self invalidate];
+}
+
+- (void) invalidate
+{
+  is_valid = NO;
+}
+
+- (Class) packetClass
+{
+  [self subclassResponsibility: _cmd];
+  return nil;
+}
+
+- (Class) classForConnectedCoder: aRmc
+{
+  /* Make sure that Connection's always send us bycopy,
+     i.e. as our own class, not a Proxy class. */
+  return [self class];
+}
+
+- (void) encodeWithCoder: (id <Encoding>)anEncoder
+{
+  [super encodeWithCoder: anEncoder];
+  /* xxx What else? */
+}
+
+- initWithCoder: (id <Decoding>)coder
+{
+  self = [super initWithCoder: coder];
+  /* xxx What else? */
+  return self;
+}
+
+@end
+
+
+@implementation InPort
+
++ newForReceivingFromRegisteredName: (id <String>)name
 {
   [self subclassResponsibility:_cmd];
   return nil;
 }
 
-+ newPortFromRegisterWithName: (id <String>)n onHost: (id <String>)host
++ newForReceiving
+{
+  return [self newForReceivingFromRegisteredName: nil];
+}
+
+- receivePacketWithTimeout: (int)milliseconds
 {
   [self subclassResponsibility:_cmd];
   return nil;
 }
 
-+ newPort
+@end
+
+
+@implementation OutPort
+
++ newForSendingToRegisteredName: (id <String>)name 
+                         onHost: (id <String>)hostname
 {
   [self subclassResponsibility:_cmd];
   return nil;
 }
 
-/* These sending and receiving interfaces will change */
-
-- (int) sendPacket: (const char *)b length: (int)l
-   toPort: (Port*) remote
-   timeout: (int) milliseconds
-{
-  [self subclassResponsibility:_cmd];
-  return 0;
-}
-
-- (int) sendPacket: (const char *)b length: (int)l
-   toPort: (Port*) remote
-{
-  return [self sendPacket:b length:l toPort:remote timeout:-1];
-}
-
-- (int) receivePacket: (char*)b length: (int)l
-   fromPort: (Port**) remote
-   timeout: (int) milliseconds
-{
-  [self subclassResponsibility:_cmd];
-  return 0;
-}
-
-- (int) receivePacket: (char*)b length: (int)l
-   fromPort: (Port**) remote
-{
-  return [self receivePacket:b length:l fromPort:remote timeout:-1];
-}
-
-- (BOOL) isSoft
-{
-  [self subclassResponsibility:_cmd];
-  return YES;
-}
-
-- (BOOL) isEqual: anotherPort
+- (BOOL) sendPacket: packet withTimeout: (int)milliseconds
 {
   [self subclassResponsibility:_cmd];
   return NO;
 }
 
-- (unsigned) hash
+@end
+
+
+@implementation Packet
+
+/* xxx There should be a designated initializer for the Packet class.
+   Currently some subclasses and users, bypass this by calling
+   MemoryStream initializers. */
+
+- initForSendingWithCapacity: (unsigned)c
+   replyPort: p
 {
-  [self subclassResponsibility:_cmd];
-  return 0;
+  [super initWithCapacity: c];
+  reply_port = p;
+  return self;
 }
 
-- (void) encodeWithCoder: (id <Encoding>)anEncoder
+- replyPort
 {
-  [super encodeWithCoder:anEncoder];
+  return reply_port;
 }
 
 @end
