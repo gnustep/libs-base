5959e1d40b54ba8359e54b58eaefc3577756d4d0
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-03-06 14:44:21 +0000

    Test the reply port mechanism; look for messages coming back from the server.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1072 72102866-910b-0410-8b05-ffd578937521

--- Testing/tcpport-client.m
+++ Testing/tcpport-client.m
@@ -1,25 +1,45 @@
 #include <stdio.h>
 #include <objects/TcpPort.h>
 
-int main ()
+int main (int argc, char *argv[])
 {
-  id port;
+  id out_port;
+  id in_port;
   id packet;
   int i;
 
-  port = [TcpOutPort newForSendingToRegisteredName: @"tcpport-test"
-		     onHost: @"localhost"];
+  if (argc > 1)
+    out_port = [TcpOutPort newForSendingToRegisteredName: 
+			     [NSString stringWithCString: argv[1]]
+			   onHost: @"localhost"];
+  else
+    out_port = [TcpOutPort newForSendingToRegisteredName: @"tcpport-test"
+			   onHost: @"localhost"];
+
+  in_port = [TcpInPort newForReceiving];
   
-  for (i = 0; i < 5; i++)
+  for (i = 0; i < 10; i++)
     {
       packet = [[TcpPacket alloc] initForSendingWithCapacity: 100
-				  replyPort: nil];
-      [packet writeFormat: @"Here is message number %d\n", i];
-      [port sendPacket: packet withTimeout: 20 * 1000];
+				  replyPort: in_port];
+      [packet writeFormat: @"Here is message number %d", i];
+      [out_port sendPacket: packet withTimeout: 20 * 1000];
       [packet release];
+
+      packet = [in_port receivePacketWithTimeout: 1000];
+      if (packet)
+	{
+	  fprintf (stdout, "received >");
+	  fwrite ([packet streamBuffer] + [packet streamBufferPrefix],
+		  [packet streamEofPosition], 1, stdout);
+	  fprintf (stdout, "<\n");
+	  [packet release];
+	}
+
+      sleep (2);
     }
 
-  [port close];
+  [out_port close];
 
   exit (0);
 }
