e8d8fc95881d0e01bba739670c16f13a3733ef59
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-03-12 15:39:13 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1114 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/base/RunLoop.h
@@ -0,0 +1,46 @@
+#ifndef __RunLoop_h_OBJECTS_INCLUDE
+#define __RunLoop_h_OBJECTS_INCLUDE
+
+#include <objects/stdobjects.h>
+#include <objects/NotificationDispatcher.h>
+#include <objects/Bag.h>
+#include <objects/Heap.h>
+#include <Foundation/NSMapTable.h>
+#include <sys/types.h>
+
+@interface RunLoop : NSObject
+{
+  fd_set _fds;
+  NSMapTable *_fd_2_object;
+  Bag *_fd_objects;
+  Heap *_timers;
+  NotificationDispatcher *_dispatcher;
+  Array *_queues;
+}
+
+- (void) addFileDescriptor: (int)fd
+                invocation: invocation
+		   forMode: (id <String>)mode;
+- (void) removeFileDescriptor: (int)fd 
+		      forMode: (id <String>)mode;
+
+- (void) addTimer: timer forMode: (id <String>)mode;
+
+- limitDateForMode: (id <String>)mode;
+- (void) acceptInputForMode: (id <String>)mode
+                 beforeDate: date;
+
+- (void) run;
+- (void) runUntilDate: limit_date;
+- (BOOL) runMode: (id <String>)mode 
+      beforeDate: limit_date;
+
++ (void) run;
++ (void) runUntilDate: date;
+
++ currentInstance;
+
+@end
+
+
+#endif /* __RunLoop_h_OBJECTS_INCLUDE */
---
+++ Source/NSTimer.m
@@ -0,0 +1,154 @@
+/* Implementation of NSTimer for GNUstep
+   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Created: March 1996
+   
+   This file is part of the GNU Objective C Class Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */
+
+#include <Foundation/NSTimer.h>
+#include <Foundation/NSDate.h>
+#include <Foundation/NSException.h>
+#include <objects/RunLoop.h>
+#include <objects/Invocation.h>
+#include <nan.h>
+
+@implementation NSTimer
+
+/* This is the designated initializer. */
+- initWithTimeInterval: (NSTimeInterval)seconds
+    targetOrInvocation: t
+	      selector: (SEL)sel
+              userInfo: info
+               repeats: (BOOL)f
+{
+  [super init];
+  _interval = seconds;
+  _fire_date = [[NSDate alloc] initWithTimeIntervalSinceNow: seconds];
+  _retain_count = 0;
+  _is_valid = YES;
+  _target = t;
+  _selector = sel;
+  _info = info;
+  _repeats = f;
+  return self;
+}
+
+- (void) dealloc
+{
+  [_fire_date release];
+  [super dealloc];
+}
+
++ timerWithTimeInterval: (NSTimeInterval)ti
+	     invocation: invocation
+		repeats: (BOOL)f
+{
+  return [[[self alloc] initWithTimeInterval: ti
+			targetOrInvocation: invocation
+			selector: NULL
+			userInfo: nil
+			repeats: f]
+	   autorelease];
+}
+
++ timerWithTimeInterval: (NSTimeInterval)ti
+		 target: object
+	       selector: (SEL)selector
+               userInfo: info
+	        repeats: (BOOL)f
+{
+  return [[[self alloc] initWithTimeInterval: ti
+			targetOrInvocation: object
+			selector: selector
+			userInfo: info
+			repeats: f]
+	   autorelease];
+}
+
++ scheduledTimerWithTimeInterval: (NSTimeInterval)ti
+		      invocation: invocation
+			 repeats: (BOOL)f
+{
+  id t = [self timerWithTimeInterval: ti
+	       invocation: invocation
+	       repeats: f];
+  [[RunLoop currentInstance] addTimer: t forMode: nil];
+  return t;
+}
+
++ scheduledTimerWithTimeInterval: (NSTimeInterval)ti
+			  target: object
+			selector: (SEL)selector
+                        userInfo: info
+			 repeats: (BOOL)f
+{
+  id t = [self timerWithTimeInterval: ti
+	       target: object
+	       selector: selector
+	       userInfo: info
+	       repeats: f];
+  [[RunLoop currentInstance] addTimer: t forMode: nil];
+  return t;
+}
+
+
+- (void) fire
+{
+  if (_selector)
+    [_target perform: _selector withObject: self];
+  else
+    [_target invoke];
+
+  if (!_repeats)
+    [self invalidate];
+  else if (_is_valid)
+    {
+      NSTimeInterval ti = [_fire_date timeIntervalSinceReferenceDate];
+      NSTimeInterval now = [NSDate timeIntervalSinceReferenceDate];
+      assert (now < 0.0);
+      while (ti < now)		// xxx remove this
+	ti += _interval;
+      [_fire_date release];
+      assert (ti != NAN);
+      _fire_date = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate: ti];
+    }
+}
+
+- (void) invalidate
+{
+  assert (_is_valid);
+  _is_valid = NO;
+}
+
+- (BOOL) isValid
+{
+  return _is_valid;
+}
+
+- fireDate
+{
+  return _fire_date;
+}
+
+- userInfo
+{
+  return _info;
+}
+
+@end
