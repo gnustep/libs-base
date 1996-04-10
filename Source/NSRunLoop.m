240f96946f2541fa38c19638dc6ecfa1300f4b22
Andrew McCallum <mccallum@gnu.org>
1996-04-10 18:20:36 +0000

    New file.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1341 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/NSRunLoop.m
@@ -0,0 +1,49 @@
+/* NSRunLoop class for GNUstep
+   Copyright (C) 1995, 1996 Free Software Foundation, Inc.
+   
+   Written by:  R. Andrew McCallum <mccallum@gnu.ai.mit.edu>
+   Created: April 1995
+   
+   This file is part of the GNU Objective C Class Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */
+
+#include <objects/stdobjects.h>
+#include <Foundation/NSRunLoop.h>
+#include <objects/RunLoop.h>
+#include <objects/Connection.h>
+
+@implementation NSRunLoop
+
+/* This class is almost fully implemented in GNU's RunLoop. */
+
++ (void) initialize
+{
+  if (self == [NSRunLoop class])
+    behavior_class_add_class (self, [RunLoop class]);
+}
+
++ (NSRunLoop*) currentRunLoop
+{
+  return [self currentInstance];
+}
+
+- (BOOL) runMode: (NSString*)mode beforeDate: (NSDate*)limit_date
+{
+  return [(id)self runOnceBeforeDate: limit_date forMode: mode];
+}
+
+@end
