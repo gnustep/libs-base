c62fe3196a80f2fafc4ebe8298364c305bd7b969
Andrew McCallum <mccallum@gnu.org>
1996-04-16 21:29:40 +0000

    New file.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1372 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/heap.m
@@ -0,0 +1,59 @@
+/* Test Heap class. */
+
+#include <objects/objects.h>
+#include <Foundation/NSValue.h>
+#include <Foundation/NSAutoreleasePool.h>
+
+#define N 20
+
+#if (sun && __svr4__) || defined(__hpux) || defined(_SEQUENT_)
+long lrand48();
+#define random lrand48
+#else
+#if WIN32
+#define random rand
+#else
+long random();
+#endif
+#endif
+
+int main()
+{
+  int i, s;
+  id s1, s2;
+  id arp;
+
+  Heap* heap = [Heap new];
+  Array* array = [Array new];
+
+  arp = [NSAutoreleasePool new];
+
+  for (i = 0; i < N; i++)
+    {
+      s = random () % 100;
+      [heap addObject: [NSNumber numberWithInt: s]];
+      [array addObject: [NSNumber numberWithInt: s]];
+    }
+  [array sortContents];
+
+  [heap printForDebugger];
+  [array printForDebugger];
+
+  for (i = 0; i < N; i++)
+    {
+      s1 = [heap firstObject];
+      s2 = [array firstObject];
+      [heap removeFirstObject];
+      [array removeFirstObject];
+      printf("(%d,%d) ", [s1 intValue], [s2 intValue]);
+      assert ([s1 intValue] == [s2 intValue]);
+    }
+  printf("\n");
+  
+  [heap release];
+  [array release];
+
+  [arp release];
+
+  exit(0);
+}
