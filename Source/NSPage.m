50bc8363a290cbd901eda0373c2dead74aa34bcb
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-05-31 14:23:46 +0000

    Comment change.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1598 72102866-910b-0410-8b05-ffd578937521

--- Source/NSPage.m
+++ Source/NSPage.m
@@ -1,4 +1,4 @@
-/* Implementation of page-related functions for OpenStep
+/* Implementation of page-related functions for GNUstep
    Copyright (C) 1996 Free Software Foundation, Inc.
    
    Written by:  Andrew Kachites McCallum <mccallum@gnu.ai.mit.edu>
