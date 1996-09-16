41c3b317026ad148e3cc6e22872fad6db77c1cb6
mccallum <mccallum@72102866-910b-0410-8b05-ffd578937521>
1996-09-16 20:55:02 +0000

    Include <objc/objc-api.h>.  (Reported by Richard Frith-Macdonald <richard@brainstorm.co.uk>.)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@1772 72102866-910b-0410-8b05-ffd578937521

--- Source/o_vscanf.c
+++ Source/o_vscanf.c
@@ -59,6 +59,7 @@ Cambridge, MA 02139, USA.  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <objc/objc-api.h>
 
 extern double strtod(const char *str, char **ptr);
 extern long strtol(const char *str, char** ptr, int base);
