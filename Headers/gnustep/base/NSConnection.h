ea41c9c1ee6b32a7a1cf57061e98034db7d1ce85
Andrew McCallum <mccallum@gnu.org>
1997-05-03 16:31:11 +0000

    Add copyright and fix method declaration.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@2284 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/NSConnection.h
+++ Headers/gnustep/base/NSConnection.h
@@ -1,3 +1,25 @@
+/* NSConnection - OpenStep front-end to GNU Distributed Objects
+   Copyright (C) 1997 Free Software Foundation, Inc.
+   
+   Written by:  Andrew Kachites McCallum <mccallum@gnu.ai.mit.edu>
+   Created: Mar 1997
+   
+   This file is part of the GNUstep Base Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   */
 
 @interface NSConnection : NSObject
 {
@@ -8,8 +30,9 @@
 + (NSConnection*) connectionWithRegisteredName: (NSString*)name 
   host: (NSString*)host;
 + (NSConnection*) defaultConnection;
-+ (NSDistantObject*) rootProxyForConnectionWithRegisteredName: (NSString*)name;
-
++ (NSDistantObject*) rootProxyForConnectionWithRegisteredName: (NSString*)name
+  host: (NSString*)host;
+   
 + (NSArray*) allConnections;
 - (BOOL) isValid;
 
