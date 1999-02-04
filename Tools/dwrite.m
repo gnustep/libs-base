362e54106f010858dfd28f029bca884e9b0e120d
richard <richard@72102866-910b-0410-8b05-ffd578937521>
1999-02-04 13:06:58 +0000

    Corrected copyrights
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@3650 72102866-910b-0410-8b05-ffd578937521

--- Tools/dwrite.m
+++ Tools/dwrite.m
@@ -4,21 +4,18 @@
    Written by:  Richard Frith-Macdonald <richard@brainstorm.co.uk>
    Created: October 1997
 
-   This file is part of the GNUstep Base Library.
+   This file is part of the GNUstep Project
 
    This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+    
+   You should have received a copy of the GNU General Public  
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
    */
 
 #include	<Foundation/NSArray.h>
