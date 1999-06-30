b8391975994a323b4d835867dc6609c7237dfa5f
Richard Frith-MacDonald <rfm@gnu.org>
1999-06-30 19:41:03 +0000

    Tidying
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@4498 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/Unicode.h
+++ Headers/gnustep/base/Unicode.h
@@ -37,5 +37,7 @@ unichar uni_toupper(unichar ch);
 unsigned char uni_cop(unichar u);
 BOOL uni_isnonsp(unichar u);
 unichar *uni_is_decomp(unichar u);
+int encode_strtoustr(unichar* u1,const char*s1,int size, NSStringEncoding enc);
+
 
 #endif /* __Unicode_h_OBJECTS_INCLUDE */
