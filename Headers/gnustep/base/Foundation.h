b014208d040fd79758481689b75fe140b466dcb2
fedor <fedor@72102866-910b-0410-8b05-ffd578937521>
1999-07-29 20:58:04 +0000

    Dragging updates
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@4672 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/Foundation.h
+++ Headers/gnustep/base/Foundation.h
@@ -61,6 +61,7 @@
 #include <Foundation/NSMethodSignature.h>
 #include <Foundation/NSNotification.h>
 #include <Foundation/NSNotificationQueue.h>
+#include <Foundation/NSPage.h>
 #include <Foundation/NSPathUtilities.h>
 #include <Foundation/NSPortCoder.h>
 #include <Foundation/NSPortMessage.h>
