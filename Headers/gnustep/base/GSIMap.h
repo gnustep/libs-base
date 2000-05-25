9f1d5b4c800cd53011024a0829d76d190ba19501
fedor <fedor@72102866-910b-0410-8b05-ffd578937521>
2000-05-25 21:53:37 +0000

    Remove config.h
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@6622 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/GSIMap.h
+++ Headers/gnustep/base/GSIMap.h
@@ -22,7 +22,6 @@
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA. */
 
-#include <config.h>
 #include <Foundation/NSObject.h>
 #include <Foundation/NSZone.h>
 
