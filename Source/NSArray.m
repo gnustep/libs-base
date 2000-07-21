b6af6f9aa37e2bc9a0c0a0640992edb3ad295810
Richard Frith-MacDonald <rfm@gnu.org>
2000-07-21 13:54:11 +0000

    Tidy object insertion stuff
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/freeze-0_6_6@6942 72102866-910b-0410-8b05-ffd578937521

--- Source/NSArray.m
+++ Source/NSArray.m
@@ -1327,3 +1327,130 @@ static NSString	*indentStrings[] = {
   return (*get)(array, oaiSel, --pos);
 }
 @end
+
+
+@implementation	NSArray (GNUstep)
+
+/*
+ *	The comparator function takes two items as arguments, the first is the
+ *	item to be added, the second is the item already in the array.
+ *      The function should return NSOrderedAscending if the item to be
+ *      added is 'less than' the item in the array, NSOrderedDescending
+ *      if it is greater, and NSOrderedSame if it is equal.
+ */
+- (unsigned) insertionPosition: (id)item
+		 usingFunction: (NSComparisonResult (*)(id, id))sorter
+{
+  unsigned	count = [self count];
+  unsigned	upper = count;
+  unsigned	lower = 0;
+  unsigned	index;
+  IMP		oai;
+
+  if (item == nil)
+    {
+      [NSException raise: NSGenericException
+		  format: @"Attempt to find position for nil object in array"];
+    }
+  if (sorter == 0)
+    {
+      [NSException raise: NSGenericException
+		  format: @"Attempt to find position with null comparator"];
+    }
+
+  oai = [self methodForSelector: oaiSel];
+  /*
+   *	Binary search for an item equal to the one to be inserted.
+   */
+  for (index = upper/2; upper != lower; index = lower+(upper-lower)/2)
+    {
+      NSComparisonResult comparison;
+
+      comparison = (*sorter)(item, (*oai)(self, oaiSel, index));
+      if (comparison == NSOrderedAscending)
+        {
+          upper = index;
+        }
+      else if (comparison == NSOrderedDescending)
+        {
+          lower = index + 1;
+        }
+      else
+        {
+          break;
+        }
+    }
+  /*
+   *	Now skip past any equal items so the insertion point is AFTER any
+   *	items that are equal to the new one.
+   */
+  while (index < count
+    && (*sorter)(item, (*oai)(self, oaiSel, index)) != NSOrderedAscending)
+    {
+      index++;
+    }
+  return index;
+}
+
+- (unsigned) insertionPosition: (id)item
+		 usingSelector: (SEL)comp
+{
+  unsigned	count = [self count];
+  unsigned	upper = count;
+  unsigned	lower = 0;
+  unsigned	index;
+  NSComparisonResult	(*imp)(id, SEL, id);
+  IMP		oai;
+
+  if (item == nil)
+    {
+      [NSException raise: NSGenericException
+		  format: @"Attempt to find position for nil object in array"];
+    }
+  if (comp == 0)
+    {
+      [NSException raise: NSGenericException
+		  format: @"Attempt to find position with null comparator"];
+    }
+  imp = (NSComparisonResult (*)(id, SEL, id))[item methodForSelector: comp];
+  if (imp == 0)
+    {
+      [NSException raise: NSGenericException
+		  format: @"Attempt to find position with unknown method"];
+    }
+
+  oai = [self methodForSelector: oaiSel];
+  /*
+   *	Binary search for an item equal to the one to be inserted.
+   */
+  for (index = upper/2; upper != lower; index = lower+(upper-lower)/2)
+    {
+      NSComparisonResult comparison;
+
+      comparison = (*imp)(item, comp, (*oai)(self, oaiSel, index));
+      if (comparison == NSOrderedAscending)
+        {
+          upper = index;
+        }
+      else if (comparison == NSOrderedDescending)
+        {
+          lower = index + 1;
+        }
+      else
+        {
+          break;
+        }
+    }
+  /*
+   *	Now skip past any equal items so the insertion point is AFTER any
+   *	items that are equal to the new one.
+   */
+  while (index < count
+    && (*imp)(item, comp, (*oai)(self, oaiSel, index)) != NSOrderedAscending)
+    {
+      index++;
+    }
+  return index;
+}
+@end
+
