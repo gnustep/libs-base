afcc063d929217fe19d4de237bb3f8b238e0118d
FredKiefer <FredKiefer@72102866-910b-0410-8b05-ffd578937521>
2000-09-12 23:08:09 +0000

    Make function encode_ustrtostr public
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@7486 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/base/Unicode.h
+++ Headers/gnustep/base/Unicode.h
@@ -26,18 +26,19 @@
 
 #include <Foundation/NSString.h>	/* For string encodings */
 
-unichar encode_chartouni(char c, NSStringEncoding enc);
-char encode_unitochar(unichar u, NSStringEncoding enc);
 unichar chartouni(char c);
 char unitochar(unichar u);
+unichar encode_chartouni(char c, NSStringEncoding enc);
+char encode_unitochar(unichar u, NSStringEncoding enc);
 int strtoustr(unichar * u1,const char *s1,int size);
 int ustrtostr(char *s2,unichar *u1,int size);
+int encode_strtoustr(unichar* u1,const char*s1,int size, NSStringEncoding enc);
+int encode_ustrtostr(char *s2, unichar *u1, int size, NSStringEncoding enc);
+
 unichar uni_tolower(unichar ch);
 unichar uni_toupper(unichar ch);
 unsigned char uni_cop(unichar u);
 BOOL uni_isnonsp(unichar u);
 unichar *uni_is_decomp(unichar u);
-int encode_strtoustr(unichar* u1,const char*s1,int size, NSStringEncoding enc);
-
 
 #endif /* __Unicode_h_OBJECTS_INCLUDE */
