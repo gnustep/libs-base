9f692f7c660e7fb4a90c247702e983d07901cea7
Richard Frith-MacDonald <rfm@gnu.org>
2000-09-20 13:48:49 +0000

    Crash fixed
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@7562 72102866-910b-0410-8b05-ffd578937521

--- Testing/basic.m
+++ Testing/basic.m
@@ -1,9 +1,20 @@
 #include <Foundation/NSObject.h>
 #include <stdio.h>
 
+#if 0
 int main ()
 {
   id o = [NSObject new];
   printf ("Hello from object at 0x%x\n", (unsigned)[o self]);
   exit (0);
 }
+#else
+int main (int argc, char **argv)
+{
+     NSString *string;
+
+     string = [NSString stringWithCString:argv[1]];
+
+     return 0;
+}
+#endif
