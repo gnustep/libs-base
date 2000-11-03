98184957c3cfcf01bf01eac340cb3dc2e5a7c4cc
richard <richard@72102866-910b-0410-8b05-ffd578937521>
2000-11-03 14:25:09 +0000

    Fixed dumb modification
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@8002 72102866-910b-0410-8b05-ffd578937521

--- Source/GSString.m
+++ Source/GSString.m
@@ -58,29 +58,6 @@
 @end
 
 /*
- * Private concrete string classes.
- * NB. All these concrete string classes MUST have the same initial ivar
- * layout so that we can swap between them as necessary.
- * The initial layout must also match that of NXConstantString (which is
- * determined by the compiler).
- */
-@interface GSString : NSString
-{
-  union {
-    unichar		*u;
-    unsigned char	*c;
-  } _contents;
-  unsigned int	_count;
-  struct {
-    unsigned int	wide: 1;	// 16-bit characters in string?
-    unsigned int	free: 1;	// Should free memory?
-    unsigned int	unused: 2;
-    unsigned int	hash: 28;
-  } _flags;
-}
-@end
-
-/*
  * GSCString - concrete class for strings using 8-bit character sets.
  */
 @interface GSCString : GSString
