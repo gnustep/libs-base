c005d3b7a3a479d4634aad76e78223f6e978072a
fedor <fedor@72102866-910b-0410-8b05-ffd578937521>
2002-01-02 21:18:09 +0000

    Abort with error message when no libxml
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@11958 72102866-910b-0410-8b05-ffd578937521

--- Tools/autogsdoc.m
+++ Tools/autogsdoc.m
@@ -362,6 +362,13 @@ main(int argc, char **argv, char **env)
   [NSProcessInfo initializeWithArguments: argv count: argc environment: env];
 #endif
 
+#if HAVE_LIBXML == 0
+  NSLog(@"ERROR: The GNUstep Base Library was built\n"
+@"        without an available libxml library. Autogsdoc needs the libxml\n"
+@"        library to function. Aborting");
+  exit(1);
+#endif
+
   defs = [NSUserDefaults standardUserDefaults];
   [defs registerDefaults: [NSDictionary dictionaryWithObjectsAndKeys:
     @"Untitled", @"Project",
