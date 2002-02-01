1cffc651a456cf0338a9d6441dcb968139cccff3
Adam Fedor <fedor@gnu.org>
2002-02-01 04:00:57 +0000

    Remove old source
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@12356 72102866-910b-0410-8b05-ffd578937521

--- Old/RunLoop.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RunLoop_h_GNUSTEP_BASE_INCLUDE
-#define __RunLoop_h_GNUSTEP_BASE_INCLUDE
-
-#include <Foundation/NSRunLoop.h>
-
-#define	RunLoop NSRunLoop
-#define	RunLoopDefaultMode	NSDefaultRunLoopMode
-
-#endif /* __RunLoop_h_GNUSTEP_BASE_INCLUDE */
