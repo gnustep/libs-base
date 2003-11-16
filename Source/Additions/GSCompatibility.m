2d8e1932e7a7eae3a3db5e19b7d7294f9d7ef9cc
CaS <CaS@72102866-910b-0410-8b05-ffd578937521>
2003-11-16 10:53:40 +0000

    Fixes for lazy locking on macos
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@18088 72102866-910b-0410-8b05-ffd578937521

--- Source/Additions/GSCompatibility.m
+++ Source/Additions/GSCompatibility.m
@@ -471,3 +471,23 @@ BOOL GSDebugSet(NSString *level)
 }
 
 @end
+
+@implementation	NSLock (GSCompatibility)
+/*
+ * Dummy implementation of garbage collection cleanup method called by
+ * GSLazyLock on deallocation.
+ */
+- (void) gcFinalize
+{
+}
+@end
+
+@implementation	NSRecursiveLock (GSCompatibility)
+/*
+ * Dummy implementation of garbage collection cleanup method called by
+ * GSLazyRecursiveLock on deallocation.
+ */
+- (void) gcFinalize
+{
+}
+@end
