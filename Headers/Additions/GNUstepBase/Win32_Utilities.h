26633b348bd60508209f61921f782c95ed1c3f27
Adam Fedor <fedor@gnu.org>
2005-03-03 16:04:22 +0000

    * Start on integrating Sheldon Gills NSPathUtilities patches. * Headers/Foundation/NSPathUtilities.h: Depreciate GSSystemRootDirectory, GSStandardPathPrefixes. Add GSPreferencesDirectory to NSSearchPathDirectory enum. Enumerate NSSearchPathDomainMask values. * Headers/Additions/GNUStepBase/Win32Support.h, Headers/Additions/GNUStepBase/Win32_Utilites.h: New files. * Source/GNUmakefile: Add NSPathUtilities.m * Source/NSUser.m: Move everything in here... * Source/NSPathUtilities.m: to here. * Source/win32/GNUmakefile: Add Win32Support.m, Win32_Utilities.m * Source/win32Win32Support.m, Win32_Utilities.m: New files * Testing/nspathutilites.m: New file. (Files and patches from Sheldon Gill with minor modifications).
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@20837 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/Additions/GNUstepBase/Win32_Utilities.h
@@ -0,0 +1,59 @@
+/** Win32 Utility support functions for GNUStep
+   Copyright (C) 2004 Free Software Foundation, Inc.
+
+   Written by:  Sheldon Gill <address@hidden>
+   Date: 2004
+   
+   This file is part of the GNUstep Base Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+
+    AutogsdocSource: Win32_Utilities.m
+*/
+
+#ifndef __Win32_Utilities_h_GNUSTEP_BASE_INCLUDE
+#define __Win32_Utilities_h_GNUSTEP_BASE_INCLUDE
+
+/* TODO:
+- Win32GetUserProfileDirectory()
+Find profile directory for non-current user.
+- Win32FindDirectory()
+NOT IMPLEMENTED YET!
+-Win32NSNumberFromRegistry()
+NOT IMPLEMENTED YET!
+-Win32NSDataFromRegistry()
+NOT IMPLEMENTED YET!
+*/
+
+#if defined(__WIN32__)
+/* ---- Init Functions ---- */
+void Win32_Utilities_init(void);
+void Win32_Utilities_fini(void);
+
+/* ---- Environment Functions ---- */
+NSString     *Win32NSStringFromEnvironmentVariable(const char * envVar);
+
+/* ---- Registry Functions ---- */
+HKEY          Win32OpenRegistry(DWORD hive, const char *key);
+NSString     *Win32NSStringFromRegistry(HKEY regkey, const char *regValue);
+// NSNumber     *Win32NSNumberFromRegistry(HKEY regkey, const char *regValue);
+// NSData       *Win32NSDataFromRegistry(HKEY regkey, const char *regValue);
+
+/* ---- Path Functions ---- */
+NSString     *Win32GetUserProfileDirectory(NSString *userName);
+NSString     *Win32FindDirectory( DWORD DirCLSID );
+#endif /* defined(__WIN32__) */
+
+#endif /* */
