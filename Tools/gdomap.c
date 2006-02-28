7968ee11329e98854c6f770caba0e92005b8678d
Richard Frith-MacDonald <rfm@gnu.org>
2006-02-28 12:48:16 +0000

    incluse stdint.h for uint32_t
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@22580 72102866-910b-0410-8b05-ffd578937521

--- Tools/gdomap.c
+++ Tools/gdomap.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <unistd.h>		/* for gethostname() */
 #ifndef __MINGW32__
 #include <sys/param.h>		/* for MAXHOSTNAMELEN */
