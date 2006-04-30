4ca24f23f83289879d1ce2fa8a0f7944cf101a67
David Ayers <ayers@fsfe.org>
2006-04-30 08:20:35 +0000

    * Source/NSString: Define _GNU_SOURCE to make protoype of fwprintf 	visible.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@22838 72102866-910b-0410-8b05-ffd578937521

--- Source/NSString.m
+++ Source/NSString.m
@@ -43,6 +43,11 @@
    Limited choice of default encodings.
 */
 
+/* Needed for visiblity of fwprintf prototype.  */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "config.h"
 #include <stdio.h>
 #include <string.h>
