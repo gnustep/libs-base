2aa7b2026c6367ace7b489eb3458c836b4ebf811
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2007-03-14 16:04:13 +0000

    Improve native locale integration and mswindows usability.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@24871 72102866-910b-0410-8b05-ffd578937521

--- Source/GSPrivate.h
+++ Source/GSPrivate.h
@@ -353,6 +353,13 @@ GSPrivateLoadModule(NSString *filename, FILE *errorStream,
   void (*loadCallback)(Class, struct objc_category *),
   void **header, NSString *debugFilename) GS_ATTRIB_PRIVATE;
 
+/* Get the native C-string encoding as used by locale specific code in the
+ * operating system.  This may differ from the default C-string encoding
+ * if the latter has bewen set via an environment variable.
+ */
+NSStringEncoding
+GSPrivateNativeCStringEncoding() GS_ATTRIB_PRIVATE;
+
 /* Function used by the NSRunLoop and friends for processing
  * queued notifications which should be processed at the first safe moment.
  */
