befaf43dd6cfd85216211369c78ecf416babc116
Richard Frith-MacDonald <rfm@gnu.org>
2007-09-25 16:30:53 +0000

    Minor bugfix parsing http headers
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@25502 72102866-910b-0410-8b05-ffd578937521

--- Source/Additions/GSMime.m
+++ Source/Additions/GSMime.m
@@ -2234,9 +2234,18 @@ NSDebugMLLog(@"GSMime", @"Header parsed - %@", info);
 	  if (dst > beg)
 	    {
 	      s = [NSStringClass allocWithZone: NSDefaultMallocZone()];
-	      s = [s initWithBytes: beg
-			    length: dst - beg
-			  encoding: NSASCIIStringEncoding];
+              if (flags.isHttp == 1)
+                {
+                  s = [s initWithBytes: beg
+                                length: dst - beg
+                              encoding: NSISOLatin1StringEncoding];
+                }
+              else
+                {
+                  s = [s initWithBytes: beg
+                                length: dst - beg
+                              encoding: NSASCIIStringEncoding];
+                }
 	      if (s == nil && _defaultEncoding != NSASCIIStringEncoding)
 	        {
 		  s = [NSStringClass allocWithZone: NSDefaultMallocZone()];
@@ -2347,9 +2356,18 @@ NSDebugMLLog(@"GSMime", @"Header parsed - %@", info);
   if (dst > beg)
     {
       s = [NSStringClass allocWithZone: NSDefaultMallocZone()];
-      s = [s initWithBytes: beg
-		    length: dst - beg
-		  encoding: NSASCIIStringEncoding];
+      if (flags.isHttp == 1)
+        {
+          s = [s initWithBytes: beg
+                        length: dst - beg
+                      encoding: NSISOLatin1StringEncoding];
+        }
+      else
+        {
+          s = [s initWithBytes: beg
+                        length: dst - beg
+                      encoding: NSASCIIStringEncoding];
+        }
       if (s == nil && _defaultEncoding != NSASCIIStringEncoding)
 	{
 	  s = [NSStringClass allocWithZone: NSDefaultMallocZone()];
