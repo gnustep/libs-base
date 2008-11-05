68d6210fa9d0719adef8bad7d1fef4e702b8d9f5
Gregory John Casamento <greg.casamento@gmail.com>
2008-11-05 22:22:16 +0000

    Adding test tool for @sync.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@27009 72102866-910b-0410-8b05-ffd578937521

---
+++ Testing/synctest/main.m
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+
+static NSArray *array;
+
+@interface SyncTest : NSObject
+- (void) sayHello;
+@end
+
+@implementation SyncTest
+- (void) sayHello
+{
+  NSLog(@"Before the sync block %@",[NSThread currentThread]);
+  @synchronized(array) {
+    NSLog(@"In the sync block %@:%d",[NSThread currentThread],[NSThread isMainThread]);
+    NSLog(@"Waiting five seconds...");
+    usleep(5000);
+    NSLog(@"Done waiting");
+  }
+  NSLog(@"After the sync block %@",[NSThread currentThread]);
+}
+@end
+
+int main (int argc, const char * argv[]) 
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  SyncTest *st = [[SyncTest alloc] init];
+  array = [NSArray arrayWithObjects: @"Hello World",nil];	
+  
+  [NSThread detachNewThreadSelector: @selector(sayHello)
+	    toTarget: st
+	    withObject: nil];	
+  [st sayHello];
+  
+  [pool drain];
+  return 0;
+}
