75d23375ce17d6c71da14b95db5504d489ff9097
Richard Frith-MacDonald <rfm@gnu.org>
2009-02-26 11:09:05 +0000

    Add some missing comments
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@27988 72102866-910b-0410-8b05-ffd578937521

--- Source/GSRunLoopWatcher.m
+++ Source/GSRunLoopWatcher.m
@@ -1,3 +1,28 @@
+/** 
+   Copyright (C) 2008-2009 Free Software Foundation, Inc.
+
+   By: Richard Frith-Macdonald <richard@brainstorm.co.uk>
+
+   This file is part of the GNUstep Base Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02111 USA.
+
+   $Date: 2009-02-23 20:42:32 +0000 (Mon, 23 Feb 2009) $ $Revision: 27962 $
+*/
+
 #import "config.h"
 
 #import "GNUstepBase/preface.h"
