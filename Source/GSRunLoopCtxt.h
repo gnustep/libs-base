1379fab9859e5c09b4a71464e1098fbc317f2106
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2009-02-26 11:09:05 +0000

    Add some missing comments
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@27988 72102866-910b-0410-8b05-ffd578937521

--- Source/GSRunLoopCtxt.h
+++ Source/GSRunLoopCtxt.h
@@ -1,5 +1,29 @@
 #ifndef __GSRunLoopCtxt_h_GNUSTEP_BASE_INCLUDE
 #define __GSRunLoopCtxt_h_GNUSTEP_BASE_INCLUDE
+/** 
+   Copyright (C) 2008-2009 Free Software Foundation, Inc.
+
+   By: Richard Frith-Macdonald <richard@brainstorm.co.uk>
+
+   This file is part of the GNUstep Base Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02111 USA.
+
+   $Date$ $Revision$
+*/
 
 #include "config.h"
 #include <Foundation/NSException.h>
