c07b8718d78dd71d79f2e3d8fb8808091723ebcd
fredkiefer <fredkiefer@72102866-910b-0410-8b05-ffd578937521>
2009-03-28 16:43:54 +0000

    Get base to compile again on Windows.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@28143 72102866-910b-0410-8b05-ffd578937521

--- Source/NSSocketPort.m
+++ Source/NSSocketPort.m
@@ -1227,9 +1227,10 @@ static Class	runLoopClass;
 {
 #if	defined(__MINGW32__)
   WSANETWORKEVENTS ocurredEvents;
-#endif
+#else
   NSDebugMLLog(@"GSTcpHandle", @"received %s event on 0x%x",
     type != ET_WDESC ? "read" : "write", self);
+#endif
   /*
    * If we have been invalidated (desc < 0) then we should ignore this
    * event and remove ourself from the runloop.
