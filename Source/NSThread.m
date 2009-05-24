8904aada4f872bb88c7a5e654fd6afd71dd5a536
Richard Frith-MacDonald <rfm@gnu.org>
2009-05-24 04:22:09 +0000

    Fix race condition
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@28298 72102866-910b-0410-8b05-ffd578937521

--- Source/NSThread.m
+++ Source/NSThread.m
@@ -1057,7 +1057,12 @@ GSRunLoopInfoForThread(NSThread *aThread)
     }
   if (((NSThread_ivars*)aThread)->_runLoopInfo == nil)
     {
-      ((NSThread_ivars*)aThread)->_runLoopInfo = [GSRunLoopThreadInfo new];
+      [gnustep_global_lock lock];
+      if (((NSThread_ivars*)aThread)->_runLoopInfo == nil)
+        {
+          ((NSThread_ivars*)aThread)->_runLoopInfo = [GSRunLoopThreadInfo new];
+	}
+      [gnustep_global_lock unlock];
     }
   info = ((NSThread_ivars*)aThread)->_runLoopInfo;
   return info;
