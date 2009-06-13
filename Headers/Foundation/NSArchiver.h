0833bae975fda6b0bcbc8caa37169331a937dbd2
Richard Frith-MacDonald <rfm@gnu.org>
2009-06-13 07:36:43 +0000

    fix for problem reported by Dave MacLachlan
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/stable@28349 72102866-910b-0410-8b05-ffd578937521

--- Headers/Foundation/NSArchiver.h
+++ Headers/Foundation/NSArchiver.h
@@ -164,7 +164,7 @@ extern "C" {
   GSIArray		objMap;		/* Object crossreference map.	*/
   GSIArray		ptrMap;		/* Pointer crossreference map.	*/
 #ifndef	_IN_NSUNARCHIVER_M
-#undef	GSUnarchiverArray
+#undef	GSIArray
 #endif
   unsigned		cursor;		/* Position in data buffer.	*/
   unsigned		version;	/* Version of archiver used.	*/
