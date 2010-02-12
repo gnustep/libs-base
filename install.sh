0f77d231f5dbde63fa0baecfd51018329b5f8569
Richard Frith-MacDonald <rfm@gnu.org>
2010-02-12 10:44:22 +0000

    Add updates from trunk
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/reorg@29576 72102866-910b-0410-8b05-ffd578937521

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PREFIX=$1
 MAKE=${2-make}
 
 . $PREFIX/System/Library/Makefiles/GNUstep.sh
-sh configure --with-installation-domain=SYSTEM
+
 $MAKE GNUSTEP_INSTALLATION_DOMAIN=SYSTEM install
 
 exit 0
