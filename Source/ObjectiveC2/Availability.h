d6a954584871cbccc61ec201cc5ac1a34c87f294
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2010-02-19 16:33:44 +0000

    make objc2 compatibility code compile on mingw
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@29674 72102866-910b-0410-8b05-ffd578937521

--- Source/ObjectiveC2/Availability.h
+++ Source/ObjectiveC2/Availability.h
@@ -1,12 +1,17 @@
 
 #ifdef STRICT_MACOS_X
-#	define OBJC_NONPORTABLE __attribute__((error("Function not supported by the Apple runtime")))
+#  define OBJC_NONPORTABLE __attribute__((error("Function not supported by the Apple runtime")))
 #else
-#	define OBJC_NONPORTABLE
+#  define OBJC_NONPORTABLE
 #endif
 
 #if !defined(__DEPRECATE_DIRECT_ACCESS) || defined(__OBJC_LEGACY_GNU_MODE__) || defined(__OBJC_RUNTIME_INTERNAL__)
-#	define OBJC_DEPRECATED
+#  define OBJC_DEPRECATED
 #else
-#	define OBJC_DEPRECATED __attribute__((deprecated))
+#  if ((__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR >= 1))
+#    define OBJC_DEPRECATED __attribute__((deprecated))
+#  else
+#    define OBJC_DEPRECATED
+#  endif
 #endif
+
