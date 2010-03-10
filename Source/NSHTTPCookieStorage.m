3eefcc5e1b4c2f027e2fd06d6942bf8e46f6e60c
Adam Fedor <fedor@gnu.org>
2010-03-10 03:50:21 +0000

    Finish implementation
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@29893 72102866-910b-0410-8b05-ffd578937521

--- Source/NSHTTPCookieStorage.m
+++ Source/NSHTTPCookieStorage.m
@@ -26,6 +26,11 @@
 #define	EXPOSE_NSHTTPCookieStorage_IVARS	1
 #import "GSURLPrivate.h"
 #import "Foundation/NSSet.h"
+#include "Foundation/NSArray.h"
+#include "Foundation/NSFileManager.h"
+#include "Foundation/NSPathUtilities.h"
+#include "Foundation/NSString.h"
+#include "Foundation/NSDistributedNotificationCenter.h"
 #import "GNUstepBase/NSObject+GNUstepBase.h"
 
 NSString * const NSHTTPCookieManagerAcceptPolicyChangedNotification
@@ -34,19 +39,21 @@ NSString * const NSHTTPCookieManagerAcceptPolicyChangedNotification
 NSString * const NSHTTPCookieManagerCookiesChangedNotification
   = @"NSHTTPCookieManagerCookiesChangedNotification";
 
+NSString *objectObserver = @"org.GNUstep.NSHTTPCookieStorage";
+
 // Internal data storage
 typedef struct {
   NSHTTPCookieAcceptPolicy	_policy;
-  NSMutableSet			*_cookies;
+  NSMutableArray		*_cookies;
 } Internal;
  
 #define	this	((Internal*)(self->_NSHTTPCookieStorageInternal))
 #define	inst	((Internal*)(o->_NSHTTPCookieStorageInternal))
 
+@interface NSHTTPCookieStorage (Private)
+- (void) _updateFromCookieStore;
+@end
 
-/* FIXME
- * handle persistent storage and use policies.
- */
 @implementation NSHTTPCookieStorage
 
 static NSHTTPCookieStorage   *storage = nil;
@@ -69,8 +76,7 @@ static NSHTTPCookieStorage   *storage = nil;
 	    NSAllocateObject(self, 0, NSDefaultMallocZone());
 	  o->_NSHTTPCookieStorageInternal = (Internal*)
 	    NSZoneCalloc(NSDefaultMallocZone(), 1, sizeof(Internal));
-	  inst->_policy = NSHTTPCookieAcceptPolicyAlways;
-	  inst->_cookies = [NSMutableSet new];
+	  [o init];
 	  storage = o;
 	}
       [gnustep_global_lock unlock];
@@ -78,14 +84,139 @@ static NSHTTPCookieStorage   *storage = nil;
   return storage;
 }
 
+- init
+{
+  this->_policy = NSHTTPCookieAcceptPolicyAlways;
+  this->_cookies = [NSMutableArray new];
+  [[NSDistributedNotificationCenter defaultCenter] 
+    addObserver: self
+    selector: @selector(cookiesChangedNotification:)
+    name: NSHTTPCookieManagerCookiesChangedNotification
+    object: objectObserver];
+  [[NSDistributedNotificationCenter defaultCenter] 
+    addObserver: self
+    selector: @selector(acceptPolicyChangeNotification:)
+    name: NSHTTPCookieManagerAcceptPolicyChangedNotification
+    object: objectObserver];
+  [self _updateFromCookieStore];
+  return self;
+}
+
 - (void) dealloc
 {
   if (this != 0)
     {
+      [[NSDistributedNotificationCenter defaultCenter] removeObserver: self];
       RELEASE(this->_cookies);
       NSZoneFree([self zone], this);
     }
   [super dealloc];
+}
+
+- (NSString *) _cookieStorePath
+{
+  BOOL isDir;
+  NSString *path;
+  NSArray *dirs;
+
+  dirs = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, 
+  	  NSUserDomainMask, YES);
+  path = [[dirs objectAtIndex: 0] stringByAppendingPathComponent: @"Cookies"];
+  if ([[NSFileManager defaultManager] 
+        fileExistsAtPath: path isDirectory: &isDir] == NO || isDir == NO)
+    {
+      BOOL ok;
+      ok = [[NSFileManager defaultManager] createDirectoryAtPath: path 
+      	      withIntermediateDirectories: YES attributes: nil error: NULL];
+      if (ok == NO)
+        return nil; 
+    }
+  path = [path stringByAppendingPathComponent: @"Cookies.plist"];
+  return path;
+}
+
+/* Remove all cookies that have expired */
+/* FIXME: When will we know that the user session expired? */
+- (BOOL) _expireCookies: (BOOL)endUserSession
+{
+  BOOL changed = NO;
+  NSDate *now = [NSDate date];
+  unsigned count = [this->_cookies count];
+
+  /* FIXME: Handle Max-age */
+  while (count-- > 0)
+    {
+      NSHTTPCookie	*ck = [this->_cookies objectAtIndex: count];
+      NSDate *expDate = [ck expiresDate];
+      if ((endUserSession && expDate == nil) ||
+	  (expDate != nil && [expDate compare: now] != NSOrderedDescending))
+        {
+          [this->_cookies removeObject: ck];
+	  changed = YES;
+	}
+    }
+  return changed;
+}
+
+- (void) _updateFromCookieStore
+{
+  int i;
+  NSArray *properties;
+  NSString *path = [self _cookieStorePath];
+  if (path == nil)
+    {
+      return;
+    }
+  properties = nil;
+  NS_DURING
+    properties = [[NSString stringWithContentsOfFile: path] propertyList];
+  NS_HANDLER
+    NSLog(@"NSHTTPCookieStorage: Error reading cookies plist");
+  NS_ENDHANDLER
+  if (properties == nil) 
+    return;
+  for (i = 0; i < [properties count]; i++)
+    [this->_cookies addObject: 
+      [NSHTTPCookie cookieWithProperties: [properties objectAtIndex: i]]];
+}
+
+- (void) _updateToCookieStore
+{
+  int i, count;
+  NSMutableArray *properties;
+  NSString *path = [self _cookieStorePath];
+  if (path == nil)
+    {
+      return;
+    }
+  count = [this->_cookies count];
+  properties = [NSMutableArray arrayWithCapacity: count];
+  for (i = 0; i < count; i++) 
+    [properties addObject: [[this->_cookies objectAtIndex: i] properties]];
+  [properties writeToFile: path atomically: YES];
+}
+
+- (void) _doExpireUpdateAndNotify
+{
+  [self _expireCookies: NO];
+  [self _updateToCookieStore];
+  [[NSDistributedNotificationCenter defaultCenter] 
+    postNotificationName: NSHTTPCookieManagerCookiesChangedNotification
+    object: objectObserver];
+}
+
+- (void) cookiesChangedNotification: (NSNotification *)note
+{
+  if ([note object] == self)
+    return;
+  [self _updateFromCookieStore];
+}
+
+- (void) acceptPolicyChangeNotification: (NSNotification *)note
+{
+  if ([note object] == self)
+    return;
+  /* FIXME: Do we need a common place to store the policy? */
 }
 
 - (NSHTTPCookieAcceptPolicy) cookieAcceptPolicy
@@ -95,45 +226,108 @@ static NSHTTPCookieStorage   *storage = nil;
 
 - (NSArray *) cookies
 {
-  return [this->_cookies allObjects];
+  return [[this->_cookies copy] autorelease];
 }
 
 - (NSArray *) cookiesForURL: (NSURL *)URL
 {
-  [self notImplemented: _cmd];	// FIXME
-  return nil;
+  NSMutableArray *a = [NSMutableArray array];
+  NSEnumerator *ckenum = [this->_cookies objectEnumerator];
+  NSHTTPCookie *cookie;
+  NSString *receive_domain = [URL host];
+
+  while ((cookie = [ckenum nextObject]))
+    {
+      if ([receive_domain hasSuffix: [cookie domain]])
+        [a addObject: cookie];
+    }
+  return a;
 }
 
 - (void) deleteCookie: (NSHTTPCookie *)cookie
 {
-  [this->_cookies removeObject: cookie];
+  if ([this->_cookies indexOfObject: cookie] != NSNotFound)
+    {
+      [this->_cookies removeObject: cookie];
+      [self _doExpireUpdateAndNotify];
+    }
+  else
+    NSLog(@"NSHTTPCookieStorage: trying to delete a cookie that is not in the storage");
+}
+
+- (void) _setCookieNoNotify: (NSHTTPCookie *)cookie
+{
+  NSEnumerator *ckenum = [this->_cookies objectEnumerator];
+  NSHTTPCookie *ck, *remove_ck;
+  NSString *name = [cookie name];
+  NSString *path = [cookie path];
+  NSString *domain = [cookie domain];
+
+  NSAssert([cookie isKindOfClass: [NSHTTPCookie class]] == YES,
+    NSInvalidArgumentException);
+  
+  remove_ck = nil;
+  while ((ck = [ckenum nextObject]))
+    {
+      if ([name isEqual: [ck name]] && [path isEqual: [ck path]])
+        {
+	  /* The Apple documentation says the domain should match and 
+	  RFC 2965 says they should match, though the original Netscape docs 
+	  doesn't mention that the domain should match, so here, if the
+	  version is explicitely set to 0, we don't require it */
+	  id ckv = [[ck properties] objectForKey: NSHTTPCookieVersion];
+	  if ((ckv && [ckv intValue] == 0) || [domain isEqual: [ck domain]])
+	    {
+	      remove_ck = ck;
+	      break;
+	    }
+	}
+    }
+  if (remove_ck)
+    [this->_cookies removeObject: remove_ck];
+  
+  [this->_cookies addObject: cookie];
 }
 
 - (void) setCookie: (NSHTTPCookie *)cookie
 {
-  NSAssert([cookie isKindOfClass: [NSHTTPCookie class]] == YES,
-    NSInvalidArgumentException);
-  [this->_cookies addObject: cookie];
+  if (this->_policy == NSHTTPCookieAcceptPolicyNever)
+    return;
+  [self _setCookieNoNotify: cookie];
+  [self _doExpireUpdateAndNotify];
 }
 
 - (void) setCookieAcceptPolicy: (NSHTTPCookieAcceptPolicy)cookieAcceptPolicy
 {
   this->_policy = cookieAcceptPolicy;
+  [[NSDistributedNotificationCenter defaultCenter] 
+    postNotificationName: NSHTTPCookieManagerAcceptPolicyChangedNotification
+    object: objectObserver];
 }
 
 - (void) setCookies: (NSArray *)cookies
 	     forURL: (NSURL *)URL
     mainDocumentURL: (NSURL *)mainDocumentURL
 {
-  unsigned	count = [cookies count];
+  BOOL     changed = NO;
+  unsigned count = [cookies count];
+
+  if (count == 0 || this->_policy == NSHTTPCookieAcceptPolicyNever)
+    return;
 
   while (count-- > 0)
     {
-      NSHTTPCookie	*c = [cookies objectAtIndex: count];
+      NSHTTPCookie	*ck = [cookies objectAtIndex: count];
 
-      // FIXME check domain matches 
-      [this->_cookies addObject: c];
+      if (this->_policy == NSHTTPCookieAcceptPolicyOnlyFromMainDocumentDomain
+          && [[URL host] hasSuffix: [mainDocumentURL host]] == NO)
+	continue;
+
+      [self _setCookieNoNotify: ck];
+      changed = YES;
     }
+  if (changed)
+    [self _doExpireUpdateAndNotify];
 }
 
 @end
