c646d76a8196ac92e17be53725c24ceb3cf0ccb8
Riccardo Mottola <rm@gnu.org>
2010-06-10 23:33:56 +0000

    attempt of fixing test again
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@30659 72102866-910b-0410-8b05-ffd578937521

--- Source/ObjectiveC2/sync.m
+++ Source/ObjectiveC2/sync.m
@@ -1,5 +1,5 @@
 /* Ensure Unix98 compatible pthreads for glibc */
-#if defined __linux__ || defined __GLIBC__
+#if defined __linux__ || defined __GNU__ || defined __GLIBC__
 #  ifndef _XOPEN_SOURCE
 #    define _XOPEN_SOURCE 600
 #  endif
