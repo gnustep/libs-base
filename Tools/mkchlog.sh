7f8ead763f83d5b54ad71a94a9720204670c7ddb
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2010-12-31 14:06:33 +0000

    Actually add ChangeLog from last commit.
    Make mkchlog.sh show the changelog so I can check that it actually worked...
    
    
    
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@31806 72102866-910b-0410-8b05-ffd578937521

--- Tools/mkchlog.sh
+++ Tools/mkchlog.sh
@@ -2,4 +2,5 @@
 svn log -rPREV --xml --verbose | xsltproc /usr/local/share/svn2cl/svn2cl.xsl - > ChangeLog.new
 cat ChangeLog >> ChangeLog.new
 mv ChangeLog.new ChangeLog
+$EDITOR ChangeLog
 svn commit -m 'Added ChangeLog entry from last commit'
