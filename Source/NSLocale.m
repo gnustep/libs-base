60c38bae4e5e2f8c9d094033977ddb9874f81ba0
Richard Frith-MacDonald <rfm@gnu.org>
2011-02-13 06:40:20 +0000

    Partial revert
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@32126 72102866-910b-0410-8b05-ffd578937521

--- Source/NSLocale.m
+++ Source/NSLocale.m
@@ -179,14 +179,6 @@ static NSMutableDictionary *allLocales = nil;
 static NSDictionary *canonicalLocales = nil;
 static NSRecursiveLock *classLock = nil;
 
-#if GS_USE_ICU == 0
-+ (id) allocWithZone: (NSZone*)z
-{
-  [self notImplemented: _cmd
-		reason: @"missing ICU support at configure time."];
-}
-#endif
-
 + (void) initialize
 {
   if (self == [NSLocale class])
