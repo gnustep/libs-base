f7cc7203d9d788adccdbbc12dd473bda65267f2e
David Chisnall <github@theravensnest.org>
2011-05-28 12:51:57 +0000

    Make the mkchlog.sh script update the changelog, so you don't get conflicts when you try to commit.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33167 72102866-910b-0410-8b05-ffd578937521

--- Tools/mkchlog.sh
+++ Tools/mkchlog.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 svn log -rHEAD --xml --verbose | xsltproc /usr/local/share/svn2cl/svn2cl.xsl - > ChangeLog.new
+svn up ChangeLog
 cat ChangeLog >> ChangeLog.new
 mv ChangeLog.new ChangeLog
 $EDITOR ChangeLog
