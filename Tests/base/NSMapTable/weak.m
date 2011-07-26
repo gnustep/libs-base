407c9d00532a88e44d8fc372bd84d69de685261e
theraven <theraven@72102866-910b-0410-8b05-ffd578937521>
2011-07-26 20:12:32 +0000

    Tweak the weak tests so that they should still work, even if -base was compiled with something that doesn't do a very good job of optimising autorelease operations away.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33644 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSMapTable/weak.m
+++ Tests/base/NSMapTable/weak.m
@@ -5,14 +5,16 @@
 int main()
 {
 	[NSAutoreleasePool new];
-	NSMapTable *map = [NSMapTable mapTableWithStrongToWeakObjects];
-	NSMapTable *map2 = [NSMapTable mapTableWithWeakToStrongObjects];
+	id pool = [NSAutoreleasePool new];
+	NSMapTable *map = [[NSMapTable mapTableWithStrongToWeakObjects] retain];
+	NSMapTable *map2 = [[NSMapTable mapTableWithWeakToStrongObjects] retain];
 	id obj = [NSObject new];
 	
 	[map setObject: obj forKey: @"1"];
 	[map2 setObject: @"1" forKey: obj];
 	PASS(obj == [map objectForKey: @"1"], "Value stored in weak-value map");
 	PASS(nil != [map2 objectForKey: obj], "Value stored in weak-key map");
+	[pool drain];
 	[obj release];
 	PASS(nil == [map objectForKey: @"1"], "Value removed from weak-value map");
 	NSEnumerator *enumerator = [map2 keyEnumerator];
