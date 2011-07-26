00259f1a7cede009b8eb0df11c415bd06eda807f
David Chisnall <github@theravensnest.org>
2011-07-26 20:12:32 +0000

    Tweak the weak tests so that they should still work, even if -base was compiled with something that doesn't do a very good job of optimising autorelease operations away.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33644 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSHashTable/weak.m
+++ Tests/base/NSHashTable/weak.m
@@ -5,13 +5,15 @@
 int main()
 {
 	[NSAutoreleasePool new];
-	NSHashTable *ht = [NSHashTable hashTableWithWeakObjects];
+	id pool = [NSAutoreleasePool new];
+	NSHashTable *ht = [[NSHashTable hashTableWithWeakObjects] retain];
 	id obj = [NSObject new];
 	[ht addObject: obj];
 	PASS([ht containsObject: obj], "Added object to weak hash table");
 	PASS(1 == [ht count], "Weak hash table contains one object");
 	PASS([ht containsObject: obj], "Added object to weak hash table");
 	[obj release];
+	[pool drain];
 	PASS(0 == [ht count], "Weak hash table contains no objects");
 	PASS(0 == [[ht allObjects] count], "Weak hash table contains no objects");
 	return 0;
--- Tests/base/NSPointerArray/weak.m
+++ Tests/base/NSPointerArray/weak.m
@@ -4,11 +4,13 @@
 int main(void)
 {
 	[NSAutoreleasePool new];
-	NSPointerArray *pa = [NSPointerArray pointerArrayWithWeakObjects];
+	id pool = [NSAutoreleasePool new];
+	NSPointerArray *pa = [[NSPointerArray pointerArrayWithWeakObjects] retain];
 	id obj = [NSObject new];
 	[pa addPointer: obj];
 	PASS([pa count] == 1, "Added object to weak array");
 	[obj release];
+	[pool drain];
 	[pa compact];
 	PASS([pa count] == 0, "Removed object to weak array");
 	return 0;
