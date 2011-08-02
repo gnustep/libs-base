5134f513203b92a3ab97d21281a3c3828a57bcb6
fredkiefer <fredkiefer@72102866-910b-0410-8b05-ffd578937521>
2011-08-02 08:52:15 +0000

    Small fixes on tests.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33681 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSString/regex.m
+++ Tests/base/NSString/regex.m
@@ -3,12 +3,15 @@
 
 int main(void)
 {
-	NSString *regex = @"abcd*";
-	NSString *source = @"abcdddddd e f g";
-	NSRange r = [source rangeOfString: regex options: NSRegularExpressionSearch];
-	PASS(r.length == 9, "Correct length for regex, expected 9 got %d", (int)r.length);
-	regex = @"aBcD*";
-	r = [source rangeOfString: regex options: (NSRegularExpressionSearch | NSCaseInsensitiveSearch)];
-	PASS(r.length == 9, "Correct length for regex, expected 9 got %d", (int)r.length);
-	return 0;
+  NSAutoreleasePool *pool = [NSAutoreleasePool new];
+  NSString *regex = @"abcd*";
+  NSString *source = @"abcdddddd e f g";
+  NSRange r = [source rangeOfString: regex options: NSRegularExpressionSearch];
+  PASS(r.length == 9, "Correct length for regex, expected 9 got %d", (int)r.length);
+  regex = @"aBcD*";
+  r = [source rangeOfString: regex options: (NSRegularExpressionSearch | NSCaseInsensitiveSearch)];
+  PASS(r.length == 9, "Correct length for regex, expected 9 got %d", (int)r.length);
+
+  [pool release];
+  return 0;
 }
