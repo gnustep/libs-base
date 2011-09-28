ab9ae9995494d8f132bd38e8e4cb854f44712198
Niels Grewe <thebeing+gna@halbordnung.de>
2011-09-28 18:10:18 +0000

    Add some initial tests for NSXMLNode (will only pass on OS X, marked testHopeful for now).
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33903 72102866-910b-0410-8b05-ffd578937521

---
+++ Tests/base/NSXMLNode/kinds.m
@@ -0,0 +1,45 @@
+#import "Testing.h"
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSXMLNode.h>
+#import <Foundation/NSXMLDocument.h>
+#import <Foundation/NSXMLElement.h>
+#import <Foundation/NSXMLDTD.h>
+#import <Foundation/NSXMLDTDNode.h>
+
+#define NODE_KIND_HAS_CLASS(node, kind, theClass) \
+  do \
+  { \
+    node = [[NSXMLNode alloc] initWithKind: kind]; \
+    PASS([node isKindOfClass: [theClass class]], "Initializing with " #kind " produces instances of " #theClass "."); \
+    [node release]; \
+    node = nil; \
+  } while (0)
+
+int main()
+{
+  START_SET("NSXMLNode -initWithKind: initializer")
+  testHopeful = YES;
+  NSAutoreleasePool     *arp = [NSAutoreleasePool new];
+  NSXMLNode           *node;
+  NS_DURING
+  {
+    NODE_KIND_HAS_CLASS(node, NSXMLDocumentKind, NSXMLDocument);
+    NODE_KIND_HAS_CLASS(node, NSXMLElementKind, NSXMLElement);
+    NODE_KIND_HAS_CLASS(node, NSXMLDTDKind, NSXMLDTD);
+    NODE_KIND_HAS_CLASS(node, NSXMLEntityDeclarationKind, NSXMLDTDNode);
+    NODE_KIND_HAS_CLASS(node, NSXMLElementDeclarationKind, NSXMLDTDNode);
+    NODE_KIND_HAS_CLASS(node, NSXMLNotationDeclarationKind, NSXMLDTDNode);
+    node = [[NSXMLNode alloc] initWithKind: NSXMLAttributeDeclarationKind];
+    PASS (NO == [node isKindOfClass: [NSXMLDTDNode class]], "Does not instantiate NSXMLAttributeDeclarations through -initWithKind:");
+    [node release];
+  }
+  NS_HANDLER
+  {
+    PASS(YES == NO, "Failed testing NSXMLNode kinds");
+  }
+  NS_ENDHANDLER
+  [arp release];
+  arp = nil;
+  END_SET("NSXMLNode -initWithKind: initializer")
+  return 0;
+}
