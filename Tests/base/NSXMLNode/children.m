b8ed8cedc299f7590900ad6bb5b3b19f055008f4
Niels Grewe <thebeing+gna@halbordnung.de>
2011-10-01 00:05:27 +0000

    Add test for child de-/re-attachment behaviour in NSXML.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@33920 72102866-910b-0410-8b05-ffd578937521

---
+++ Tests/base/NSXMLNode/children.m
@@ -0,0 +1,31 @@
+#import "Testing.h"
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSXMLNode.h>
+#import <Foundation/NSXMLDocument.h>
+#import <Foundation/NSXMLElement.h>
+
+
+int main()
+{
+  START_SET("NSXMLNode - handling children")
+  testHopeful = YES;
+  NS_DURING
+  {
+    NSXMLElement *node = [[NSXMLElement alloc] initWithKind: NSXMLElementKind];
+    NSXMLDocument *docA = [[NSXMLDocument alloc] initWithRootElement: node];
+    NSXMLDocument *docB = nil;
+    [node detach];
+    PASS(docB = [[NSXMLDocument alloc] initWithRootElement: node], "Detached children can be reattached.");
+    [docA release];
+    [docB release];
+    docA = [[NSXMLDocument alloc] initWithRootElement: node];
+    PASS_EXCEPTION(docB = [[NSXMLDocument alloc] initWithRootElement: node], NSInternalInconsistencyException, "Reusing a child throws an exception");
+  }
+  NS_HANDLER
+  {
+    PASS (0 == 1, "NSXML child handling working.");
+  }
+  NS_ENDHANDLER
+  END_SET("NSXMLNode - handling children")
+  return 0;
+}
