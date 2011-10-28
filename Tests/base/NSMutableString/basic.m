f6a225bfccd07c67b9364f4f409d93824e089f14
Fred Kiefer <fredkiefer@gmx.de>
2011-10-28 12:21:14 +0000

    * Tests/base/NSMutableString/basic.m: Add test for replaceOccurrencesOfString:...range:
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@34080 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSMutableString/basic.m
+++ Tests/base/NSMutableString/basic.m
@@ -69,7 +69,8 @@ int main()
   NSAutoreleasePool   *arp = [NSAutoreleasePool new];
   unichar	u0 = 'a';
   unichar	u1 = 0xfe66;
-  NSMutableString *testObj,*base,*ext,*want;
+  NSMutableString *testObj,*base,*ext,*want, *str1, *str2;
+  unichar chars[3];
 
   test_alloc(@"NSMutableString");
   testObj = [[NSMutableString alloc] initWithCString:"Hello\n"];
@@ -117,6 +118,22 @@ int main()
   PASS([testObj isEqual: @"hally"],
     "custom string replacement works at end of string");
 
+  chars[0] = '\"';
+  chars[1] = 1;
+  str1 = [NSMutableString stringWithCharacters: chars length: 2];
+  [str1 replaceOccurrencesOfString: @"\""
+                       withString: @"\\\""
+                          options: 0
+                            range: NSMakeRange(0, [str1 length])];
+
+  chars[0] = '\\';
+  chars[1] = '\"';
+  chars[2] = 1;
+  str2 = [NSMutableString stringWithCharacters: chars length: 3];
+  NSLog(@"string 1 %@ string 2 %@", str1, str2);
+  PASS([str1 isEqual: str2],
+    "string occurrences replacement works");
+  
   [testObj release];
   [arp release]; arp = nil; 
   return 0;
