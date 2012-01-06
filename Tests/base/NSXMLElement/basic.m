49030b1dc76eeb6a9973aced00683cfe88980d49
Richard Frith-MacDonald <rfm@gnu.org>
2012-01-06 09:46:50 +0000

    some basic setter/getter tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@34437 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSXMLElement/basic.m
+++ Tests/base/NSXMLElement/basic.m
@@ -20,6 +20,30 @@ int main()
   PASS([other isEqual: node], "elements with same name are equal");
 
   [other release];
+
+  PASS(NSXMLElementKind == [node kind], "invalid node kind is correct");
+  PASS(0 == [node level], "element node level is zero");
+  PASS_EQUAL([node URI], nil, "element node URI is nil");
+  PASS_EQUAL([node objectValue], @"", "element node object value is empty");
+  PASS_EQUAL([node stringValue], @"", "element node string value is empty");
+  PASS_EQUAL([node children], nil, "element node children is nil");
+
+  [node setURI: @"URI"];
+  PASS_EQUAL([node URI], @"URI",
+    "setting URI on element node works");
+  [node setObjectValue: @"anObject"];
+  PASS_EQUAL([node objectValue], @"anObject",
+    "setting object value on element node works");
+  [node setObjectValue: nil];
+  PASS_EQUAL([node objectValue], @"",
+    "setting nil object value on element node gives empty string");
+  [node setStringValue: @"aString"];
+  PASS_EQUAL([node stringValue], @"aString",
+    "setting string value on element node works");
+  [node setStringValue: nil];
+  PASS_EQUAL([node stringValue], @"",
+    "setting nil string value on element node gives empty string");
+
   [node release];
 
   [arp release];
--- Tests/base/NSXMLNode/basic.m
+++ Tests/base/NSXMLNode/basic.m
@@ -18,10 +18,35 @@ int main()
 
   other = [[NSXMLNode alloc] initWithKind: NSXMLInvalidKind];
   PASS([other isEqual: node], "empty nodes are equal");
-  [node release];
 
-  [other setName: @"other"];
-  PASS(nil == [other name], "setting name on invalid node gives a nil name");
+  PASS(NSXMLInvalidKind == [node kind], "invalid node kind is correct");
+  PASS(0 == [node level], "invalid node level is zero");
+  PASS_EQUAL([node name], nil, "invalid node name is nil");
+  PASS_EQUAL([node URI], nil, "invalid node URI is nil");
+  PASS_EQUAL([node objectValue], nil, "invalid node object value is nil");
+  PASS_EQUAL([node stringValue], @"", "invalid node string value is empty");
+  PASS_EQUAL([node children], nil, "invalid node children is nil");
+
+  [node setName: @"name"];
+  PASS_EQUAL([node name], nil,
+    "setting name on invalid node gives a nil name");
+  [node setURI: @"URI"];
+  PASS_EQUAL([node URI], nil,
+    "setting URI on invalid node gives a nil URI");
+  [node setObjectValue: @"anObject"];
+  PASS_EQUAL([node objectValue], @"anObject",
+    "setting object value on invalid node works");
+  [node setObjectValue: nil];
+  PASS_EQUAL([node objectValue], nil,
+    "setting nil object value on invalid node works");
+  [node setStringValue: @"aString"];
+  PASS_EQUAL([node stringValue], @"aString",
+    "setting string value on invalid node works");
+  [node setStringValue: nil];
+  PASS_EQUAL([node stringValue], @"",
+    "setting nil string value on invalid node gives empty string");
+
+  [node release];
   [other release];
 
   [arp release];
