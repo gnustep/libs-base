aa4ce6d7202bc8ea01de9e3f8849e6a61178ad49
Fred Kiefer <fredkiefer@gmx.de>
2012-02-27 18:35:38 +0000

    Replace last change by a better one.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@34829 72102866-910b-0410-8b05-ffd578937521

--- Source/NSXMLNode.m
+++ Source/NSXMLNode.m
@@ -937,14 +937,11 @@ NSArray *execute_xpath(NSXMLNode *xmlNode,
 - (id) copyWithZone: (NSZone*)zone
 {
   id c = [[self class] allocWithZone: zone];
-  xmlNodePtr newNode = xmlCopyNode([self _node], 1); // make a deep copy
+  // make a deep copy
+  xmlNodePtr newNode = xmlCopyNode([self _node], 2);
   clearPrivatePointers(newNode);
 
-  //c = [c initWithKind: internal->kind options: internal->options];
-  //[c _setNode: newNode];
   c = [c _initWithNode: newNode kind: internal->kind];
-
-  
 
 //  [c setName: [self name]];
 //  [c setURI: [self URI]];
