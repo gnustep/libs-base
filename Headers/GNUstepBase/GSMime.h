52bc22f8be625904de46a65874e4a0a156841253
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2012-10-30 14:05:12 +0000

    correct declared return type
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@35765 72102866-910b-0410-8b05-ffd578937521

--- Headers/GNUstepBase/GSMime.h
+++ Headers/GNUstepBase/GSMime.h
@@ -237,7 +237,7 @@ extern "C" {
 + (GSMimeParser*) mimeParser;
 
 - (GSMimeCodingContext*) contextFor: (GSMimeHeader*)info;
-- (NSData*) data;
+- (NSMutableData*) data;
 - (BOOL) decodeData: (NSData*)sData
 	  fromRange: (NSRange)aRange
 	   intoData: (NSMutableData*)dData
