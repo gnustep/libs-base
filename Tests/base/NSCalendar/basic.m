c3b5c2897a41ca3fbb10106efa893940178a6f67
Richard Frith-MacDonald <rfm@gnu.org>
2013-03-26 09:59:35 +0000

    a few more tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@36435 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSCalendar/basic.m
+++ Tests/base/NSCalendar/basic.m
@@ -16,10 +16,45 @@ int main()
   id testObj = [NSCalendar currentCalendar];
 
   test_NSObject(@"NSCalendar", [NSArray arrayWithObject: testObj]);
+
+#if     __APPLE__ 
+  test_keyed_NSCoding([NSArray arrayWithObject: testObj]);
+#else
   test_NSCoding([NSArray arrayWithObject: testObj]);
+#endif
+
   test_NSCopying(@"NSCalendar", @"NSCalendar",
     [NSArray arrayWithObject: testObj], NO, NO);
   
+/* Test first weekday
+ */
+  [testObj setFirstWeekday: 3];
+  PASS([testObj firstWeekday] == 3, "-firstWeekday returns the correct day");
+  [testObj setFirstWeekday: 1];
+  PASS([testObj firstWeekday] == 1, "-setFirstWeekday: works");
+
+  [testObj setFirstWeekday: 0];
+  PASS([testObj firstWeekday] == 0, "-setFirstWeekday: can set zero");
+  [testObj setFirstWeekday: 10];
+  PASS([testObj firstWeekday] == 10, "-setFirstWeekday: can set ten");
+
+/* Test minimum days in first week
+ */
+  [testObj setMinimumDaysInFirstWeek: 1];
+  PASS([testObj minimumDaysInFirstWeek] == 1,
+    "-minimumDaysInFirstWeek returns the correct count");
+  [testObj setMinimumDaysInFirstWeek: 4];
+  PASS([testObj minimumDaysInFirstWeek] == 4,
+    "-setMinimumDaysInFirstWeek: works");
+
+  [testObj setMinimumDaysInFirstWeek: 0];
+  PASS([testObj minimumDaysInFirstWeek] == 0,
+    "-setMinimumDaysInFirstWeek: can set zero");
+  [testObj setMinimumDaysInFirstWeek: 10];
+  PASS([testObj minimumDaysInFirstWeek] == 10,
+    "-setMinimumDaysInFirstWeek: can set ten");
+
+
   END_SET("NSCalendar basic")
   return 0;
 }
