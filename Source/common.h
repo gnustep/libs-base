42c5213da1e51eb44b2a34db83c4a1b0ab12f562
Riccardo Mottola <rm@gnu.org>
2014-06-18 22:49:39 +0000

    Include and do not import pure-C header, guard against re-inclusion
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@37949 72102866-910b-0410-8b05-ffd578937521

--- Source/common.h
+++ Source/common.h
@@ -4,7 +4,10 @@
  * might be from an earlier build.
  */
 
-#import	"config.h"
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "config.h"
 
 #if	defined(HAVE_STRING_H)
 /* For POSIX strerror_r() and others
@@ -82,3 +85,4 @@
 #  endif
 #endif
 
+#endif /* COMMON_H */
