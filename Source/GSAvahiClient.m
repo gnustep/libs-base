ebd14e8ab6e0d0c0dc9d9ca7f4beb9c6ac46a7ba
Marcian Lytwyn <gna@advcsi.com>
2015-02-03 22:56:15 +0000

    Avahi crash fixes if daemon is not running
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/gnustep_testplant_branch@38325 72102866-910b-0410-8b05-ffd578937521

--- Source/GSAvahiClient.m
+++ Source/GSAvahiClient.m
@@ -147,10 +147,10 @@ GSAvahiClientState(AvahiClient *client, AvahiClientState state, void *userInfo)
     {
       [_lock lock];
       if (_client != NULL)
-	{
-	  avahi_client_free((AvahiClient*)_client);
-	  _client = NULL;
-	}
+        {
+          avahi_client_free((AvahiClient*)_client);
+          _client = NULL;
+        }
       [_lock unlock];
     }
 }
@@ -166,14 +166,19 @@ GSAvahiClientState(AvahiClient *client, AvahiClientState state, void *userInfo)
     {
       [_lock lock];
       if (_client == NULL)
-	{
-	  _client = (void*)avahi_client_new([ctx avahiPoll],
-	    (AvahiClientFlags)flags,
-	    GSAvahiClientState,
-	    (void*)self,
-	    errNo);
-	}
+        {
+          _client = (void*)avahi_client_new([ctx avahiPoll],
+                                            (AvahiClientFlags)flags,
+                                            GSAvahiClientState,
+                                            (void*)self,
+                                            errNo);
+        }
       [_lock unlock];
+      
+      if ((_client == NULL) || (*errNo != 0))
+        {
+          NSWarnLog(@"client allocation error: %ld", *errNo);
+        }
     }
 }
 
