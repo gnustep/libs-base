23ccf7f801795fd5325a19793cd4063eb721e1c2
Marcian Lytwyn <gna@advcsi.com>
2015-02-04 21:04:46 +0000

    Fix multiple callbacks for didNotResolve service
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/gnustep_testplant_branch@38333 72102866-910b-0410-8b05-ffd578937521

--- Source/GSMDNSNetServices.m
+++ Source/GSMDNSNetServices.m
@@ -1010,9 +1010,9 @@ QueryCallback(DNSServiceRef			 sdRef,
   
   service = (Service *) _reserved;
   
+  LOCK(service);
   if (service->timeout)
   {
-    LOCK(service);
     {
       [service->timeout invalidate];
       DESTROY(service->timeout);
@@ -1022,8 +1022,8 @@ QueryCallback(DNSServiceRef			 sdRef,
       [self netService: self
          didNotResolve: CreateError(self, NSNetServicesTimeoutError)];
     }
-    UNLOCK(service);
   }
+  UNLOCK(service);
 }
 
 /**
@@ -1121,8 +1121,6 @@ QueryCallback(DNSServiceRef			 sdRef,
       if (kDNSServiceErr_NoError != errorCode)
       {
         // Notify delegate...
-        [self netService: self
-           didNotResolve: CreateError(self, errorCode)];
         [self stopResolving: self];
       }
       else
@@ -1176,8 +1174,6 @@ QueryCallback(DNSServiceRef			 sdRef,
         if (kDNSServiceErr_NoError != errorCode)
         {
           // Notify delegate...
-          [self netService: self
-             didNotResolve: CreateError(self, errorCode)];
           [self stopResolving: self];
         }
         else
@@ -1185,12 +1181,11 @@ QueryCallback(DNSServiceRef			 sdRef,
         if (([self addresses] == nil) || ([[self addresses] count] == 0))
         {
           // Notify delegate...
-          [self netService: self
-             didNotResolve: CreateError(self, errorCode)];
           [self stopResolving: self];
         }
         else
         {
+          // timeout should have been stopped byt the query results processing...
           // notify the delegate
           [self netServiceDidResolveAddress: self];
         }
