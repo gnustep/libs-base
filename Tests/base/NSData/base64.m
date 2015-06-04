9958374c341eb13f2a77ef42092ad9ee717a6210
Richard Frith-MacDonald <rfm@gnu.org>
2015-06-04 09:10:50 +0000

    first try
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@38598 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSData/base64.m
+++ Tests/base/NSData/base64.m
@@ -8,30 +8,43 @@ int main()
   NSData        *data;
   NSData        *ref;
 
-/*
+  PASS_EXCEPTION([[NSData alloc] initWithBase64EncodedString: nil options: 0],
+    NSInvalidArgumentException, "nil argument causes exception");
+
   data = [[NSData alloc] initWithBase64EncodedString: @"" options: 0];
   ref = [NSData data];
   PASS_EQUAL(data, ref, "base64 decoding vector 1")
   [data release];
 
-  data = [[NSData alloc] initWithBase64EncodedString: @"Zg" options: 0];
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zg==" options: 0];
   ref = [NSData dataWithBytes: "f" length: 1];
   PASS_EQUAL(data, ref, "base64 decoding vector 2")
   [data release];
 
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zg=="],
-    @"f", "base64 decoding vector 2")
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zm8="],
-    @"fo", "base64 decoding vector 3")
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zm9v"],
-    @"foo", "base64 decoding vector 4")
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zm9vYg=="],
-    @"foob", "base64 decoding vector 5")
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zm9vYmE="],
-    @"fooba", "base64 decoding vector 6")
-  PASS_EQUAL([GSMimeDocument decodeBase64String: @"Zm9vYmFy"],
-    @"foobar", "base64 decoding vector 7")
-*/
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zm8=" options: 0];
+  ref = [NSData dataWithBytes: "fo" length: 2];
+  PASS_EQUAL(data, ref, "base64 decoding vector 3")
+  [data release];
+
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zm9v" options: 0];
+  ref = [NSData dataWithBytes: "foo" length: 3];
+  PASS_EQUAL(data, ref, "base64 decoding vector 4")
+  [data release];
+
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zm9vYg==" options: 0];
+  ref = [NSData dataWithBytes: "foob" length: 4];
+  PASS_EQUAL(data, ref, "base64 decoding vector 5")
+  [data release];
+
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zm9vYmE=" options: 0];
+  ref = [NSData dataWithBytes: "fooba" length: 5];
+  PASS_EQUAL(data, ref, "base64 decoding vector 6")
+  [data release];
+
+  data = [[NSData alloc] initWithBase64EncodedString: @"Zm9vYmFy" options: 0];
+  ref = [NSData dataWithBytes: "foobar" length: 6];
+  PASS_EQUAL(data, ref, "base64 decoding vector 7")
+  [data release];
 
   [arp release]; arp = nil;
   return 0;
