2ec70cee86c6e07e5f2cfc8ac93e1cfea5ea0567
rmottola <rmottola@72102866-910b-0410-8b05-ffd578937521>
2015-09-16 00:53:30 +0000

    Add simple (files only and no keys handling) implementation of 10.6 method contentsOfDirectoryAtURL
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@38987 72102866-910b-0410-8b05-ffd578937521

--- Headers/Foundation/NSFileManager.h
+++ Headers/Foundation/NSFileManager.h
@@ -198,6 +198,13 @@ typedef	uint32_t	OSType;
 #define OSTYPE_DECLARED
 #endif
 
+typedef enum : NSUInteger
+  {
+    NSDirectoryEnumerationSkipsSubdirectoryDescendants = 1L << 0,
+    NSDirectoryEnumerationSkipsPackageDescendants = 1L << 1,
+    NSDirectoryEnumerationSkipsHiddenFiles = 1L << 2
+  } NSDirectoryEnumerationOptions; 
+  
 @interface NSFileManager : NSObject
 {
 #if	GS_EXPOSE(NSFileManager)
@@ -299,9 +306,23 @@ typedef	uint32_t	OSType;
 - (BOOL) contentsEqualAtPath: (NSString*)path1
 		     andPath: (NSString*)path2;
 
+#if OS_API_VERSION(MAC_OS_X_VERSION_10_6, GS_API_LATEST)
+/**
+ * Returns an array of NSURL of the contents of the specified directory. <br>
+ * The listing is shallow and does not recurse into subdirectories.
+ * The special files '.' and '..' are excluded but it can return hidden files. <br>
+ * The only <i>mask</i> option supported is  NSDirectoryEnumerationSkipsHiddenFiles.<br>
+ * The current implementation handles only files and property keys are ignored.
+ */
+- (NSArray*) contentsOfDirectoryAtURL:(NSURL*)url
+           includingPropertiesForKeys:(NSArray*)keys
+                              options:(NSDirectoryEnumerationOptions)mask
+                                error:(NSError **)error;
+#endif
+  
 #if OS_API_VERSION(MAC_OS_X_VERSION_10_5, GS_API_LATEST)
 /**
- * Returns an array of the contents of the specified directory.<br />
+ * Returns an array of NSStrings of the contents of the specified directory.<br />
  * The listing does <strong>not</strong> recursively list subdirectories.<br />
  * The special files '.' and '..' are not listed.<br />
  * Indicates an error by returning nil (eg. if path is not a directory or
