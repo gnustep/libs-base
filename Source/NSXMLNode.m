f8a6a4513379b29606d07e0381c12595e57d2bb0
Richard Frith-MacDonald <rfm@gnu.org>
2015-11-01 23:28:18 +0000

    another stub
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@39114 72102866-910b-0410-8b05-ffd578937521

--- Source/NSXMLNode.m
+++ Source/NSXMLNode.m
@@ -2090,4 +2090,272 @@ execute_xpath(xmlNodePtr node, NSString *xpath_exp, NSDictionary *constants,
 }
 @end
 
+#else
+
+#import "Foundation/NSXMLNode.h"
+
+@implementation NSXMLNode
+
++ (id) attributeWithName: (NSString*)name
+	     stringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
++ (id) attributeWithName: (NSString*)name
+		     URI: (NSString*)URI
+	     stringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
++ (id) commentWithStringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
++ (id) DTDNodeWithXMLString: (NSString*)string
+{
+  return nil;
+}
+
++ (id) document
+{
+  return nil;
+}
+
++ (id) documentWithRootElement: (NSXMLElement*)element
+{
+  return nil;
+}
+
++ (id) elementWithName: (NSString*)name
+{
+  return nil;
+}
+
++ (id) elementWithName: (NSString*)name
+	      children: (NSArray*)children
+	    attributes: (NSArray*)attributes
+{
+  return nil;
+}
+
++ (id) elementWithName: (NSString*)name
+		   URI: (NSString*)URI
+{
+  return nil;
+}
+
++ (id) elementWithName: (NSString*)name
+	   stringValue: (NSString*)string
+{
+  return nil;
+}
+
++ (NSString*) localNameForName: (NSString*)name
+{
+  return nil;
+}
+
++ (id) namespaceWithName: (NSString*)name
+	     stringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
++ (NSXMLNode*) predefinedNamespaceForPrefix: (NSString*)name
+{
+  return nil;
+}
+
++ (NSString*) prefixForName: (NSString*)name
+{
+  return nil;
+}
+
++ (id) processingInstructionWithName: (NSString*)name
+			 stringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
++ (id) textWithStringValue: (NSString*)stringValue
+{
+  return nil;
+}
+
+- (NSString*) canonicalXMLStringPreservingComments: (BOOL)comments
+{
+  return nil;
+}
+
+- (NSXMLNode*) childAtIndex: (NSUInteger)index
+{
+  return nil;
+}
+
+- (NSUInteger) childCount
+{
+  return 0;
+}
+
+- (NSArray*) children
+{
+  return nil;
+}
+
+- (id) copyWithZone: (NSZone*)zone
+{
+  return nil;
+}
+
+- (void) detach
+{
+}
+
+- (NSUInteger) index
+{
+  return 0;
+}
+
+- (id) init
+{
+  DESTROY(self);
+  return nil;
+}
+
+- (id) initWithKind: (NSXMLNodeKind)theKind
+{
+  return [self init];
+}
+
+- (id) initWithKind: (NSXMLNodeKind)theKind options: (NSUInteger)theOptions
+{
+  return [self init];
+}
+
+- (NSXMLNodeKind) kind
+{
+  return 0;
+}
+
+- (NSUInteger) level
+{
+  return 0;
+}
+
+- (NSString*) localName
+{
+  return nil;
+}
+
+- (NSString*) name
+{
+  return nil;
+}
+
+- (NSXMLNode*) nextNode
+{
+  return nil;
+}
+
+- (NSXMLNode*) nextSibling
+{
+  return nil;
+}
+
+- (id) objectValue
+{
+  return nil;
+}
+
+- (NSXMLNode*) parent
+{
+  return nil;
+}
+
+- (NSString*) prefix
+{
+  return nil;
+}
+
+- (NSXMLNode*) previousNode
+{
+  return nil;
+}
+
+- (NSXMLNode*) previousSibling
+{
+  return nil;
+}
+
+- (NSXMLDocument*) rootDocument
+{
+  return nil;
+}
+
+- (NSString*) stringValue
+{
+  return nil;
+}
+
+- (void) setObjectValue: (id)value
+{
+}
+
+- (void) setName: (NSString *)name
+{
+}
+
+- (void) setStringValue: (NSString*)string
+{
+}
+
+- (void) setStringValue: (NSString*)string resolvingEntities: (BOOL)resolve
+{
+}
+
+- (void) setURI: (NSString*)URI
+{
+}
+
+- (NSString*) URI
+{
+  return nil;
+}
+
+- (NSString*) XMLString
+{
+  return nil;
+}
+
+- (NSString*) XMLStringWithOptions: (NSUInteger)theOptions
+{
+  return nil;
+}
+
+- (NSString*) XPath
+{
+  return nil;
+}
+
+- (NSArray*) nodesForXPath: (NSString*)anxpath error: (NSError**)error
+{
+  return nil;
+}
+
+ - (NSArray*) objectsForXQuery: (NSString*)xquery
+		     constants: (NSDictionary*)constants
+		         error: (NSError**)error
+{
+  return nil;
+}
+
+- (NSArray*) objectsForXQuery: (NSString*)xquery error: (NSError**)error
+{
+  return nil;
+}
+@end
+
 #endif	/* HAVE_LIBXML */
