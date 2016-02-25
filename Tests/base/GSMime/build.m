49fc46614e05d546a73268c348b1d3231494fd64
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2016-02-25 11:55:58 +0000

    Fix typos in comments, add a couple of testcases
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@39414 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/GSMime/build.m
+++ Tests/base/GSMime/build.m
@@ -5,21 +5,37 @@
 int main(int argc,char **argv)
 {
   NSAutoreleasePool   *arp = [NSAutoreleasePool new];
-  NSString *data = nil;
+  NSData *data = nil;
+  NSString *string = nil;
   GSMimeDocument *doc = [[GSMimeDocument alloc] init];
   NSMutableDictionary *par = [[NSMutableDictionary alloc] init];
+
   [par setObject: @"my/type" forKey: @"type"];
   [doc setContent: @"Hello\r\n"];
   [doc setHeader: [[GSMimeHeader alloc] initWithName: @"content-type"
   					       value: @"text/plain"
 					  parameters: par]];
 
-  [doc setHeader: [[GSMimeHeader alloc] initWithName: @"content-transfer-encoding"
-  					       value: @"binary"
-					  parameters: nil]];
+  [doc setHeader:
+    [[GSMimeHeader alloc] initWithName: @"content-transfer-encoding"
+                                 value: @"binary"
+                            parameters: nil]];
 				
   data = [NSData dataWithContentsOfFile: @"mime8.dat"];
   PASS([[doc rawMimeData] isEqual: data], "Can make a simple document");
+
+  string = @"MBNA credit card account − more information about Apple Pay.";
+  [doc setHeader:
+    [[GSMimeHeader alloc] initWithName: @"subject"
+                                 value: string
+                            parameters: nil]];
+  data = [doc rawMimeData];
+  PASS(data != nil, "Can use non-ascii character in subject");
+  [[doc rawMimeData] writeToFile: @"/tmp/xyz" atomically: NO];
+  doc = [GSMimeParser documentFromData: data];
+  PASS_EQUAL([[doc headerNamed: @"subject"] value], string,
+   "Can restore non-ascii character in subject");
+
   [arp release]; arp = nil;
   return 0;
 }
