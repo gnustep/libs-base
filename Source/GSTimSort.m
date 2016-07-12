gnustep/libs-base

commit 71941dd24dfa036e08d826ff1e8f894f96265ef9

cleanup changes
git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@39762 72102866-910b-0410-8b05-ffd578937521

Source/GSTimSort.m
@@ -53,157 +53,6 @@
 #define GS_MIN_GALLOP 7
 #define GS_INITIAL_TEMP_STORAGE 256
 
-static inline void
-reverseRange(id *buffer, NSRange r)
-{
-  NSUInteger loc = r.location;
-  NSUInteger max = (NSMaxRange(r) - 1);
-
-  while (loc < max)
-    {
-      id temp = buffer[loc];
-      buffer[loc++] = buffer[max];
-      buffer[max--] = temp;
-    }
-}
-
-/* In-place binary insertion sorting for small arrays (i.e. those which are
- * smaller than GS_MIN_MERGE. We use this to generate minimal runs for timsort.
- */
-static void
-internalBinarySort(id *buffer,
-  NSRange r,
-  NSUInteger start,
-  id compOrDesc,
-  GSComparisonType type,
-  void *context)
-{
-  NSUInteger min = r.location;
-  NSUInteger max = NSMaxRange(r);
-
-  NSCAssert2(NSLocationInRange(start, r),
-    @"Start index %lu not in range %@",
-    start, NSStringFromRange(r));
-
-  if (min == start)
-    {
-      start++;
-    }
-  // We assume that everything before start is sorted.
-  for (; start < max; ++start)
-    {
-      NSUInteger left = min;
-      NSUInteger right = start;
-      id pivot = buffer[right];
-      int i = 0;
-
-      do
-        {
-          NSUInteger midPoint = (left + ((right - left) >> 1));
-          NSComparisonResult res = GSCompareUsingDescriptorOrComparator(pivot,
-            buffer[midPoint],
-            compOrDesc,
-            type,
-            context);
-          if (NSOrderedAscending == res)
-            {
-              right = midPoint;
-            }
-          else
-            {
-              left = midPoint + 1;
-            }
-        } while (left < right);
-      NSCAssert(left == right, @"Binary sort iteration did not end correctly,");
-      // We make room for the pivot and place it at left.
-      for (i = start; i > left; --i)
-        {
-          buffer[i] = buffer[(i - 1)];
-        }
-      buffer[left] = pivot;
-    }
-}
-
-
-/*
- * Count the number of elements in the range that are already ordered.
- * If the order is a descending one, reverse it so that all runs are ordered the
- * same way.
- */
-static inline NSUInteger
-countAscendizedRun(id *buf, NSRange r, id descOrComp,
-  GSComparisonType type, void*context)
-{
-  NSUInteger min = r.location;
-  NSUInteger runMax = min + 1;
-  NSUInteger rangeMax = NSMaxRange(r);
-
-  if (runMax == rangeMax)
-    {
-      return 1;
-    }
-  if (NSOrderedDescending == GSCompareUsingDescriptorOrComparator(buf[min],
-    buf[runMax++], descOrComp, type, context))
-    {
-      while ((runMax < rangeMax) && NSOrderedDescending
-        == GSCompareUsingDescriptorOrComparator(buf[runMax - 1],
-        buf[runMax], descOrComp, type, context))
-        {
-          runMax++;
-        }
-      reverseRange(buf, NSMakeRange(min, (runMax - min)));
-    }
-  else // ascending or equal
-    {
-      while ((runMax < rangeMax) && NSOrderedDescending
-        != GSCompareUsingDescriptorOrComparator(buf[runMax - 1],
-        buf[runMax], descOrComp, type, context))
-        {
-          runMax++;
-        }
-    }
-  return (runMax - min);
-}
-
-
-/*
- * Calculate a sensible minimum length for the runs, these need to be powers of
- * two, or less than, but close to, one, but always at least GS_MIN_MERGE. For
- * details on why this is useful, see Python's listsort.txt.
- */
-static inline NSUInteger
-minimumRunLength(NSUInteger length)
-{
-  NSUInteger r = 0;
-
-  while (length >= GS_MIN_MERGE)
-    {
-      r |= length & 1;
-      length >>= 1;
-    }
-
-  return (length + r);
-}
-
-/*
- * For arrays up to GS_MIN_MERGE, we don't do merging. Instead, we identify
- * pre-ordering at the begining of the range and sort the rest using binary
- * sort.
- */
-static inline void
-miniTimSort(id *buf, NSRange r, id descOrComp, GSComparisonType ty, void *ctx)
-{
-  NSUInteger firstRunLength = countAscendizedRun(buf, r, descOrComp, ty, ctx);
-
-  if (r.length == firstRunLength)
-    {
-      // In this case, we have already sorted the array here.
-      return;
-    }
-  internalBinarySort(buf, r, (r.location + firstRunLength),
-    descOrComp, ty, ctx);
-}
-
 /*
  * Galloping from left searches for an insertion point for key into the
  * already sorted buffer and returns the point immediately left of the first
@@ -303,9 +152,9 @@ gallopLeft(id key, id *buf, NSRange r, NSUInteger hint, id descOrComp,
    */
   offset = MIN(offset, NSMaxRange(r));
   if (lastOffset < (NSInteger)r.location)
-  {
-    lastOffset = (NSInteger)r.location;
-  }
+    {
+      lastOffset = (NSInteger)r.location;
+    }
   while (lastOffset < offset)
     {
       NSInteger midPoint = lastOffset + ((offset - lastOffset) >> 1);
@@ -447,6 +296,156 @@ NSRange range, NSComparator cmptr)
 }
 
 #if GS_USE_TIMSORT
+
+static inline void
+reverseRange(id *buffer, NSRange r)
+{
+  NSUInteger loc = r.location;
+  NSUInteger max = (NSMaxRange(r) - 1);
+
+  while (loc < max)
+    {
+      id temp = buffer[loc];
+      buffer[loc++] = buffer[max];
+      buffer[max--] = temp;
+    }
+}
+
+/* In-place binary insertion sorting for small arrays (i.e. those which are
+ * smaller than GS_MIN_MERGE. We use this to generate minimal runs for timsort.
+ */
+static void
+internalBinarySort(id *buffer,
+  NSRange r,
+  NSUInteger start,
+  id compOrDesc,
+  GSComparisonType type,
+  void *context)
+{
+  NSUInteger min = r.location;
+  NSUInteger max = NSMaxRange(r);
+
+  NSCAssert2(NSLocationInRange(start, r),
+    @"Start index %lu not in range %@",
+    start, NSStringFromRange(r));
+
+  if (min == start)
+    {
+      start++;
+    }
+  // We assume that everything before start is sorted.
+  for (; start < max; ++start)
+    {
+      NSUInteger left = min;
+      NSUInteger right = start;
+      id pivot = buffer[right];
+      int i = 0;
+
+      do
+        {
+          NSUInteger midPoint = (left + ((right - left) >> 1));
+          NSComparisonResult res = GSCompareUsingDescriptorOrComparator(pivot,
+            buffer[midPoint],
+            compOrDesc,
+            type,
+            context);
+          if (NSOrderedAscending == res)
+            {
+              right = midPoint;
+            }
+          else
+            {
+              left = midPoint + 1;
+            }
+        } while (left < right);
+      NSCAssert(left == right, @"Binary sort iteration did not end correctly,");
+      // We make room for the pivot and place it at left.
+      for (i = start; i > left; --i)
+        {
+          buffer[i] = buffer[(i - 1)];
+        }
+      buffer[left] = pivot;
+    }
+}
+
+
+/*
+ * Count the number of elements in the range that are already ordered.
+ * If the order is a descending one, reverse it so that all runs are ordered the
+ * same way.
+ */
+static inline NSUInteger
+countAscendizedRun(id *buf, NSRange r, id descOrComp,
+  GSComparisonType type, void*context)
+{
+  NSUInteger min = r.location;
+  NSUInteger runMax = min + 1;
+  NSUInteger rangeMax = NSMaxRange(r);
+
+  if (runMax == rangeMax)
+    {
+      return 1;
+    }
+  if (NSOrderedDescending == GSCompareUsingDescriptorOrComparator(buf[min],
+    buf[runMax++], descOrComp, type, context))
+    {
+      while ((runMax < rangeMax) && NSOrderedDescending
+        == GSCompareUsingDescriptorOrComparator(buf[runMax - 1],
+        buf[runMax], descOrComp, type, context))
+        {
+          runMax++;
+        }
+      reverseRange(buf, NSMakeRange(min, (runMax - min)));
+    }
+  else // ascending or equal
+    {
+      while ((runMax < rangeMax) && NSOrderedDescending
+        != GSCompareUsingDescriptorOrComparator(buf[runMax - 1],
+        buf[runMax], descOrComp, type, context))
+        {
+          runMax++;
+        }
+    }
+  return (runMax - min);
+}
+
+/*
+ * Calculate a sensible minimum length for the runs, these need to be powers of
+ * two, or less than, but close to, one, but always at least GS_MIN_MERGE. For
+ * details on why this is useful, see Python's listsort.txt.
+ */
+static inline NSUInteger
+minimumRunLength(NSUInteger length)
+{
+  NSUInteger r = 0;
+
+  while (length >= GS_MIN_MERGE)
+    {
+      r |= length & 1;
+      length >>= 1;
+    }
+
+  return (length + r);
+}
+
+/*
+ * For arrays up to GS_MIN_MERGE, we don't do merging. Instead, we identify
+ * pre-ordering at the begining of the range and sort the rest using binary
+ * sort.
+ */
+static inline void
+miniTimSort(id *buf, NSRange r, id descOrComp, GSComparisonType ty, void *ctx)
+{
+  NSUInteger firstRunLength = countAscendizedRun(buf, r, descOrComp, ty, ctx);
+
+  if (r.length == firstRunLength)
+    {
+      // In this case, we have already sorted the array here.
+      return;
+    }
+  internalBinarySort(buf, r, (r.location + firstRunLength),
+    descOrComp, ty, ctx);
+}
 
 /* These macros make calling the cached IMPs easier,
  * if we choose to do so later.
@@ -754,7 +753,8 @@ descriptorOrComparator: (id)descriptorOrComparator
                */
               localMinGallop -= localMinGallop > 1;
               minGallop = localMinGallop;
-              k = gallopRight(*buf2, buf1, NSMakeRange(0,num1), 0, descOrComp, ty, ctx);
+              k = gallopRight(*buf2, buf1,
+                NSMakeRange(0,num1), 0, descOrComp, ty, ctx);
               winners1 = k;
               if (0 != k)
                 {
@@ -1011,7 +1011,6 @@ descriptorOrComparator: (id)descriptorOrComparator
   NS_ENDHANDLER
 }
 
-
 - (void) mergeAtIndex: (NSUInteger)i
 {
   NSRange r1;
@@ -1023,7 +1022,8 @@ descriptorOrComparator: (id)descriptorOrComparator
 
   r1 = runStack[i];
   r2 = runStack[i+1];
-  NSDebugMLLog(@"GSTimSort", @"Merging stack location %lu (stack size: %lu, run %@ with %@)", i,
+  NSDebugMLLog(@"GSTimSort",
+    @"Merging stack location %lu (stack size: %lu, run %@ with %@)", i,
     stackSize, NSStringFromRange(r1), NSStringFromRange(r2));
 
   /* Do some housekeeping on the stack: We combine the two runs
@@ -1045,11 +1045,12 @@ descriptorOrComparator: (id)descriptorOrComparator
   r1.length = r1.length - (insert - r1.location);
   r1.location = insert;
   if (r1.length == 0)
-  {
-    // The entire run r2 lies after r1, just return.
-    return;
-  }
-  NSDebugMLLog(@"GSTimSort", @"Insertion point for r2 in r1: %lu, r1 for the merge is now %@.",
+    {
+      // The entire run r2 lies after r1, just return.
+      return;
+    }
+  NSDebugMLLog(@"GSTimSort",
+    "Insertion point for r2 in r1: %lu, r1 for the merge is now %@.",
     insert, NSStringFromRange(r1));
 
   // Find an insertion point for the last element of r1 into r2. Subtracting the
@@ -1064,7 +1065,9 @@ descriptorOrComparator: (id)descriptorOrComparator
       return;
     }
 
-  (r1.length <= r2.length) ? GS_TIMSORT_MERGE_LOW(self, r1, r2) : GS_TIMSORT_MERGE_HIGH(self, r1, r2);
+  (r1.length <= r2.length)
+    ? GS_TIMSORT_MERGE_LOW(self, r1, r2)
+    : GS_TIMSORT_MERGE_HIGH(self, r1, r2);
 }
 
 /**
@@ -1084,8 +1087,6 @@ descriptorOrComparator: (id)descriptorOrComparator
     }
 }
 
-
-
 - (void) dealloc
 {
   free(runStack);
@@ -1093,8 +1094,8 @@ descriptorOrComparator: (id)descriptorOrComparator
   [super dealloc];
 }
 
-
 @end
+
 static void
 _GSTimSort(id *objects,
   NSRange sortRange,

Headers/Foundation/NSNotification.h
@@ -70,7 +70,7 @@ DEFINE_BLOCK_TYPE(GSNotificationBlock, void, NSNotification *);
 {
 #if	GS_EXPOSE(NSNotificationCenter)
 @private
-  GS_GC_STRONG void	*_table;
+  void	*_table;
 #endif
 }
 

Headers/Foundation/NSZone.h
@@ -308,14 +308,14 @@ enum {
  * garbage collected itsself.<br />
  * In any case the memory returned is zero'ed.
  */
-GS_EXPORT GS_GC_STRONG void *
+GS_EXPORT void *
 NSAllocateCollectable(NSUInteger size, NSUInteger options);
 
 /** Reallocate memory to be of a different size and/or to have different
  * options settings.  The behavior of options is as for
  * the NSAllocateCollectable() function.
  */ 
-GS_EXPORT GS_GC_STRONG void *
+GS_EXPORT void *
 NSReallocateCollectable(void *ptr, NSUInteger size, NSUInteger options);
 
 #endif

Headers/GNUstepBase/GSConfig.h.in
@@ -419,32 +419,16 @@ typedef	struct {
 #include <stdbool.h>
 #endif
 
-// Strong has different semantics in GC and ARC modes, so we need to have a
-// macro that picks the correct one.
-#if __OBJC_GC__
-#  define GS_GC_STRONG __strong
-#else
-#  define GS_GC_STRONG
-#endif
-
 #if !__has_feature(objc_arc)
 // NetBSD > 6 defines __weak in cdefs_elf.h
 #ifdef __NetBSD__
 #undef __weak
 #endif
 #  if !defined(__weak)
-#    if __OBJC_GC__
-#      define __weak __attribute__((objc_gc(weak)))
-#    else
-#      define __weak
-#    endif
+#    define __weak
 #  endif
 #  if !defined(__strong)
-#    if __OBJC_GC__
-#      define __strong __attribute__((objc_gc(strong)))
-#    else
-#      define __strong
-#    endif
+#    define __strong
 #  endif
 #endif
 

Source/GSQuickSort.m
@@ -31,7 +31,12 @@
 #import "Foundation/NSObjCRuntime.h"
 #import "GSSorting.h"
 
-/// Swaps the two provided objects.
+/**
+ * Sorts the provided object array's sortRange according to sortDescriptor.
+ */
+// Quicksort algorithm copied from Wikipedia :-).
+#if GS_USE_QUICKSORT
+
 static inline void
 SwapObjects(id * o1, id * o2)
 {
@@ -42,11 +47,6 @@ SwapObjects(id * o1, id * o2)
   *o2 = temp;
 }
 
-/**
- * Sorts the provided object array's sortRange according to sortDescriptor.
- */
-// Quicksort algorithm copied from Wikipedia :-).
-#if GS_USE_QUICKSORT
 static void
 _GSQuickSort(id *objects,
   NSRange sortRange,

Source/GSString.m
@@ -895,7 +895,7 @@ tsbytes(uintptr_t s, char *buf)
       maxLength /= 2;
       if (maxLength > 1)
 	{
-          unichar       *buf = (unichar*)buffer;
+          unichar       *buf = (unichar*)(void*)buffer;
 
           if (maxLength <= length)
             {

Source/NSAutoreleasePool.m
@@ -42,157 +42,6 @@
 #  endif
 #endif
 
-#if __OBJC_GC__
-
-@interface GSAutoreleasePool : NSAutoreleasePool @end
-
-@implementation NSAutoreleasePool
-
-static NSAutoreleasePool	*pool = nil;
-
-static Class PoolClass;
-
-+ (void) initialize
-{
-  if ([NSGarbageCollector defaultCollector])
-    {
-      pool = NSAllocateObject(self, 0, NSDefaultMallocZone());
-    }
-  else
-    {
-      PoolClass = [GSAutoreleasePool class];
-    }
-  return;
-}
-
-+ (id) allocWithZone: (NSZone*)zone
-{
-  if (nil == pool)
-    {
-      return NSAllocateObject(PoolClass, 0, 0);
-    }
-  return pool;
-}
-
-+ (id) new
-{
-  if (nil == pool)
-    {
-      return [NSAllocateObject(PoolClass, 0, 0) init];
-    }
-  return pool;
-}
-
-- (id) init
-{
-  return self;
-}
-
-- (unsigned) autoreleaseCount
-{
-  return 0;
-}
-
-- (unsigned) autoreleaseCountForObject: (id)anObject
-{
-  return 0;
-}
-
-+ (unsigned) autoreleaseCountForObject: (id)anObject
-{
-  return 0;
-}
-
-+ (id) currentPool
-{
-  return pool;
-}
-
-+ (void) addObject: (id)anObj
-{
-  return;
-}
-
-- (void) addObject: (id)anObj
-{
-  return;
-}
-
-- (void) drain
-{
-  static NSGarbageCollector	*collector = nil;
-  static SEL			sel;
-  static IMP			imp;
-
-  if (collector == nil)
-    {
-      collector = [NSGarbageCollector defaultCollector];
-      sel = @selector(collectIfNeeded);
-      imp = [collector methodForSelector: sel];
-    }
-  (*imp)(collector, sel);
-}
-
-- (id) retain
-{
-  [NSException raise: NSGenericException
-	       format: @"Don't call `-retain' on a NSAutoreleasePool"];
-  return self;
-}
-
-- (oneway void) release
-{
-  return;
-}
-
-- (void) dealloc
-{
-  [NSException raise: NSGenericException
-    format: @"dealloc should not be called in garbage collected mode"];
-  GSNOSUPERDEALLOC;
-  return;
-}
-
-- (void) emptyPool
-{
-  return;
-}
-
-- (id) autorelease
-{
-  [NSException raise: NSGenericException
-	       format: @"Don't call `-autorelease' on a NSAutoreleasePool"];
-  return self;
-}
-
-+ (void) _endThread: (NSThread*)thread
-{
-  return;
-}
-
-+ (void) enableRelease: (BOOL)enable
-{
-  return;
-}
-
-+ (void) freeCache
-{
-  return;
-}
-
-+ (void) setPoolCountThreshhold: (unsigned)c
-{
-  return;
-}
-
-+ (void) setPoolNumberThreshhold: (unsigned)c
-{
-  return;
-}
-
-@end
-
-#endif
 
 
 
@@ -278,11 +127,7 @@ pop_pool_from_cache (struct autorelease_thread_vars *tv)
 }
 
 
-#if __OBJC_GC__
-@implementation GSAutoreleasePool
-#else
 @implementation NSAutoreleasePool
-#endif
 
 + (void) initialize
 {

Source/NSConcretePointerFunctions.h
@@ -29,16 +29,7 @@
 #  include <objc/capabilities.h>
 #endif
 
-// Define a weak read barrier macro for ARC or GC, depending on which one this
-// target supports.  If this target doesn't support zeroing weak references,
-// then use an unsafe unretained access.
-#if __OBJC_GC__
-#  include <objc/objc-auto.h>
-#  define WEAK_READ(x) objc_read_weak((id*)x)
-#  define WEAK_WRITE(addr, x) objc_assign_weak((id)x, (id*)addr)
-#  define STRONG_WRITE(addr, x) objc_assign_strongCast((id)x, (id*)addr)
-#  define STRONG_ACQUIRE(x) x
-#elif defined(OBJC_CAP_ARC)
+#if defined(OBJC_CAP_ARC)
 #    include <objc/objc-arc.h>
 #    define ARC_WEAK_READ(x) objc_loadWeak((id*)x)
 #    define ARC_WEAK_WRITE(addr, x) objc_storeWeak((id*)addr, (id)x)

commit 8cccc932bdbc5efce44b05627d077541f1ae225d

Fixup breakage introduced by r39762 and r39764, which apparently were never attempted to compile
git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@39990 72102866-910b-0410-8b05-ffd578937521

Source/GSTimSort.m
@@ -1050,7 +1050,7 @@ descriptorOrComparator: (id)descriptorOrComparator
       return;
     }
   NSDebugMLLog(@"GSTimSort",
-    "Insertion point for r2 in r1: %lu, r1 for the merge is now %@.",
+    @"Insertion point for r2 in r1: %lu, r1 for the merge is now %@.",
     insert, NSStringFromRange(r1));
 
   // Find an insertion point for the last element of r1 into r2. Subtracting the

Source/NSSortDescriptor.m
@@ -42,7 +42,7 @@ static BOOL     initialized = NO;
 #endif
 
 #if     GS_USE_TIMSORT
-@interface GSTimSortPlaceHolder : NSObject
+@interface GSTimSortDescriptor : NSObject
 @end
 #endif
 #if     GS_USE_QUICKSORT