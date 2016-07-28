de3e9739ab307f9f44610e74c6abe44a4e8c544e
Richard Frith-MacDonald <rfm@gnu.org>
2016-07-28 05:21:01 +0000

    A case insensitive compare with self is always NSOrderedSame
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@40041 72102866-910b-0410-8b05-ffd578937521

--- Source/NSString.m
+++ Source/NSString.m
@@ -5580,6 +5580,7 @@ static NSFileManager *fm = nil;
  */
 - (NSComparisonResult) caseInsensitiveCompare: (NSString*)aString
 {
+  if (string == self) return NSOrderedSame;
   return [self compare: aString
 	       options: NSCaseInsensitiveSearch
 		 range: ((NSRange){0, [self length]})];
