1fd37a2b660396d496a011c1e4ff2ebea6e5cabd
Marcian Lytwyn <gna@advcsi.com>
2016-08-08 21:34:16 +0000

    Update for redirecting from subsystem:windows application
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/branches/gnustep_testplant_branch@40055 72102866-910b-0410-8b05-ffd578937521

--- Source/NSDistributedNotificationCenter.m
+++ Source/NSDistributedNotificationCenter.m
@@ -737,8 +737,8 @@ static NSDistributedNotificationCenter	*netCenter = nil;
 	    }
 #if defined(__MINGW32__)
     NSTask *task = AUTORELEASE([NSTask new]);
-    [task setStandardError:[NSFileHandle fileHandleForWritingAtPath:@"CON"]];
-    [task setStandardOutput:[NSFileHandle fileHandleForWritingAtPath:@"CON"]];
+    [task setStandardError:[NSFileHandle fileHandleForWritingAtPath:@"NUL"]];
+    [task setStandardOutput:[NSFileHandle fileHandleForWritingAtPath:@"NUL"]];
     [task setLaunchPath:cmd];
     [task setArguments:args];
     [task launch];
