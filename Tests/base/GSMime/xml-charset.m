b1c979898def91df05834ffa54454c2b97718970
Niels Grewe <thebeing+gna@halbordnung.de>
2016-09-19 15:56:51 +0000

    Fix detection of the charset in XML data
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@40090 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/GSMime/xml-charset.m
+++ Tests/base/GSMime/xml-charset.m
@@ -8,9 +8,10 @@ int main()
   NSAutoreleasePool   *arp = [NSAutoreleasePool new];
   NSString *xml = @"<?xml version=\"1.0\" encoding=\"UTF-8\"?><html></html>";
   NSString *charset = nil;
-  testHopeful = YES;
-  PASS_RUNS(charset = [GSMimeDocument charsetForXml: xml], "Can determine cahrset of xml document.");
+  PASS_RUNS(charset = [GSMimeDocument charsetForXml: xml], "Can determine charset of xml document.");
+  PASS_EQUAL(@"UTF-8", charset, "Charset detected correctly");
   DESTROY(arp);
+  return 0;
 }
 #else
 int main(int argc,char **argv)
