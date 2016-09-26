6ba57fe92b508f0a63da81844821607443d4b0c2
rfm <rfm@72102866-910b-0410-8b05-ffd578937521>
2016-09-26 11:04:51 +0000

    add fffd and ffff
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/base/trunk@40108 72102866-910b-0410-8b05-ffd578937521

--- Tests/base/NSString/noncharacter.m
+++ Tests/base/NSString/noncharacter.m
@@ -9,6 +9,18 @@ int main(int argc, char **argv)
   NSString *str;
   unichar u;
 
+  u = (unichar)0xfffd;
+  str = [[NSString alloc] initWithCharacters: &u length: 1];
+  PASS([str length] == 1, "fffd codpepoint is permitted in string");
+  PASS([str characterAtIndex: 0] == 0xfffd, "fffd is returned properly");
+  [str release];
+
+  u = (unichar)0xfffe;
+  str = [[NSString alloc] initWithCharacters: &u length: 1];
+  PASS([str length] == 1, "fffe codpepoint is permitted in string");
+  PASS([str characterAtIndex: 0] == 0xfffe, "fffe is returned properly");
+  [str release];
+
   u = (unichar)0xffff;
   str = [[NSString alloc] initWithCharacters: &u length: 1];
   PASS([str length] == 1, "ffff codpepoint is permitted in string");
