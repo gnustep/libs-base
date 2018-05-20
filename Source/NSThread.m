0e6865a3fdac043ab4c13e00e5900946b9cc4fe7
Ivan Vučica <ivan@vucica.net>
2018-05-20 12:40:24 +0100

    Fix typo in code branch for platforms without pthread_spin_lock().
    Check for value of HAVE_PTHREAD_SPIN_LOCK instead of whether the
    macro is defined.

--- Source/NSThread.m
+++ Source/NSThread.m
@@ -37,7 +37,7 @@
 
 // Dummy implementatation
 // cleaner than IFDEF'ing the code everywhere
-#ifndef HAVE_PTHREAD_SPIN_LOCK
+#if !(HAVE_PTHREAD_SPIN_LOCK)
 #warning no spin_locks, using dummy versions
 typedef int pthread_spinlock_t;
 int pthread_spin_init(pthread_spinlock_t *lock, int pshared
@@ -51,7 +51,7 @@ int pthread_spin_lock(pthread_spinlock_t *lock)
 }
 int pthread_spin_unlock(pthread_spinlock_t *lock)
 {
-  return 0;c
+  return 0;
 }
 int pthread_spin_destroy(pthread_spinlock_t *lock)
 {
