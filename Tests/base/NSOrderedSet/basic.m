48cf585c463e2ca758c6bb8cb17223a0204b037b
Gregory John Casamento <greg.casamento@gmail.com>
2019-06-08 08:56:18 -0400

    Rudimentary NSOrderedSet tests

--- Tests/base/NSOrderedSet/basic.m
+++ Tests/base/NSOrderedSet/basic.m
@@ -1,14 +1,11 @@
 #import "ObjectTesting.h"
 #import <Foundation/NSAutoreleasePool.h>
-/*
 #import <Foundation/NSOrderedSet.h>
- */
 
 int main()
 {
   START_SET("NSOrderedSet base")
 
-/*
   NSOrderedSet *testObj;
   NSMutableArray *testObjs = [NSMutableArray new];
 
@@ -17,7 +14,7 @@ int main()
   PASS(testObj != nil && [testObj count] == 0,
     "can create an empty ordered set");
    
-  testObj = [NSOrderedSet setWithObject: @"Hello"];
+  testObj = [NSOrderedSet orderedSetWithObject: @"Hello"];
   [testObjs addObject: testObj];
   PASS(testObj != nil && [testObj count] == 1,
     "can create an ordered set with one element");
@@ -26,7 +23,6 @@ int main()
   test_NSCoding(testObjs);
   test_NSCopying(@"NSOrderedSet", @"NSMutableOrderedSet", testObjs, YES, NO);
   test_NSMutableCopying(@"NSOrderedSet", @"NSMutableOrderedSet", testObjs);
-*/
 
   END_SET("NSOrderedSet base")
   return 0;
