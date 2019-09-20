aa6a0175b1f3c87345e28ba036e152c42d4cdad3
Gregory John Casamento <greg.casamento@gmail.com>
2019-09-20 14:50:54 -0400

    NSFileCoordinator skeleton compiling cleanly

--- Source/NSFileCoordinator.m
+++ Source/NSFileCoordinator.m
@@ -24,9 +24,104 @@
 */
 
 #import <Foundation/NSFileCoordinator.h>
+#import <Foundation/NSURL.h>
+#import <Foundation/NSArray.h>
 
 @implementation NSFileAccessIntent
++ (instancetype) readingIntentWithURL: (NSURL *)url
+                              options: (NSFileCoordinatorReadingOptions)options
+{
+  return nil;
+}
+
++ (instancetype) writingIntentWithURL: (NSURL *)url
+                              options: (NSFileCoordinatorWritingOptions)options
+{
+  return nil;
+}
+
+- (NSURL *) URL
+{
+  return nil;
+}
 @end
 
 @implementation NSFileCoordinator
+
++ (NSArray *) filePresenters
+{
+  return nil;
+}
+
+- (NSString *) purposeIdentifier
+{
+  return nil;
+}
+
+- (void) setPurposeIdentifier: (NSString *)ident  // copy
+{
+}
+
+- (void)cancel
+{
+}
+
+- (void)coordinateAccessWithIntents: (NSArray *)intents
+                              queue: (NSOperationQueue *)queue
+                         byAccessor: (GSAccessorCallbackHandler)accessor
+{
+}
+
+- (void)coordinateReadingItemAtURL: (NSURL *)readingURL
+                           options: (NSFileCoordinatorReadingOptions)readingOptions
+                  writingItemAtURL: (NSURL *)writingURL
+                           options: (NSFileCoordinatorWritingOptions)writingOptions
+                             error: (NSError **)outError
+                        byAccessor: (GSNoEscapeReadWriteHandler)readerWriter
+{
+}
+                 
+- (void)coordinateReadingItemAtURL: (NSURL *)url
+                           options: (NSFileCoordinatorReadingOptions)options
+                             error: (NSError **)outError
+                        byAccessor: (GSNoEscapeNewURLHandler)reader
+{
+}
+                 
+- (void)coordinateWritingItemAtURL: (NSURL *)url
+                           options: (NSFileCoordinatorWritingOptions)options error:(NSError **)outError
+                        byAccessor: (GSNoEscapeNewURLHandler)writer
+{
+}
+
+- (void)coordinateWritingItemAtURL: (NSURL *)url1
+                           options: (NSFileCoordinatorWritingOptions)options1
+                  writingItemAtURL: (NSURL *)url2
+                           options: (NSFileCoordinatorWritingOptions)options2
+                             error: (NSError **)outError
+                        byAccessor: (GSDualWriteURLCallbackHandler)writer
+{
+}
+
+- (void)itemAtURL:(NSURL *)oldURL didMoveToURL: (NSURL *)newURL
+{
+}
+
+- (void)itemAtURL:(NSURL *)oldURL willMoveToURL: (NSURL *)newURL 
+{
+}
+
+- (void)itemAtURL:(NSURL *)url didChangeUbiquityAttributes: (NSSet *)attributes
+{
+}
+
+- (void)prepareForReadingItemsAtURLs: (NSArray *)readingURLs
+                             options: (NSFileCoordinatorReadingOptions)readingOptions
+                  writingItemsAtURLs: (NSArray *)writingURLs
+                             options: (NSFileCoordinatorWritingOptions)writingOptions
+                               error: (NSError **)outError
+                          byAccessor: (GSAccessorHandlerBlock)batchAccessor
+{
+}
+
 @end
