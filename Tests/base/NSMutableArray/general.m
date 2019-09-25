ddcff45b89aaf9739e9ed322dbffdb42ff97ce45
Richard Frith-Macdonald <rfm@gnu.org>
2019-09-25 10:36:41 +0100

    add test for removing all the objects in an array. cleanup other tests a bit

--- Tests/base/NSMutableArray/general.m
+++ Tests/base/NSMutableArray/general.m
@@ -11,10 +11,12 @@ int main()
   val2 = @"A Goodbye";
   val3 = @"Testing all strings";
   
-  vals1 = [[NSMutableArray arrayWithObject:val1] retain];
-  [vals1 addObject:val2];
-  vals2 = [[vals1 arrayByAddingObject:val2] retain];
-  vals3 = [[vals1 arrayByAddingObject:val3] retain];
+  vals1 = [[NSMutableArray arrayWithObject: val1] retain];
+  [vals1 addObject: val2];
+  vals2 = AUTORELEASE([vals1 mutableCopy]);
+  [vals2 addObject: val2];
+  vals3 = AUTORELEASE([vals2 mutableCopy]);
+  [vals3 addObject: val3];
   
   obj = [NSMutableArray new];
   arr = obj;
@@ -96,6 +98,21 @@ int main()
          [a objectAtIndex:0] == val2 && [a objectAtIndex:1] == val1,
 	 "-sortedArrayUsingSelector: seems ok");
 
+  }
+  {
+    NSMutableArray *ma = [NSMutableArray new];
+    NSString	*s[5] = { @"1",@"2",@"3",@"4",@"5" };
+    NSUInteger	before;
+    NSUInteger	after;
+    for (int i = 0; i < 5; i++)
+      {
+	[ma addObject: s[i]];
+      }
+    before = [ma count];
+    [ma removeObjectsInArray: ma];
+    after = [ma count];
+    [ma release];
+    PASS(5 == before && 0 == after, "-removeObjectsInArray: works for self")
   }
   [arp release]; arp = nil;
   return 0;
