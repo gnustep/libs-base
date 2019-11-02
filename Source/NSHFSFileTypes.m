e6a764d7f99a6c8f5210e6acc02a82c3508d67a9
Gregory John Casamento <greg.casamento@gmail.com>
2019-11-02 19:13:13 -0400

    Slightly modified to return bogus results

--- Source/NSHFSFileTypes.m
+++ Source/NSHFSFileTypes.m
@@ -1,7 +1,7 @@
 /* Implementation of class NSHFSFileTypes
    Copyright (C) 2019 Free Software Foundation, Inc.
    
-   By: heron
+   By: Gregory John Casamento <greg.casamento@gmail.com>
    Date: Fri Nov  1 00:25:22 EDT 2019
 
    This file is part of the GNUstep Library.
