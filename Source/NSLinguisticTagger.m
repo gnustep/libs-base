48e923168b595cfc99f97ad2c05cc370aa0c3e2e
Gregory John Casamento <greg.casamento@gmail.com>
2019-11-05 04:35:21 -0500

    Add NSLinguisticTagger skeleton

--- Source/NSLinguisticTagger.m
+++ Source/NSLinguisticTagger.m
@@ -23,8 +23,169 @@
 */
 
 #include <Foundation/NSLinguisticTagger.h>
+#include <Foundation/NSRange.h>
+#include <Foundation/NSString.h>
+#include <Foundation/NSDictionary.h>
+#include <Foundation/NSArray.h>
+#include <Foundation/NSOrthography.h>
 
 @implementation NSLinguisticTagger
 
+- (instancetype) initWithTagSchemes: (NSArray *)tagSchemes
+                            options: (NSUInteger)opts
+{
+  return nil;
+}
+
+- (NSArray *) tagSchemes
+{
+  return nil;
+}
+
+- (NSString *) string
+{
+  return nil;
+}
+
+- (void) setString: (NSString *)string
+{
+}
+  
++ (NSArray *) availableTagSchemesForUnit: (NSLinguisticTaggerUnit)unit
+                                language: (NSString *)language
+{
+  return nil;
+}
+
++ (NSArray *) availableTagSchemesForLanguage: (NSString *)language
+{
+  return nil;
+}
+
+- (void) setOrthography: (NSOrthography *)orthography
+                  range: (NSRange)range
+{
+}
+  
+- (NSOrthography *) orthographyAtIndex: (NSUInteger)charIndex
+                        effectiveRange: (NSRangePointer)effectiveRange
+{
+  return nil;
+}
+
+- (void) stringEditedInRange: (NSRange)newRange
+              changeInLength: (NSInteger)delta
+{
+}
+  
+- (NSRange) tokenRangeAtIndex: (NSUInteger)charIndex
+                         unit: (NSLinguisticTaggerUnit)unit
+{
+  return NSMakeRange(0,0);
+}
+  
+- (NSRange) sentenceRangeForRange: (NSRange)range
+{
+  return NSMakeRange(0,0);
+}
+
+- (void) enumerateTagsInRange: (NSRange)range
+                         unit: (NSLinguisticTaggerUnit)unit
+                       scheme: (NSLinguisticTagScheme)scheme
+                      options: (NSLinguisticTaggerOptions)options
+                   usingBlock: (GSLinguisticTagRangeBoolBlock)block
+{
+}
+
+- (NSLinguisticTag) tagAtIndex: (NSUInteger)charIndex
+                          unit: (NSLinguisticTaggerUnit)unit
+                        scheme: (NSLinguisticTagScheme)scheme
+                    tokenRange: (NSRangePointer)tokenRange
+{
+  return nil;
+}
+
+- (NSArray *) tagsInRange: (NSRange)range
+                     unit: (NSLinguisticTaggerUnit)unit
+                   scheme: (NSLinguisticTagScheme)scheme
+                  options: (NSLinguisticTaggerOptions)options
+              tokenRanges: (NSArray **)tokenRanges
+{
+  return nil;
+}
+
+- (void) enumerateTagsInRange: (NSRange)range
+                       scheme: (NSLinguisticTagScheme)tagScheme
+                      options: (NSLinguisticTaggerOptions)opts
+                   usingBlock: (GSLinguisticTagRangeRangeBoolBlock)block
+{
+}
+
+- (NSLinguisticTag) tagAtIndex: (NSUInteger)charIndex
+                        scheme: (NSLinguisticTagScheme)scheme
+                    tokenRange: (NSRangePointer)tokenRange
+                 sentenceRange: (NSRangePointer)sentenceRange
+{
+  return nil;
+}
+  
+- (NSArray *) tagsInRange: (NSRange)range
+                   scheme: (NSString *)tagScheme
+                  options: (NSLinguisticTaggerOptions)opts
+              tokenRanges: (NSArray **)tokenRanges
+{
+  return nil;
+}
+
+- (NSString *) dominantLanguage
+{
+  return nil;
+}
+
++ (NSString *) dominantLanguageForString: (NSString *)string
+{
+  return nil;
+}
+
++ (NSLinguisticTag) tagForString: (NSString *)string
+                         atIndex: (NSUInteger)charIndex
+                            unit: (NSLinguisticTaggerUnit)unit
+                          scheme: (NSLinguisticTagScheme)scheme
+                     orthography: (NSOrthography *)orthography
+                      tokenRange: (NSRangePointer)tokenRange
+{
+  return nil;
+}
+  
++ (NSArray *)tagsForString: (NSString *)string
+                     range: (NSRange)range
+                      unit: (NSLinguisticTaggerUnit)unit
+                    scheme: (NSLinguisticTagScheme)scheme
+                   options: (NSLinguisticTaggerOptions)options
+               orthography: (NSOrthography *)orthography
+               tokenRanges: (NSArray **)tokenRanges
+{
+  return nil;
+}
+
++ (void) enumerateTagsForString: (NSString *)string
+                          range: (NSRange)range
+                           unit: (NSLinguisticTaggerUnit)unit
+                         scheme: (NSLinguisticTagScheme)scheme
+                        options: (NSLinguisticTaggerOptions)options
+                    orthography: (NSOrthography *)orthography
+                     usingBlock: (GSLinguisticTagRangeBoolBlock)block
+{
+}
+  
+
+- (NSArray *) possibleTagsAtIndex: (NSUInteger)charIndex
+                           scheme: (NSString *)tagScheme
+                       tokenRange: (NSRangePointer)tokenRange
+                    sentenceRange: (NSRangePointer)sentenceRange
+                           scores: (NSArray **)scores
+{
+  return nil;
+}
 @end
 
