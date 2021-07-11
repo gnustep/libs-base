e002a2d1ef587773af85898267adf51def3f5242
Gregory John Casamento <greg.casamento@gmail.com>
2021-07-11 09:42:30 -0400

    Build base and test

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -32,3 +32,20 @@ fi;
 
 # Install dependencies
 ./.github/scripts/dependencies.sh
+
+# before script
+export LIBRARY_PATH=$HOME/staging/lib:$HOME/staging/lib64:$LIBRARY_PATH;
+export LD_LIBRARY_PATH=$HOME/staging/lib:$HOME/staging/lib64:$LD_LIBRARY_PATH;
+if [ $LIBRARY_COMBO = 'ng-gnu-gnu' ];
+then
+  export CPATH=$HOME/staging/include;
+else
+  export CPATH=/usr/lib/gcc/x86_64-linux-gnu/4.8/include;
+fi;
+export PATH=$HOME/staging/bin:$PATH;
+export GNUSTEP_MAKEFILES=$HOME/staging/share/GNUstep/Makefiles;
+. $HOME/staging/share/GNUstep/Makefiles/GNUstep.sh;
+
+# script
+./configure $BASE_ABI || (cat config.log && false);
+make && make install && make check || (cat Tests/tests.log && false);
