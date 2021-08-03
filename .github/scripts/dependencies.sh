f5113fe460dbdb05c2cda4cb2178448badb0472b
Frederik Seiffert <frederik@algoriddim.com>
2021-08-03 23:48:49 +0200

    CI: add Windows MSVC builds

--- .github/scripts/dependencies.sh
+++ .github/scripts/dependencies.sh
@@ -3,24 +3,28 @@
 set -ex
 
 install_gnustep_make() {
-    cd $DEP_SRC
+    echo "::group::GNUstep Make"
+    cd $DEPS_PATH
     git clone https://github.com/gnustep/tools-make.git
     cd tools-make
-    if [ -n "$RUNTIME_VERSION" ]
-    then
-        WITH_RUNTIME_ABI="--with-runtime-abi=${RUNTIME_VERSION}"
-    else
-        WITH_RUNTIME_ABI=""
+    MAKE_OPTS=
+    if [ -n "$HOST" ]; then
+      MAKE_OPTS="$MAKE_OPTS --host=$HOST"
     fi
-    ./configure --prefix=$DEP_ROOT --with-library-combo=$LIBRARY_COMBO $WITH_RUNTIME_ABI
+    if [ -n "$RUNTIME_VERSION" ]; then
+      MAKE_OPTS="$MAKE_OPTS --with-runtime-abi=$RUNTIME_VERSION"
+    fi
+    ./configure --prefix=$INSTALL_PATH --with-library-combo=$LIBRARY_COMBO $MAKE_OPTS
     make install
 
     echo Objective-C build flags:
-    $DEP_ROOT/bin/gnustep-config --objc-flags
+    $INSTALL_PATH/bin/gnustep-config --objc-flags
+    echo "::endgroup::"
 }
 
-install_ng_runtime() {
-    cd $DEP_SRC
+install_libobjc2() {
+    echo "::group::libobjc2"
+    cd $DEPS_PATH
     git clone https://github.com/gnustep/libobjc2.git
     cd libobjc2
     git submodule sync
@@ -31,13 +35,15 @@ install_ng_runtime() {
       -DTESTS=off \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DGNUSTEP_INSTALL_TYPE=NONE \
-      -DCMAKE_INSTALL_PREFIX:PATH=$DEP_ROOT \
+      -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_PATH \
       ../
     make install
+    echo "::endgroup::"
 }
 
 install_libdispatch() {
-    cd $DEP_SRC
+    echo "::group::libdispatch"
+    cd $DEPS_PATH
     # will reference upstream after https://github.com/apple/swift-corelibs-libdispatch/pull/534 is merged
     git clone -b system-blocksruntime https://github.com/ngrewe/swift-corelibs-libdispatch.git libdispatch
     mkdir libdispatch/build
@@ -46,20 +52,21 @@ install_libdispatch() {
     cmake \
       -DBUILD_TESTING=off \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DCMAKE_INSTALL_PREFIX:PATH=$DEP_ROOT \
+      -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_PATH \
       -DCMAKE_C_FLAGS="-Wno-error=void-pointer-to-int-cast" \
       -DINSTALL_PRIVATE_HEADERS=1 \
-      -DBlocksRuntime_INCLUDE_DIR=$DEP_ROOT/include \
-      -DBlocksRuntime_LIBRARIES=$DEP_ROOT/lib/libobjc.so \
+      -DBlocksRuntime_INCLUDE_DIR=$INSTALL_PATH/include \
+      -DBlocksRuntime_LIBRARIES=$INSTALL_PATH/lib/libobjc.so \
       ../
     make install
+    echo "::endgroup::"
 }
 
-mkdir -p $DEP_SRC
+mkdir -p $DEPS_PATH
 
-if [ "$LIBRARY_COMBO" = 'ng-gnu-gnu' ]
-then
-    install_ng_runtime
+# Windows MSVC toolchain uses tools-windows-msvc scripts to install non-GNUstep dependencies
+if [ "$LIBRARY_COMBO" = "ng-gnu-gnu" -a "$IS_WINDOWS_MSVC" != "true" ]; then
+    install_libobjc2
     install_libdispatch
 fi
 
