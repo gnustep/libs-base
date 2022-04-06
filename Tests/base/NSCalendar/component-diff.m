9cc72669317cdce1ab57ea7299b73d1f5c378caf
Gregory John Casamento <greg.casamento@gmail.com>
2022-04-06 16:01:41 -0400

    Move era/hour/min/sec tests to it's own file.

--- Tests/base/NSCalendar/component-diff.m
+++ Tests/base/NSCalendar/component-diff.m
@@ -53,39 +53,6 @@ int main()
   PASS([comps month] == NSNotFound, "no month returned if not requested");
   PASS([comps day] == 33, "day difference without larger unit correct");
 
-  
-  /* Test getEra:year:month:day:fromDate:
-   */
-  NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-  [dateFormatter setDateFormat: @"d MMM yyyy HH:mm:ss Z"];
-  NSDate *date = [dateFormatter dateFromString:@"22 Nov 1969 08:15:00 Z"];
-
-  NSInteger era = 0;
-  NSInteger year = 0;
-  NSInteger month = 0;
-  NSInteger day = 0;
-  NSInteger hour = 0;
-  NSInteger min = 0;
-  NSInteger sec = 0;
-  NSInteger nano = 0;
-  
-  [cal getEra:&era year:&year month:&month day:&day fromDate:date];
-
-  PASS(era == 1, "getEra:year:month:day:fromDate: returns correct era");
-  PASS(year == 1969, "getEra:year:month:day:fromDate: returns correct year");
-  PASS(month == 11, "getEra:year:month:day:fromDate: returns correct month");
-  PASS(day == 22, "getEra:year:month:day:fromDate: returns correct day");
-  
-  /* Test getHour:minute:second:nanosecond:fromDate:
-   */
-  [cal getHour:&hour minute:&min second:&sec nanosecond:&nano fromDate:date];
-
-  PASS(hour == 3, "getHour:minute:second:nanosecond:fromDate: returns correct hour");
-  PASS(min == 15, "getHour:minute:second:nanosecond:fromDate: returns correct minute");
-  PASS(sec == 0, "getHour:minute:second:nanosecond:fromDate: returns correct second");
-  PASS(nano == 0, "getHour:minute:second:nanosecond:fromDate: returns correct nanosecond");
-  
-  RELEASE(dateFormatter);
   RELEASE(cal);
   
   END_SET("NSCalendar date component differences")
