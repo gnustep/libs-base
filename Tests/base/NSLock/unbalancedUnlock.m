6ed52a506ea787fd958306dbcdce8007b2319a67
Richard Frith-Macdonald <richard@localhost.localdomain>
2023-09-11 17:41:04 +0100

    Make testcases more robust.

--- Tests/base/NSLock/unbalancedUnlock.m
+++ Tests/base/NSLock/unbalancedUnlock.m
@@ -6,18 +6,19 @@ int main()
 {
   START_SET("Unbalanced unlocking")
 
-  NSLock		*lock;
+  NSLock		*lock = [NSLock new];
+  NSUserDefaults	*defs = [NSUserDefaults standardUserDefaults];
+  BOOL			mode = [defs boolForKey: @"GSMacOSXCompatible"]; 
 
-  lock = [NSLock new];
-
+  [defs setBool: NO forKey: @"GSMacOSXCompatible"];
   PASS_EXCEPTION([lock unlock], @"NSLockException",
     "unlocking an unlocked lock raises NSLockException")
 
-  [[NSUserDefaults standardUserDefaults] setBool: YES
-					  forKey: @"GSMacOSXCompatible"];
-
+  [defs setBool: YES forKey: @"GSMacOSXCompatible"];
   PASS_RUNS([lock unlock],
     "unlocking an unlocked lock does not raise in MacOSX compatibility mode")
+
+  [defs setBool: mode forKey: @"GSMacOSXCompatible"];
 
   END_SET("Unbalanced unlocking")
   return 0;
