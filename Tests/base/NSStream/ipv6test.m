51e07bc5d84b7389972af835ab91df7d38d4f2fe
Gregory John Casamento <greg.casamento@gmail.com>
2023-10-31 12:47:54 -0400

    Add test for IPv6

---
+++ Tests/base/NSStream/ipv6test.m
@@ -0,0 +1,81 @@
+#if	defined(GNUSTEP_BASE_LIBRARY)
+/**
+ * This test tests IPv6 using NSStream
+ */
+#import <Foundation/Foundation.h>
+#import "Testing.h"
+#import "ObjectTesting.h"
+
+@interface IPv6StreamTest : NSObject
+@end
+
+@implementation IPv6StreamTest
+
+- (void) runTest
+{
+  NSInputStream *inputStream;
+  NSOutputStream *outputStream;
+  
+  NSString *ipv6ServerAddress = @"your_ipv6_server_address"; // Replace with your actual IPv6 server address
+  uint16_t port = 12345; // Replace with the actual port number
+  
+  // Resolve the IPv6 address using NSHost
+  NSHost *host = [NSHost hostWithName: ipv6ServerAddress];
+  NSArray *addresses = [host addresses];
+
+  PASS([addresses count] > 0, "Resolve IPv6 address");
+  
+  NSString *ipv6Address = [addresses objectAtIndex: 0];
+  
+  [NSStream getStreamsToHost: [NSHost hostWithName: ipv6Address]
+			port: port
+		 inputStream: &inputStream
+		outputStream: &outputStream];
+  
+  [inputStream open];
+  [outputStream open];
+  
+  // Perform your tests here to validate the IPv6 stream
+  // You can write and read data from the stream and assert the expected results
+  // For example:
+  
+  NSString *testData = @"Test Data";
+  NSData *dataToWrite = [testData dataUsingEncoding:NSUTF8StringEncoding];
+  NSInteger bytesWritten = [outputStream write: [dataToWrite bytes] maxLength: [dataToWrite length]];
+  
+  PASS(bytesWritten > 0, "Write data to the stream");
+  
+  uint8_t buffer[1024];
+  NSInteger bytesRead = [inputStream read:buffer maxLength:1024];
+  NSString *receivedString = [[NSString alloc] initWithBytes:buffer length:bytesRead encoding:NSUTF8StringEncoding];
+  
+  PASS(bytesRead > 0, "Read data from the stream");
+  PASS([receivedString isEqualToString: testData], "Received data matches the expected data");
+  
+  [inputStream close];
+  [outputStream close];
+}
+
+@end
+
+
+int main()
+{
+  NSAutoreleasePool   *arp = [NSAutoreleasePool new];
+
+  // Run the test...
+  IPv6StreamTest *test = [[IPv6StreamTest alloc] init];
+
+  [test runTest];
+  
+  [arp release];
+  arp = nil;
+
+  return 0;
+}
+#else
+int main()
+{
+  return 0;
+}
+#endif
