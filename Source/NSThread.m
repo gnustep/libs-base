d53a7325762469fbbebe98e5aed4f2c4c836bc28
Hugo Melder <service@hugomelder.com>
2024-10-30 03:13:28 -0700

    NSThread: Implement +detatchThreadWithBlock: and -initWithBlock: (#454)
    * NSThread: Implement +detatchThreadWithBlock: and -initWithBlock:
    
    * Remove extraneous include
    
    * NSThread: Define GSThreadBlock so that GCC stub impl does not fail
    
    * Add missing spaces - trivial style fixup
    
    ---------
    
    Co-authored-by: rfm <richardfrithmacdonald@gmail.com>

--- Source/NSThread.m
+++ Source/NSThread.m
@@ -1337,7 +1337,15 @@ unregisterActiveThread(NSThread *thread)
         NSStringFromSelector(_cmd)];
     }
 
-  [_target performSelector: _selector withObject: _arg];
+  if (_targetIsBlock)
+    {
+      GSThreadBlock block = (GSThreadBlock)_target;
+      CALL_BLOCK_NO_ARGS(block);
+    }
+  else
+    {
+      [_target performSelector: _selector withObject: _arg];
+    }
 }
 
 - (NSString*) name
@@ -2410,6 +2418,31 @@ GSRunLoopInfoForThread(NSThread *aThread)
   [NSThread detachNewThreadSelector: aSelector
                            toTarget: self
                          withObject: anObject];
+}
+
+@end
+
+@implementation NSThread (BlockAdditions)
+
++ (void) detachNewThreadWithBlock: (GSThreadBlock)block
+{
+  NSThread	*thread;
+
+  thread = [[NSThread alloc] initWithBlock: block];
+  [thread start];
+
+  RELEASE(thread);
+}
+
+- (instancetype) initWithBlock: (GSThreadBlock)block
+{
+  if (nil != (self = [self init]))
+    {
+      _targetIsBlock = YES;
+      /* Copy block to heap */
+      _target = _Block_copy(block);
+    } 
+  return self;
 }
 
 @end
