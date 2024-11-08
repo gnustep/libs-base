75fe3d2e8d2a692368a2b23587d0381890a54ce9
rfm <richardfrithmacdonald@gmail.com>
2024-11-08 13:35:48 +0000

    Fix FSF address

--- Headers/Foundation/NSHashTable.h
+++ Headers/Foundation/NSHashTable.h
@@ -21,7 +21,7 @@
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, * Boston, 02111 USA.
+ * Software Foundation, Inc., 31 Milk Street #960789 Boston, MA 02196 USA.
  */ 
 
 #ifndef __NSHashTable_h_GNUSTEP_BASE_INCLUDE
