b3f95f99e6a5062f25c3191b12e254dc3f832bfc
rfm <richardfrithmacdonald@gmail.com>
2025-06-01 17:36:10 +0100

    Test with bad path that won't raise exception

--- Tests/base/NSData/errors.m
+++ Tests/base/NSData/errors.m
@@ -4,27 +4,30 @@
 
 int main()
 {
-    NSError *error;
-    NSString *path;
-    NSData *data;
+  START_SET("NSData file/url loading")
 
-    START_SET("NSData file/url loading")
+  NSError	*error;
+  NSString	*path;
+  NSData	*data;
 
-    // Try loading from a nonexistent path to trigger error
-    path = @"/tmp/nonexistent_file.txt";
-    data = [NSData dataWithContentsOfFile: path options:0 error:&error];
-    PASS(data == nil && error != nil, "+dataWithContentsOfFile:options:error: sets error on failure");
+  // Try loading from a nonexistent path to trigger error
+  path = @"/tmp/nonexistent_file.txt";
+  data = [NSData dataWithContentsOfFile: path options: 0 error: &error];
+  PASS(data == nil && error != nil,
+    "+dataWithContentsOfFile:options:error: sets error on failure")
 
-    data = [NSData dataWithContentsOfURL:[NSURL fileURLWithPath:path]
-                                                options:0
-                                                  error:&error];
-    PASS(data == nil && error != nil, "+dataWithContentsOfURL:options:error: sets error on failure");
+  data = [NSData dataWithContentsOfURL: [NSURL fileURLWithPath: path]
+						       options: 0
+							 error: &error];
+  PASS(data == nil && error != nil,
+    "+dataWithContentsOfURL:options:error: sets error on failure")
 
-    // Try loading with invalid utf-8 path
-    data = [NSData dataWithContentsOfFile: @"\xC3\x28" options:0 error:&error];
-    PASS(data == nil && error != nil, "+dataWithContentsOfURL:options:error: sets error when path is invalid");
+  // Try loading with bad path
+  data = [NSData dataWithContentsOfFile: nil options: 0 error: &error];
+  PASS(data == nil && error != nil,
+    "+dataWithContentsOfURL:options:error: sets error when path is invalid")
 
-    END_SET("NSData file/url loading")
+  END_SET("NSData file/url loading")
 
-    return 0;
+  return 0;
 }
