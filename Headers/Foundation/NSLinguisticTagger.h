e4d8113d6deaaaf12023ab5d2428d884b132bab8
Gregory John Casamento <greg.casamento@gmail.com>
2025-06-25 01:36:22 -0400

    Add documentation for NSLinguisticTagger, minor change

--- Headers/Foundation/NSLinguisticTagger.h
+++ Headers/Foundation/NSLinguisticTagger.h
@@ -1,4 +1,5 @@
-/**Definition of class NSLinguisticTagger
+/**
+   Definition of class NSLinguisticTagger
    Copyright (C) 2019 Free Software Foundation, Inc.
    
    By: Gregory John Casamento <greg.casamento@gmail.com>
@@ -117,45 +118,88 @@ GS_EXPORT_CLASS
   NSArray *_orthographyArray;
   NSUInteger _options;
 }
-  
+
+/**
+ * Initializes a tagger with the given linguistic tag schemes and options.
+ */
 - (instancetype) initWithTagSchemes: (NSArray *)tagSchemes
                             options: (NSUInteger)opts;
 
+/**
+ * Returns the list of tag schemes used by this tagger.
+ */
 - (NSArray *) tagSchemes;
 
+/**
+ * Gets the string to be tagged.
+ */
 - (NSString *) string;
+
+/**
+ * Sets the string to be tagged.
+ */
 - (void) setString: (NSString *)string;
   
+/**
+ * Returns all available tag schemes for the specified unit and language.
+ */
 + (NSArray *) availableTagSchemesForUnit: (NSLinguisticTaggerUnit)unit
                                 language: (NSString *)language;
 
+/**
+ * Returns all available tag schemes for a specific language.
+ */
 + (NSArray *) availableTagSchemesForLanguage: (NSString *)language;
 
+/**
+ * Assigns a custom orthography object for the specified range.
+ */
 - (void) setOrthography: (NSOrthography *)orthography
                   range: (NSRange)range;
-  
+
+/**
+ * Returns the orthography at a specific character index and the effective range.
+ */  
 - (NSOrthography *) orthographyAtIndex: (NSUInteger)charIndex
                         effectiveRange: (NSRangePointer)effectiveRange; 
 
+/**
+ * Informs the tagger that a portion of the string has been edited.
+ */
 - (void) stringEditedInRange: (NSRange)newRange
               changeInLength: (NSInteger)delta;
   
+/**
+ * Returns the token range at a given index for the specified unit.
+ */
 - (NSRange) tokenRangeAtIndex: (NSUInteger)charIndex
                          unit: (NSLinguisticTaggerUnit)unit;
   
+/**
+ * Returns the sentence range encompassing the given range.
+ */
 - (NSRange) sentenceRangeForRange: (NSRange)range;
 
+/**
+ * Enumerates tags over a range using a second block type (for finer control).
+ */
 - (void) enumerateTagsInRange: (NSRange)range
                          unit: (NSLinguisticTaggerUnit)unit
                        scheme: (NSLinguisticTagScheme)scheme
                       options: (NSLinguisticTaggerOptions)options
                    usingBlock: (GSLinguisticTagRangeBoolBlock)block;
   
+/**
+ * Returns the tag at a character index and provides the token and sentence ranges.
+ */
 - (NSLinguisticTag) tagAtIndex: (NSUInteger)charIndex
                           unit: (NSLinguisticTaggerUnit)unit
                         scheme: (NSLinguisticTagScheme)scheme
                     tokenRange: (NSRangePointer)tokenRange;
 
+/**
+ * Returns tags in a range for a given scheme with optional token ranges.
+ */
 - (NSArray *) tagsInRange: (NSRange)range
                      unit: (NSLinguisticTaggerUnit)unit
                    scheme: (NSLinguisticTagScheme)scheme
@@ -177,10 +221,19 @@ GS_EXPORT_CLASS
                   options: (NSLinguisticTaggerOptions)opts
               tokenRanges: (NSArray **)tokenRanges;
 
+/**
+ * Returns the dominant language of the text.
+ */
 - (NSString *) dominantLanguage;
 
+/**
+ * Returns the dominant language for a given string.
+ */
 + (NSString *) dominantLanguageForString: (NSString *)string;
 
+/**
+ * Returns the tag for a character index in a string.
+ */
 + (NSLinguisticTag) tagForString: (NSString *)string
                          atIndex: (NSUInteger)charIndex
                             unit: (NSLinguisticTaggerUnit)unit
@@ -188,6 +241,9 @@ GS_EXPORT_CLASS
                      orthography: (NSOrthography *)orthography
                       tokenRange: (NSRangePointer)tokenRange;
   
+/**
+ * Returns tags for a string in a range with optional token ranges.
+ */
 + (NSArray *)tagsForString: (NSString *)string
                      range: (NSRange)range
                       unit: (NSLinguisticTaggerUnit)unit
@@ -196,6 +252,9 @@ GS_EXPORT_CLASS
                orthography: (NSOrthography *)orthography
                tokenRanges: (NSArray **)tokenRanges;
 
+/**
+ * Enumerates tags for a string over a range using a block.
+ */
 + (void) enumerateTagsForString: (NSString *)string
                           range: (NSRange)range
                            unit: (NSLinguisticTaggerUnit)unit
@@ -205,6 +264,9 @@ GS_EXPORT_CLASS
                      usingBlock: (GSLinguisticTagRangeBoolBlock)block;
   
 
+/**
+ * Returns the possible tags at a character index and their scores.
+ */
 - (NSArray *) possibleTagsAtIndex: (NSUInteger)charIndex
                            scheme: (NSString *)tagScheme
                        tokenRange: (NSRangePointer)tokenRange
