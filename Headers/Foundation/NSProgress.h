0ddd8e6aa9434ba0e44a728e39dd9689241e1ac5
Hugo Melder <service@hugomelder.com>
2025-08-18 10:13:32 +0200

    NSProgress: Reimplement according to specification
    The current implementation of NSProgress in GNUstep is outright wrong,
    and is not implemented according to the public API documentation
    provided by Apple.
    
    While Apple's documentation omits important implementation details,
    even explicitly mentioned behaviour was either not implemented or
    invented.
    
    This commit provides a complete reimplementation of the NSProgress
    API. The following process was used to develop an implementation
    close to the one in Foundation:
    
    1. Write unit tests based on the description in the Apple API
       documentation.
    2. Validate the tests on macOS 15.5
    3. Write more unit tests of implementation-specific and undocumented
       behaviour (including order of KVO event messages).
    4. Implementation in GNUstep

--- Headers/Foundation/NSProgress.h
+++ Headers/Foundation/NSProgress.h
@@ -49,6 +49,31 @@ DEFINE_BLOCK_TYPE(NSProgressPublishingHandler, void, NSProgress*);
 DEFINE_BLOCK_TYPE_NO_ARGS(NSProgressUnpublishingHandler, void); 
 DEFINE_BLOCK_TYPE_NO_ARGS(GSProgressPendingUnitCountBlock, void); 
 DEFINE_BLOCK_TYPE_NO_ARGS(GSProgressResumingHandler, void); 
+
+/**
+ * A progress object tracks an amount of work that is yet to be completed.  Work
+ * is tracked in units. A progress is finished when the completed number of unit
+ * work is equal to the total number of unit work.
+ *
+ * Progress objects can be arranged in a tree formation. Children of a parent progress
+ * continuosly report their progress and update the parents fractionCompleted property.
+ * When a child is added to a parent progress, a pending unit count is
+ * specified. This count is added to the parent after completion of the child.
+ *
+ * There are two mechanisms to add a progress as a child to another progress object:
+ * One can explicitly add a child with -addChild:withPendingUnitCount:. There is also
+ * a way to implicitly add newly instantiated progress object as a child to an existing progress.
+ *
+ * For this, we first need to define how the "currentProgress" API works:
+ * Every thread has a thread-specific current progress stack. The top-most
+ * element on this stack can be accessed by +currentProgress class method.  By
+ * sending -becomeCurrentWithPendingUnitCount: to a progress, the progress
+ * object is pushed onto the stack and becomes the new current progress.
+ *
+ * The next progress object instantiated (excluding creation with
+ * discreteProgressWithTotalUnitCount:) becomes a child of this current progress.
+ * This is the second mechanism to implicitly add a child to a progress object.
+ */
   
 GS_EXPORT_CLASS
 @interface NSProgress : NSObject
@@ -69,82 +94,189 @@ GS_NSProgress_IVARS;
   @private id _internal GS_UNUSED_IVAR;
 #endif
 }
-  
-// Creating progress objects...
+
+/**
+ * Instantiate a new progress object by optionally supplying a parent and user
+ * information dictionary.
+ *
+ * The parent progress must either be the current progress object of the current
+ * thread, or nil.
+ */ 
 - (instancetype) initWithParent: (NSProgress *)parent 
                        userInfo: (NSDictionary *)userInfo;
+
+/**
+ * Creates a progress object for a total number of work units. This object will
+ * not be part of an existing progress tree.
+ *
+ * This object cannot implicitly become a child of the current progress, even if
+ * the current progress expects the next created progress object to be an
+ * implicit child.
+ */
 + (NSProgress *) discreteProgressWithTotalUnitCount: (int64_t)unitCount;
+
+/**
+ * Creates a progress object for a total number of work units.
+ *
+ * Note that this object may become an implicit child of the thread-local
+ * current progress object, if requested by the current progress object. 
+ */
 + (NSProgress *) progressWithTotalUnitCount: (int64_t)unitCount;
+
+/**
+ * Creates a progress object for a total number of work units. This object is
+ * then added as a child to the parent with a pending number of units.
+ */
 + (NSProgress *) progressWithTotalUnitCount: (int64_t)unitCount 
   parent: (NSProgress *)parent 
   pendingUnitCount: (int64_t)portionOfParentTotalUnitCount;
 
-// Current progress
+/**
+ * Retrieve the first progress object that is on the thread-local current progress
+ * stack.
+ */
 + (NSProgress *) currentProgress;
+
+/**
+ * Push the receiver to the thread-local current progress stack.
+ * The first object on the stack can be accessed with
+ * +[NSProgress currentProgress]. 
+ *
+ * Upon initialisation of the next progress object, the newly created object
+ * becomes a child of receiver. Note that this only holds for the first object
+ * created. If the child progress completes, the pending unit count is added to
+ * the receiver.
+ *
+ * The pending unit count represents the portion of work to perform in relation to
+ * the total number of units of work, which is the value of the receivers
+ * totalUnitCount property. The pending units of work should be less than or equal
+ * to the total units of work of the receiver. Ensure that the total number is not
+ * exceeded.
+ *
+ * Calls to this method must be matched by -[NSProgress resignCurrent].
+ */
 - (void) becomeCurrentWithPendingUnitCount: (int64_t)unitCount;
-- (void) addChild: (NSProgress *)child
-  withPendingUnitCount: (int64_t)inUnitCount;
+/**
+ * Receiver resigns the role of the current progress. The receiver is popped
+ * from the thread-local current progress stack.
+ *
+ * If the receiver is not the current progress, an exception will be thrown.
+ */
 - (void) resignCurrent;
 
-// Reporting progress
+/**
+ * Adds the progress to the receiver as a child.  The pending unit count is
+ * added to the receiver upon completion of the child progress.
+ *
+ * The pending unit count should not exceed the receivers total unit count.
+ * However, this is not enforced in Apple's NSProgress implementation.
+ */
+- (void) addChild: (NSProgress *)child
+  withPendingUnitCount: (int64_t)inUnitCount;
+
+/**
+ * Returns the total number of unit work to be tracked by the receiver.
+ */
 - (int64_t) totalUnitCount;
+/**
+ * Sets the total nubmer of unit work to be tracked by the receiver.
+ */
 - (void) setTotalUnitCount: (int64_t)unitCount;
 
+/**
+ * Returns the number of unit work already completed.
+ */
 - (int64_t) completedUnitCount;
+/**
+ * Updates the number of unit work already completed. Monotonicity of this
+ * count is not enforced.
+ */
 - (void) setCompletedUnitCount: (int64_t)unitCount;
 
 - (NSString *) localizedDescription;
 - (NSString *) localizedAdditionalDescription;
 
-// Observing progress
+/**
+ * Provides a fraction of completed work, including work done by child
+ * progresses. Different from the completedUnitCount property, this property is
+ * continuously updated, tracking the progress of children as well.
+ */
 - (double) fractionCompleted;
 
-// Controlling progress
+/**
+ * Indicates whether the progress is cancellable. By default, a progress is
+ * cancellable but not pausable.
+ */
 - (BOOL) isCancellable;
 - (BOOL) isCancelled;
+
+/**
+ * Cancels a progress and its unfinished children.  If a cancellation handler is
+ * set, it will be called before cancelling child progresses.
+ */
 - (void) cancel;
+/**
+ * Sets a cancellation handler that is called when a progress gets cancelled.
+ */
 - (void) setCancellationHandler: (GSProgressCancellationHandler) handler;
 
+/**
+ * Pausation of a progress is not implemented. This method always returns 'NO'.
+ */
 - (BOOL) isPausable;
+/**
+ * Pausation of a progress is not implemented. Therefore a progress is never
+ * paused and this method always returns 'NO'.
+ */
 - (BOOL) isPaused;
+/**
+ * Pausation of a progress is not implemented. This method will throw an
+ * exception.
+ */
 - (void) pause;
 - (void) setPausingHandler: (GSProgressPausingHandler) handler;
 
 - (void) resume;
 - (void) setResumingHandler: (GSProgressResumingHandler) handler;
 
-// Progress Information
 - (BOOL) isIndeterminate;
-- (void) setIndeterminate: (BOOL)flag;
-- (NSProgressKind) kind;
+
 - (void) setKind: (NSProgressKind)k;
+- (NSProgressKind) kind;
+
 - (void) setUserInfoObject: (id)obj
                     forKey: (NSProgressUserInfoKey)key;
-- (GS_GENERIC_CLASS(NSDictionary,NSProgressUserInfoKey,id) *)userInfo;
 
-// Instance property accessors...
 - (void) setFileOperationKind: (NSProgressFileOperationKind)k;
 - (NSProgressFileOperationKind) fileOperationKind;
 - (void) setFileUrl: (NSURL *)u;
+
 - (NSURL *) fileUrl;
+
 - (BOOL) isFinished;
 - (BOOL) isOld;
+
 - (void) setEstimatedTimeRemaining: (NSNumber *)n;
 - (NSNumber *) estimatedTimeRemaining;
+
 - (void) setFileCompletedCount: (NSNumber *)n;
 - (NSNumber *) fileCompletedCount;
+
 - (void) setFileTotalCount: (NSNumber *)n;
 - (NSNumber *) fileTotalCount;
-- (void) setThroughput: (NSNumber *)n;
-- (NSNumber *) throughtput;
 
-// Instance methods
+- (void) setThroughput: (NSNumber *)n;
+- (NSNumber *) throughput;
+
+/**
+ * The progress discovery functionality is not implemented. This method will
+ * throw an exception.
+ */
 - (void) publish;
 - (void) unpublish;
 - (void) performAsCurrentWithPendingUnitCount: (int64_t)unitCount 
   usingBlock: (GSProgressPendingUnitCountBlock)work;
 
-// Type methods
 + (id) addSubscriberForFileURL: (NSURL *)url 
          withPublishingHandler: (NSProgressPublishingHandler)publishingHandler;
 + (void) removeSubscriber: (id)subscriber;
---
+++ Tests/base/NSProgress/kvo.m
@@ -0,0 +1,147 @@
+/* Test KVO support in NSProgress */
+
+#include "Foundation/NSException.h"
+#include "GNUstepBase/GNUstep.h"
+#import <Foundation/NSProgress.h>
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSKeyValueObserving.h>
+#import "ObjectTesting.h"
+
+@interface ParentObserver : NSObject
+{
+@public
+    uint32_t _counter;
+}
+@end
+
+/* Order of events as seen in macOS 15.5.
+ */
+@implementation ParentObserver
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context
+{
+  switch (_counter)
+  {
+    case 0: // parent change totalUnitCount (prior)
+        PASS(context == (void *)1, "Observation is parent 'totalUnitCount'")
+        break;
+    case 1: // parent change co
+        PASS(context == (void *)1, "Observation is parent 'totalUnitCount'")
+        break;
+    case 2: // parent change completedUnitCount (prior)
+        PASS((int64_t)context == 2, "Observation is parent 'completedUnitCount'")
+        break;
+    case 3: // parent change finished (prior)
+        PASS((int64_t)context == 3, "Observation is parent 'finished'")
+        break;
+    case 4: // parent change finished
+        PASS((int64_t)context == 3, "Observation is parent 'finished'")
+        break;
+    case 5: // parent change completedUnitCount
+        PASS((int64_t)context == 2, "Observation is parent 'completedUnitCount'")
+        break;
+    default:
+        PASS(0, "Unexpected KVO change event");
+  }
+  _counter += 1;
+}
+@end
+
+@interface ChildObserver : NSObject
+{
+@public
+    uint32_t _counter;
+}
+@end
+
+@implementation ChildObserver
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context
+{
+  switch (_counter)
+  {
+    case 0: // child change fractionCompleted (prior)
+        PASS(context == (void *)3, "Observation is child 'fractionCompleted'")
+        break;
+    case 1: // child change completedUnitCount (prior)
+        PASS(context == (void *)2, "Observation is child 'completedUnitCount'")
+        break;
+    case 2: // child change finished (prior)
+        PASS(context == (void *)4, "Observation is child 'finished'")
+        break;
+    case 3: // child change finished
+        PASS(context == (void *)4, "Observation is child 'finished'")
+        break;
+    case 4: // child change completedUnitCount
+        PASS(context == (void *)2, "Observation is child 'completedUnitCount'")
+        break;
+    case 5: // child change fractionCompleted
+        PASS(context == (void *)3, "Observation is child 'fractionCompleted'")
+        break;
+    default:
+        PASS(0, "Unexpected KVO change event");
+  }
+  _counter += 1;
+}
+@end
+
+int main(void)
+{
+  @autoreleasepool {
+    ParentObserver *parentObserver = [ParentObserver new];
+    NSProgress *parent = [[NSProgress alloc] initWithParent: nil userInfo: nil];
+    NSKeyValueObservingOptions options = NSKeyValueObservingOptionNew |
+            NSKeyValueObservingOptionOld | NSKeyValueObservingOptionPrior;
+
+    [parent addObserver: parentObserver
+             forKeyPath: @"totalUnitCount"
+            options: options
+            context: (void *)1];
+    [parent addObserver: parentObserver
+             forKeyPath: @"completedUnitCount"
+            options: options
+            context: (void *)2];
+    [parent addObserver: parentObserver
+             forKeyPath: @"finished"
+            options: options
+            context: (void *)3];
+
+    [parent setTotalUnitCount: 10]; // First set of notifications
+    [parent becomeCurrentWithPendingUnitCount: 10];
+
+    ChildObserver *childObserver = [ChildObserver new];
+    NSProgress *child = [NSProgress progressWithTotalUnitCount: 10];
+
+    [child addObserver: childObserver
+             forKeyPath: @"completedUnitCount"
+            options: options
+            context: (void *)2];
+    [child addObserver: childObserver
+             forKeyPath: @"fractionCompleted"
+            options: options
+            context: (void *)3];
+    [child addObserver: childObserver
+             forKeyPath: @"finished"
+            options: options
+            context: (void *)4];
+
+    [child setCompletedUnitCount: 10]; // Second set of notifications
+    [parent resignCurrent];
+
+    [child removeObserver: childObserver forKeyPath: @"completedUnitCount"];
+    [child removeObserver: childObserver forKeyPath: @"fractionCompleted"];
+    [child removeObserver: childObserver forKeyPath: @"finished"];
+    [childObserver release];
+
+    [parent removeObserver: parentObserver forKeyPath: @"totalUnitCount"];
+    [parent removeObserver: parentObserver forKeyPath: @"completedUnitCount"];
+    [parent removeObserver: parentObserver forKeyPath: @"finished"];
+    [parentObserver release];
+    [parent release];
+  }
+  return 0;
+}
