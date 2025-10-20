9bf352344e5d6805850d04890dcdbf47e4a1b7a8
richard <richard@alpine.my.domain>
2025-10-20 14:42:25 +0100

    avoid compiler warning

--- Tools/gdnc.m
+++ Tools/gdnc.m
@@ -49,20 +49,20 @@
 #endif
 
 
-#if	defined(HAVE_SYS_FCNTL_H)
-#  include	<sys/fcntl.h>
-#elif	defined(HAVE_FCNTL_H)
+#if	defined(HAVE_FCNTL_H)
 #  include	<fcntl.h>
+#elif	defined(HAVE_SYS_FCNTL_H)
+#  include	<sys/fcntl.h>
 #endif
 
 #ifdef	HAVE_SYSLOG_H
 #include <syslog.h>
 #endif
 
-#if	defined(HAVE_SYS_SIGNAL_H)
-#  include	<sys/signal.h>
-#elif	defined(HAVE_SIGNAL_H)
+#if	defined(HAVE_SIGNAL_H)
 #  include	<signal.h>
+#elif	defined(HAVE_SYS_SIGNAL_H)
+#  include	<sys/signal.h>
 #endif
 
 #ifndef NSIG
