6cf7be7a8e3ce587a9441a27f43c85e8fd88dda2
Gregory John Casamento <greg.casamento@gmail.com>
2025-12-12 05:35:21 -0500

    Update NSEnergyFormatter to eliminate whitespace, and re-indent

--- Source/NSEnergyFormatter.m
+++ Source/NSEnergyFormatter.m
@@ -1,21 +1,21 @@
 /** Implementation of class NSEnergyFormatter
    Copyright (C) 2019 Free Software Foundation, Inc.
-   
+
    By: Gregory John Casamento <greg.casamento@gmail.com>
    Date: Tue Oct  8 13:30:10 EDT 2019
 
    This file is part of the GNUstep Library.
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free
    Software Foundation, Inc., 31 Milk Street #960789 Boston, MA 02196 USA.
@@ -65,7 +65,7 @@
 {
   ASSIGN(_numberFormatter, formatter);
 }
-  
+
 - (NSFormattingUnitStyle) unitStyle
 {
   return _unitStyle;
@@ -75,7 +75,7 @@
 {
   _unitStyle = style;
 }
-  
+
 - (BOOL) isForFoodEnergyUse
 {
   return _isForFoodEnergyUse;
@@ -91,112 +91,112 @@
   NSString *formattedNumber = nil;
   NSString *unitString = nil;
   NSString *result = nil;
-  
+
   // Format the numeric value
   formattedNumber = [[self numberFormatter] stringFromNumber: [NSNumber numberWithDouble: value]];
-  
+
   // Determine unit string based on style and unit
   switch (_unitStyle)
     {
     case NSFormattingUnitStyleShort:
       switch (unit)
-        {
-        case NSEnergyFormatterUnitJoule:
-          unitString = (_isForFoodEnergyUse) ? @"J" : @"J";
-          break;
-        case NSEnergyFormatterUnitKilojoule:
-          unitString = (_isForFoodEnergyUse) ? @"kJ" : @"kJ";
-          break;
-        case NSEnergyFormatterUnitCalorie:
-          unitString = (_isForFoodEnergyUse) ? @"cal" : @"cal";
-          break;
-        case NSEnergyFormatterUnitKilocalorie:
-          unitString = (_isForFoodEnergyUse) ? @"Cal" : @"kcal";
-          break;
-        }
+	{
+	case NSEnergyFormatterUnitJoule:
+	  unitString = (_isForFoodEnergyUse) ? @"J" : @"J";
+	  break;
+	case NSEnergyFormatterUnitKilojoule:
+	  unitString = (_isForFoodEnergyUse) ? @"kJ" : @"kJ";
+	  break;
+	case NSEnergyFormatterUnitCalorie:
+	  unitString = (_isForFoodEnergyUse) ? @"cal" : @"cal";
+	  break;
+	case NSEnergyFormatterUnitKilocalorie:
+	  unitString = (_isForFoodEnergyUse) ? @"Cal" : @"kcal";
+	  break;
+	}
       result = [NSString stringWithFormat: @"%@%@", formattedNumber, unitString];
       break;
-      
+
     case NSFormattingUnitStyleMedium:
       switch (unit)
-        {
-        case NSEnergyFormatterUnitJoule:
-          unitString = @"J";
-          break;
-        case NSEnergyFormatterUnitKilojoule:
-          unitString = @"kJ";
-          break;
-        case NSEnergyFormatterUnitCalorie:
-          unitString = (_isForFoodEnergyUse) ? @"cal" : @"cal";
-          break;
-        case NSEnergyFormatterUnitKilocalorie:
-          unitString = (_isForFoodEnergyUse) ? @"Cal" : @"kcal";
-          break;
-        }
+	{
+	case NSEnergyFormatterUnitJoule:
+	  unitString = @"J";
+	  break;
+	case NSEnergyFormatterUnitKilojoule:
+	  unitString = @"kJ";
+	  break;
+	case NSEnergyFormatterUnitCalorie:
+	  unitString = (_isForFoodEnergyUse) ? @"cal" : @"cal";
+	  break;
+	case NSEnergyFormatterUnitKilocalorie:
+	  unitString = (_isForFoodEnergyUse) ? @"Cal" : @"kcal";
+	  break;
+	}
       result = [NSString stringWithFormat: @"%@ %@", formattedNumber, unitString];
       break;
-      
+
     case NSFormattingUnitStyleLong:
       switch (unit)
-        {
-        case NSEnergyFormatterUnitJoule:
-          unitString = (value == 1.0) ? @"joule" : @"joules";
-          break;
-        case NSEnergyFormatterUnitKilojoule:
-          unitString = (value == 1.0) ? @"kilojoule" : @"kilojoules";
-          break;
-        case NSEnergyFormatterUnitCalorie:
-          if (_isForFoodEnergyUse)
-            unitString = (value == 1.0) ? @"Calorie" : @"Calories";
-          else
-            unitString = (value == 1.0) ? @"calorie" : @"calories";
-          break;
-        case NSEnergyFormatterUnitKilocalorie:
-          if (_isForFoodEnergyUse)
-            unitString = (value == 1.0) ? @"Calorie" : @"Calories";
-          else
-            unitString = (value == 1.0) ? @"kilocalorie" : @"kilocalories";
-          break;
-        }
+	{
+	case NSEnergyFormatterUnitJoule:
+	  unitString = (value == 1.0) ? @"joule" : @"joules";
+	  break;
+	case NSEnergyFormatterUnitKilojoule:
+	  unitString = (value == 1.0) ? @"kilojoule" : @"kilojoules";
+	  break;
+	case NSEnergyFormatterUnitCalorie:
+	  if (_isForFoodEnergyUse)
+	    unitString = (value == 1.0) ? @"Calorie" : @"Calories";
+	  else
+	    unitString = (value == 1.0) ? @"calorie" : @"calories";
+	  break;
+	case NSEnergyFormatterUnitKilocalorie:
+	  if (_isForFoodEnergyUse)
+	    unitString = (value == 1.0) ? @"Calorie" : @"Calories";
+	  else
+	    unitString = (value == 1.0) ? @"kilocalorie" : @"kilocalories";
+	  break;
+	}
       result = [NSString stringWithFormat: @"%@ %@", formattedNumber, unitString];
       break;
-      
+
     default:
       // Fallback to NSMeasurementFormatter for unknown styles
       {
-        NSUnit *u = nil;
-        NSMeasurement *m = nil;
-        NSMeasurementFormatter *mf = nil;
+	NSUnit *u = nil;
+	NSMeasurement *m = nil;
+	NSMeasurementFormatter *mf = nil;
 
-        switch (unit)
-          {
-          case NSEnergyFormatterUnitJoule:
-            u = [NSUnitEnergy joules];
-            break;
-          case NSEnergyFormatterUnitKilojoule:
-            u = [NSUnitEnergy kilojoules];
-            break;
-          case NSEnergyFormatterUnitCalorie:
-            u = [NSUnitEnergy calories];
-            break;
-          case NSEnergyFormatterUnitKilocalorie:
-            u = [NSUnitEnergy kilocalories];
-            break;
-          }
+	switch (unit)
+	  {
+	  case NSEnergyFormatterUnitJoule:
+	    u = [NSUnitEnergy joules];
+	    break;
+	  case NSEnergyFormatterUnitKilojoule:
+	    u = [NSUnitEnergy kilojoules];
+	    break;
+	  case NSEnergyFormatterUnitCalorie:
+	    u = [NSUnitEnergy calories];
+	    break;
+	  case NSEnergyFormatterUnitKilocalorie:
+	    u = [NSUnitEnergy kilocalories];
+	    break;
+	  }
 
-        m = [[NSMeasurement alloc] initWithDoubleValue: value
-                                                  unit: u];
-        AUTORELEASE(m);
-        mf = [[NSMeasurementFormatter alloc] init];
-        AUTORELEASE(mf);
-        [mf setUnitStyle: _unitStyle];
-        [mf setNumberFormatter: [self numberFormatter]];
-        
-        result = [mf stringFromMeasurement: m];
+	m = [[NSMeasurement alloc] initWithDoubleValue: value
+						  unit: u];
+	AUTORELEASE(m);
+	mf = [[NSMeasurementFormatter alloc] init];
+	AUTORELEASE(mf);
+	[mf setUnitStyle: _unitStyle];
+	[mf setNumberFormatter: [self numberFormatter]];
+
+	result = [mf stringFromMeasurement: m];
       }
       break;
     }
-  
+
   return result;
 }
 
@@ -247,17 +247,17 @@
   if (self != nil)
     {
       if ([coder allowsKeyedCoding])
-        {
-          _isForFoodEnergyUse = [coder decodeBoolForKey: @"NS.forFoodEnergyUse"];
-          ASSIGN(_numberFormatter, [coder decodeObjectForKey: @"NS.numberFormatter"]);
-          _unitStyle = [coder decodeIntegerForKey: @"NS.unitOptions"];
-        }
+	{
+	  _isForFoodEnergyUse = [coder decodeBoolForKey: @"NS.forFoodEnergyUse"];
+	  ASSIGN(_numberFormatter, [coder decodeObjectForKey: @"NS.numberFormatter"]);
+	  _unitStyle = [coder decodeIntegerForKey: @"NS.unitOptions"];
+	}
       else
-        {
-          [coder decodeValueOfObjCType: @encode(BOOL) at: &_isForFoodEnergyUse];
-          [coder decodeValueOfObjCType: @encode(id) at: &_numberFormatter];
-          [coder decodeValueOfObjCType: @encode(NSFormattingUnitStyle) at: &_unitStyle];
-        }
+	{
+	  [coder decodeValueOfObjCType: @encode(BOOL) at: &_isForFoodEnergyUse];
+	  [coder decodeValueOfObjCType: @encode(id) at: &_numberFormatter];
+	  [coder decodeValueOfObjCType: @encode(NSFormattingUnitStyle) at: &_unitStyle];
+	}
     }
   return self;
 }
@@ -280,4 +280,3 @@
 }
 
 @end
-
